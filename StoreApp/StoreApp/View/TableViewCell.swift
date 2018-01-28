11ba425fb5b1ec5bcb2cfbdb1314ef8eb850643f
yang hee jung <puuurm@naver.com>
2018-01-28 20:20:10 +0900

    Create tablew view cell

---
+++ StoreApp/StoreApp/View/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  StoreApp
+//
+//  Created by yangpc on 2018. 1. 28..
+//  Copyright © 2018년 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+class TablewViewCell: UITableViewCell {
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var badgeLabel: UILabel!
+    @IBOutlet weak var foodimageView: UIImageView!
+}
