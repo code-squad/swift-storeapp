9f38226f8e35ef61f4564b499fb97c8334ec6121
yang hee jung <puuurm@naver.com>
2018-01-29 10:20:29 +0900

    Add various colors to the badge

---
+++ StoreApp/StoreApp/Model/Badge.swift
@@ -0,0 +1,14 @@
+//
+//  Badge.swift
+//  StoreApp
+//
+//  Created by yangpc on 2018. 1. 29..
+//  Copyright © 2018년 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+struct Badge {
+    static var colors = ["이벤트특가": UIColor.lightPurple, "론칭특가": UIColor.tangerine]
+}
+
---
+++ StoreApp/StoreApp/Util/Extension+UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  Extension+UIColor.swift
+//  StoreApp
+//
+//  Created by yangpc on 2018. 1. 29..
+//  Copyright © 2018년 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class var lightPurple: UIColor { return UIColor(red: 207/255, green: 151/255, blue: 215/255, alpha: 1) }
+    class var tangerine: UIColor { return UIColor(red: 255/255, green: 203/255, blue: 53/255, alpha: 1)}
+    class var skyBlue: UIColor {return UIColor(red: 137/255, green: 209/255, blue: 236/255, alpha: 1)}
+}
