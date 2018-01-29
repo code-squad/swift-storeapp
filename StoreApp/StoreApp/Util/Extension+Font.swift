d412566fb16a2464129472c47c55a8cfc4c37c74
yang hee jung <puuurm@naver.com>
2018-01-29 15:49:43 +0900

    Modify constant value, such as font name or font size, to a meaningful code

---
+++ StoreApp/StoreApp/Util/Extension+Font.swift
@@ -0,0 +1,20 @@
+//
+//  Extension+Font.swift
+//  StoreApp
+//
+//  Created by yangpc on 2018. 1. 29..
+//  Copyright © 2018년 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    class func euphemiaUCAS(ofSize fontSize: CGFloat) -> UIFont? {
+        return UIFont(name: "EuphemiaUCAS", size: fontSize)
+    }
+
+    class func euphemiaUCASBold(ofSize fontSize: CGFloat) -> UIFont? {
+        return UIFont(name: "EuphemiaUCAS-Bold", size: fontSize)
+    }
+
+}
