155b038f224a83bd5c848d838815e8167fb42af1
yang hee jung <puuurm@naver.com>
2018-01-30 15:21:54 +0900

    Modify string value to string extension static let

--- StoreApp/StoreApp/Util/Extension+String.swift
+++ StoreApp/StoreApp/Util/Extension+String.swift
@@ -31,6 +31,19 @@ extension String {
     static let main = "main"
     static let soup = "soup"
     static let side = "side"
+
+    // View
+    static let headerView = "HeaderView"
+    static let tableViewCell = "TableViewCell"
+
+    // Section
+    static let mainTitle = "메인반찬"
+    static let soupTitle = "국.찌게"
+    static let sideTitle = "밑반찬"
+
+    static let mainDetail = "메인반찬 / 한그릇 뚝딱 메인 요리"
+    static let soupDetail = "국.찌게 / 김이 모락모락 국.찌게"
+    static let sideDetail = "밑반찬 / 언제 먹어도 든든한 밑반찬"
 }
 
 extension Int {
