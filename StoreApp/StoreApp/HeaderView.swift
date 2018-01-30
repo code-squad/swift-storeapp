7205e6744d3d98c3b6f594e6aa98f69807af3559
yang hee jung <puuurm@naver.com>
2018-01-30 11:53:46 +0900

    Add HeadView xib file

---
+++ StoreApp/StoreApp/HeaderView.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderView.swift
+//  StoreApp
+//
+//  Created by yangpc on 2018. 1. 30..
+//  Copyright © 2018년 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
