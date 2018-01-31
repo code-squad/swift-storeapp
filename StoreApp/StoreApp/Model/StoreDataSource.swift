11f5acfe0c9eff4106e6457ab26d3976a069eb09
yang hee jung <puuurm@naver.com>
2018-01-31 12:10:25 +0900

    Solve the out of index problem on items property in StoreDataSource

--- StoreApp/StoreApp/Model/StoreDataSource.swift
+++ StoreApp/StoreApp/Model/StoreDataSource.swift
@@ -16,11 +16,12 @@ struct StoreDataSource {
     }
 
     mutating func setItems(resources: [String]) throws {
-        let section = resources.count
-        for s in 0..<section {
+        let sectionCount = resources.count
+        items = [[StoreItem]](repeating: [], count: sectionCount)
+        for s in 0..<sectionCount {
             do {
                 let item = try decodeJSONData(from: resources[s])
-                items[section].append(contentsOf: item)
+                items[s].append(contentsOf: item)
             } catch let error {
                 throw error
             }
@@ -28,10 +29,12 @@ struct StoreDataSource {
     }
 
     func numberOfRowsInSection(section: Int) -> Int {
+        if items.isEmpty { return 0 }
         return items[section].count
     }
 
-    func item(indexPath: IndexPath) -> StoreItem {
+    func item(indexPath: IndexPath) -> StoreItem? {
+        if items.isEmpty { return nil }
         return items[indexPath.section][indexPath.row]
     }
 
