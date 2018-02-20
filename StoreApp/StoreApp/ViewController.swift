ddb508c9dd19a8e3bf9f0663b8a8db9e5e965846
undervineg <undervineg@gmail.com>
2018-02-20 14:18:18 +0900

    [설정] SwiftLint 적용

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by 심 승민 on 2018. 2. 20..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
