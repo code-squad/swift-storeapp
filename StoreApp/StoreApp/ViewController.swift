d695c165f3fa33daa1e53e185495f540a381fe05
undervineg <undervineg@gmail.com>
2018-02-21 11:07:59 +0900

    [추가] 데이터를 뷰에 추가하여 테이블 뷰 완성

--- StoreApp/StoreApp/ViewController.swift
+++ StoreApp/StoreApp/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    private var items = [StoreItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        let data = JSONParser.getDataFromJSONFile("main")
+        self.items = JSONParser.decode(data: data, toType: [StoreItem].self)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.estimatedRowHeight = 40
+        self.tableView.rowHeight = UITableViewAutomaticDimension
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currentRowData: StoreItem = self.items[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as? ItemCell else {
+            return UITableViewCell()
+        }
+        cell.title.text = currentRowData.title
+        cell.titleDescription.text = currentRowData.description
+        cell.pricesContainer.normalPrice?.attributedText = currentRowData.normalPrice?.strike
+        cell.pricesContainer.salePrice.attributedText = currentRowData.salePrice.salesHighlight
+        cell.badges?.appendItems(with: currentRowData.badges)
+        return cell
+    }
 
 }
-
