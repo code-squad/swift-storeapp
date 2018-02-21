8a50a46be097aca31c82d2a531adce447a0e6a3a
undervineg <undervineg@gmail.com>
2018-02-21 10:56:25 +0900

    [추가] 셀 내부 콘텐츠뷰들의 스타일을 지정하기 위한 상수값 일괄 저장

---
+++ StoreApp/StoreApp/Model/Style.swift
@@ -0,0 +1,73 @@
+//
+//  ViewModel.swift
+//  StoreApp
+//
+//  Created by 심 승민 on 2018. 2. 21..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+protocol UILabelPresentable {
+    var fontSize: CGFloat { get }
+    var textColor: UIColor { get }
+    var isBold: Bool { get }
+    var textAlignment: NSTextAlignment { get }
+}
+
+struct Style {
+    struct Title: UILabelPresentable {
+        private(set) var fontSize: CGFloat = 16.0
+        private(set) var textColor: UIColor = .black
+        private(set) var isBold: Bool = true
+        private(set) var textAlignment: NSTextAlignment = .left
+    }
+
+    struct Description: UILabelPresentable {
+        private(set) var fontSize: CGFloat = 12.0
+        private(set) var textColor: UIColor = .lightGray
+        private(set) var isBold: Bool = false
+        private(set) var textAlignment: NSTextAlignment = .left
+    }
+
+    struct NormalPrice: UILabelPresentable {
+        private(set) var fontSize: CGFloat = 14.0
+        private(set) var textColor: UIColor = .lightGray
+        private(set) var isBold: Bool = false
+        private(set) var textAlignment: NSTextAlignment = .left
+    }
+
+    struct SalePrice: UILabelPresentable {
+        private(set) var fontSize: CGFloat = 12.0
+        private(set) var textColor: UIColor = .emerald
+        private(set) var isBold: Bool = true
+        private(set) var textAlignment: NSTextAlignment = .left
+        private(set) var font: UIFont? = UIFont(name: "AvenirNext-Medium", size: 18)
+    }
+
+    struct Badges {
+        static let textAlignment: NSTextAlignment = .center
+        static let font: UIFont = .boldSystemFont(ofSize: 14)
+        static let textColor: UIColor = .white
+        static let cornerRadius: CGFloat = 5
+        static let clipsToBounds: Bool = true
+        static let topPadding: CGFloat = 0
+        static let leftPadding: CGFloat = 3
+        static let bottomPadding: CGFloat = 0
+        static let rightPadding: CGFloat = 3
+    }
+
+    enum BadgeType: String {
+        case event = "이벤트특가"
+        case launching = "론칭특가"
+        case soldout = "품절"
+
+        var color: UIColor {
+            switch self {
+            case .event: return UIColor.lightPurple
+            case .launching: return UIColor.lightOrange
+            case .soldout: return UIColor.deepGray
+            }
+        }
+    }
+}
