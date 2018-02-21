9d871021e9319676f6c50aad9e483daea5d77105
undervineg <undervineg@gmail.com>
2018-02-21 10:54:01 +0900

    [추가] 테이블 셀 내부 데이터를 저장할 구조체 추가
    Decodable을 채택하여 JSON 데이터를 decode 할 수 있도록 함.

---
+++ StoreApp/StoreApp/Model/StoreItem.swift
@@ -0,0 +1,46 @@
+//
+//  StoreItem.swift
+//  StoreApp
+//
+//  Created by 심 승민 on 2018. 2. 20..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import Foundation
+
+struct StoreItem: Decodable {
+    let detailHash: String
+    let image: String
+    let alt: String
+    let deliveryTypes: [String]
+    let title: String
+    let description: String
+    let salePrice: String
+    let normalPrice: String?
+    let badges: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case detailHash = "detail_hash"
+        case image
+        case alt
+        case deliveryTypes = "delivery_type"
+        case title
+        case description
+        case salePrice = "s_price"
+        case normalPrice = "n_price"
+        case badges = "badge"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        self.detailHash = try values.decode(String.self, forKey: .detailHash)
+        self.image = try values.decode(String.self, forKey: .image)
+        self.alt = try values.decode(String.self, forKey: .alt)
+        self.deliveryTypes = try values.decode([String].self, forKey: .deliveryTypes)
+        self.title = try values.decode(String.self, forKey: .title)
+        self.description = try values.decode(String.self, forKey: .description)
+        self.salePrice = try values.decode(String.self, forKey: .salePrice)
+        self.normalPrice = try? values.decode(String.self, forKey: .normalPrice)
+        self.badges = try? values.decode([String].self, forKey: .badges)
+    }
+}
