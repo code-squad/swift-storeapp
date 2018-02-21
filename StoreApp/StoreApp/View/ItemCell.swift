f78329cf93638f344f350048cb6a7ef57c20d8fc
undervineg <undervineg@gmail.com>
2018-02-21 21:14:56 +0900

    [추가] 뷰 설명 추가

--- StoreApp/StoreApp/View/ItemCell.swift
+++ StoreApp/StoreApp/View/ItemCell.swift
@@ -10,11 +10,11 @@ import UIKit
 
 // 커스텀 셀 클래스 (뷰)
 class ItemCell: UITableViewCell {
-    @IBOutlet weak var thumbnail: UIImageView!
-    @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var titleDescription: UILabel!
-    @IBOutlet weak var pricesContainer: PricesContainer!
-    @IBOutlet weak var badges: BadgesContainer?
+    @IBOutlet weak var thumbnail: UIImageView!              // 썸네일
+    @IBOutlet weak var title: UILabel!                      // 제목
+    @IBOutlet weak var titleDescription: UILabel!           // 설명
+    @IBOutlet weak var pricesContainer: PricesContainer!    // 정가, 할인가
+    @IBOutlet weak var badges: BadgesContainer?             // 뱃지들
 
     override func awakeFromNib() {
         self.thumbnail.tag = Tag.thumbnail.rawValue
