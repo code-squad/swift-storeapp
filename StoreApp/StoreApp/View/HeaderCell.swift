6f72d63501248c9839ef650031b39bb59d8ee827
undervineg <undervineg@gmail.com>
2018-02-22 13:13:15 +0900

    [추가] 섹션별 제목, 부제목, 셀 데이터를 담을 구조체 추가

---
+++ StoreApp/StoreApp/View/HeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderCell.swift
+//  StoreApp
+//
+//  Created by 심 승민 on 2018. 2. 22..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
