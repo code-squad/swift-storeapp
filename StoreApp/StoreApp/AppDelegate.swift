dd9c4103190aa3133ca75060594f7e7a22481b24
undervineg <undervineg@gmail.com>
2018-02-22 13:15:57 +0900

    [추가] Status bar가 투명해서 콘텐츠가 비쳐보이는 것을 방지하기 위해 색상 설정

--- StoreApp/StoreApp/AppDelegate.swift
+++ StoreApp/StoreApp/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        StatusBar.setStatusBarBackgroundColor(color: UIColor.white)
         return true
     }
 
