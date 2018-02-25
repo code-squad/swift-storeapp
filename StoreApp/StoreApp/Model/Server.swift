99ac1a616b57397508af8e58cec4eff1b8a196c0
undervineg <undervineg@gmail.com>
2018-02-26 06:12:10 +0900

    [추가] 로컬서버/리모트서버를 구분하기 위한 열거형 추가

--- StoreApp/StoreApp/Model/Server.swift
+++ StoreApp/StoreApp/Model/Server.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+enum Server {
+    case local
+    case remote
+
+    var api: ServerAPI {
+        switch self {
+        case .local: return Local.init()
+        case .remote: return Remote.init()
+        }
+    }
+}
+
+protocol ServerAPI {
+    var main: String { get }
+    var soup: String { get }
+    var side: String { get }
+}
+
+class Local: ServerAPI {
+    var main: String {
+        return "http://localhost:3000/main"
+    }
+
+    var soup: String {
+        return "http://localhost:3000/soup"
+    }
+
+    var side: String {
+        return "http://localhost:3000/side"
+    }
+}
+
+class Remote: ServerAPI {
+    var main: String {
+        return "http://crong.codesquad.kr:8080/woowa/main"
+    }
+
+    var soup: String {
+        return "http://crong.codesquad.kr:8080/woowa/soup"
+    }
+
+    var side: String {
+        return "http://crong.codesquad.kr:8080/woowa/side"
+    }
+}
