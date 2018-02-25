6e217d4ba55a7be60910fa67b9aadcd11396344a
undervineg <undervineg@gmail.com>
2018-02-26 06:01:40 +0900

    [추가] 서버에서 파일을 받아와 디코딩하는 함수 추가

--- StoreApp/StoreApp/Helper/Downloader.swift
+++ StoreApp/StoreApp/Helper/Downloader.swift
@@ -30,37 +30,4 @@ class Downloader<T: Decodable>: NSObject {
         }.resume()
     }
 
-    // 비동기로 받는 테스트를 위해 로컬데이터에도 쓰레드 적용
-    static func download(fileName: String, toType type: [T].Type, completionHandler: @escaping DecodeResult) {
-        DispatchQueue.global().async {
-            let data = getDataFromJSONFile(fileName)
-            let result = decode(data: data, toType: type)
-            completionHandler(.success(result))
-        }
-    }
-
-    static func decode(data: Data?, toType type: [T].Type) -> [T] {
-        guard let data = data else { return [] }
-        let decoder = JSONDecoder()
-        var decodedData: [T] = [T]()
-        do {
-            decodedData = try decoder.decode(type, from: data)
-        } catch {
-            NSLog(error.localizedDescription)
-        }
-        return decodedData
-    }
-
-    static func getDataFromJSONFile(_ fileName: String) -> Data? {
-        guard let path = Bundle.main.path(forResource: fileName, ofType: "json") else { return nil }
-        let url = URL(fileURLWithPath: path)
-        var data: Data?
-        do {
-            data = try Data(contentsOf: url)
-        } catch {
-            NSLog(error.localizedDescription)
-        }
-        return data
-    }
-
 }
