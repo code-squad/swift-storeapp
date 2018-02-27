95cbcbef12856cfeb16a8579714f53390116486b
undervineg <undervineg@gmail.com>
2018-02-28 06:26:24 +0900

    [이동] TableSection 열거형을 새 파일로 분리

--- StoreApp/StoreApp/Model/Section.swift
+++ StoreApp/StoreApp/Model/Section.swift
@@ -6,62 +6,20 @@
 //  Copyright © 2018년 심 승민. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-class Section {
+class Section: NSObject {
     let type: TableSection
-    let cell: [StoreItem]
+    let cells: [StoreItem]
 
-    init() {
+    override init() {
         self.type = .main
-        self.cell = []
+        self.cells = []
+        super.init()
     }
 
     init(type: TableSection, cell: [StoreItem]) {
         self.type = type
-        self.cell = cell
-    }
-}
-
-enum TableSection: Int {
-    case main = 0
-    case soup
-    case side
-
-    static var cases: [TableSection] {
-        return [TableSection.main, .soup, .side]
-    }
-
-    var title: String {
-        switch self {
-        case .main: return "메인반찬"
-        case .soup: return "국.찌게"
-        case .side: return "밑반찬"
-        }
-    }
-
-    var subtitle: String {
-        switch self {
-        case .main: return "한그릇 뚝딱 메인 요리"
-        case .soup: return "김이 모락모락 국.찌게"
-        case .side: return "언제 먹어도 든든한 밑반찬"
-        }
-    }
-
-    func api(from serverType: Server) -> String {
-        let serverType = serverType.api
-        switch self {
-        case .main: return serverType.main
-        case .soup: return serverType.soup
-        case .side: return serverType.side
-        }
-    }
-
-    var localFilename: String {
-        switch self {
-        case .main: return "main"
-        case .soup: return "soup"
-        case .side: return "side"
-        }
+        self.cells = cell
     }
 }
