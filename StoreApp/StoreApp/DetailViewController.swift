99df5452d83b5a4e5174ea8aa1a0a214294cf1e4
undervineg <undervineg@gmail.com>
2018-03-06 14:55:49 +0900

    [추가] 데이터 로드 및 주문하기 버튼 클릭 이벤트 추가

--- StoreApp/StoreApp/DetailViewController.swift
+++ StoreApp/StoreApp/DetailViewController.swift
@@ -8,28 +8,52 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, Reusable, DetailViewDelegate {
+    var detailHash: String?
+    private var items: ItemDetail?
+    var detailView: DetailView! {
+        return self.view as? DetailView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        (self.view as? DetailView)?.orderDelegate = self
+        loadItemsFromAPI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = false
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func loadItemsFromAPI() {
+        guard let url = URL(string: "http://crong.codesquad.kr:8080/woowa/detail/"+detailHash!) else { return }
+        do {
+            let contents = try Data(contentsOf: url)
+            self.items = try JSONDecoder().decode(ItemDetail.self, from: contents)
+        } catch {
+            NSLog(error.localizedDescription)
+        }
+        presentView()
     }
-    */
+
+    func  presentView() {
+        if let items = self.items {
+            self.detailView.setContents(items)
+        }
+    }
+
+    func orderButtonDidTapped() {
+        let urlString = "https://hooks.slack.com/services/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"
+        let price = String(describing: items!.data.prices.first!)
+        let menu = String(describing: items!.data.productDescription)
+        let payload = ["text": "- 주문자: Min,\n - 가격: \(price),\n - 메뉴: \(menu)"]
+        do {
+            let payloadData = try JSONSerialization.data(withJSONObject: payload, options: .init(rawValue: 0))
+            Downloader.post(to: urlString, with: payloadData)
+        } catch {
+            NSLog(error.localizedDescription)
+        }
+        self.navigationController?.popViewController(animated: true)
+    }
 
 }
