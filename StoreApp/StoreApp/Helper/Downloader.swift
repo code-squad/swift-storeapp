c0c7cb36126ae8e561a1849ab18a2a855b3754ff
undervineg <undervineg@gmail.com>
2018-03-06 14:53:48 +0900

    [추가] POST 요청 가능한 메소드 추가

--- StoreApp/StoreApp/Helper/Downloader.swift
+++ StoreApp/StoreApp/Helper/Downloader.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class Downloader {
 
-    typealias AfterDownload = (Result) -> Void
+    typealias AfterTask = (Result) -> Void
     enum Result {
         case success(Data)
         case failure(Error)
     }
 
-    static func download(from urlString: String, completionHandler: @escaping AfterDownload) {
+    static func post(to urlString: String, with payload: Data) {
+        guard let url = URL(string: urlString) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpBody = payload
+        URLSession.shared.dataTask(with: request).resume()
+    }
+
+    static func downloadWithDataTask(from urlString: String, completionHandler: @escaping AfterTask) {
         guard let url = URL(string: urlString) else { return }
         URLSession.shared.dataTask(with: url) { (data, _, error) in
             if let error = error {
@@ -28,4 +38,20 @@ class Downloader {
         }.resume()
     }
 
+    static func downloadToGlobalQueue(from urlString: String, completionHandler: @escaping AfterTask) {
+        guard let url = URL(string: urlString) else { return }
+        DispatchQueue.global().async(execute: {
+            if let nsData = NSData(contentsOf: url) {
+                let data = Data(referencing: nsData)
+                completionHandler(.success(data))
+            } else {
+                completionHandler(.failure(DownloadError.downloadFail(message: "다운로드 에러")))
+            }
+        })
+    }
+
+}
+
+enum DownloadError: Error {
+    case downloadFail(message: String)
 }
