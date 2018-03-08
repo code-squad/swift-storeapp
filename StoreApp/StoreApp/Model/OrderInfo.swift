25d542f47f1514e4bf4198ee8b50deefa96a1136
undervineg <undervineg@gmail.com>
2018-03-08 16:10:44 +0900

    [분리] 구조체 파일 새로 생성하여 분리

---
+++ StoreApp/StoreApp/Model/OrderInfo.swift
@@ -0,0 +1,15 @@
+//
+//  OrderInfo.swift
+//  StoreApp
+//
+//  Created by 심 승민 on 2018. 3. 8..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import Foundation
+
+struct OrderInfo {
+    let name: String = "Min"
+    let price: String
+    let menu: String
+}
