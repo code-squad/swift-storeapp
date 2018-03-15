07acb4eb5c1fa5071cbd058ca878a18d0192229d
undervineg <undervineg@gmail.com>
2018-03-15 21:18:23 +0900

    [추가] 네트워크 상태를 체킹하는 클래스 추가. Alamofire 프레임워크 사용

---
+++ StoreApp/StoreApp/Controller/ReachabilityMonitor.swift
@@ -0,0 +1,39 @@
+//
+//  ReachabilityMonitor.swift
+//  StoreApp
+//
+//  Created by 심 승민 on 2018. 3. 9..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ReachabilityMonitor {
+    private let reachability: NetworkReachabilityManager!
+
+    init(hostName: String) {
+        reachability = NetworkReachabilityManager(host: hostName)
+    }
+
+    var isAvailable: Bool {
+        return reachability.isReachable
+    }
+
+    func startMonitoring() {
+        reachability.listener = { status in
+            NotificationCenter.default.post(name: .connectionChanged, object: nil,
+                                            userInfo: ["NetworkStatus": self.isAvailable])
+        }
+        reachability.startListening()
+    }
+
+    func stopMonitoring() {
+        reachability.stopListening()
+    }
+
+}
+
+extension Notification.Name {
+    static let connectionChanged = Notification.Name("connectionChanged")
+}
