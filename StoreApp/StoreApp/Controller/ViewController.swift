d97834630eafb043b302ece929b80d84e267875a
undervineg <undervineg@gmail.com>
2018-03-15 21:45:02 +0900

    [추가] 네트워크 연결성에 따라 테두리 표시 및 네트워크 재연결 시 데이터 서버에서 재로드

--- StoreApp/StoreApp/Controller/ViewController.swift
+++ StoreApp/StoreApp/Controller/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import Toaster
+import Alamofire
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, ReachabilityDetectable, RespondableForNetwork {
     @IBOutlet weak var tableView: UITableView! {
         didSet {
             tableView.delegate = self
@@ -17,12 +18,38 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     private var sections = [Section]()
+    var reachabilityMonitor: ReachabilityMonitor? {
+        didSet {
+            NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(onConnectionChanged(_:)),
+                                                   name: .connectionChanged, object: nil)
+        }
+    }
+
+    @objc private func onConnectionChanged(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+            let isAvailable = userInfo["NetworkStatus"] as? Bool else { return }
+        drawBorder(on: self.view, when: isAvailable)
+        loadDataFromServer()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.estimatedRowHeight = 40
         tableView.estimatedSectionHeaderHeight = 40
         tableView.register(HeaderCell.self, forHeaderFooterViewReuseIdentifier: HeaderCell.reuseId)
+        if let monitor = reachabilityMonitor, !monitor.isAvailable {
+            loadDataFromLocal()
+        }
+    }
+
+    private func loadDataFromLocal() {
+        loadItemsFromFile(forSection: .main)
+        loadItemsFromFile(forSection: .soup)
+        loadItemsFromFile(forSection: .side)
+    }
+
+    private func loadDataFromServer() {
         loadItemsFromAPI(forSection: .main, from: .remote)
         loadItemsFromAPI(forSection: .soup, from: .remote)
         loadItemsFromAPI(forSection: .side, from: .remote)
@@ -35,32 +62,66 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.estimatedSectionHeaderHeight = 40
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.sectionHeaderHeight = UITableViewAutomaticDimension
+        if let monitor = reachabilityMonitor {
+            drawBorder(on: self.view, when: monitor.isAvailable)
+        }
+    }
+
+    private func loadItemsFromFile(forSection section: TableSection) {
+        guard let data = Downloader.getDataFromJSONFile(section.localFilename) else { return }
+        presentDecodedData(on: section, from: data)
     }
 
     private func loadItemsFromAPI(forSection section: TableSection, from server: Server) {
         Downloader.downloadWithDataTask(from: section.api(from: server)) { response in
             switch response {
-            case .success(let result):
-                if let items = try? JSONDecoder().decode([StoreItem].self, from: result) {
-                    self.present(section: section, with: items)
-                }
+            case .success(let data):
+                self.presentDecodedData(on: section, from: data)
             case .failure(let error):
                 self.presentError(error)
             }
         }
     }
 
-    private func present(section: TableSection, with items: [StoreItem]) {
+    private func presentDecodedData(on section: TableSection, from rawData: Data) {
+        guard let items = try? JSONDecoder().decode([StoreItem].self, from: rawData) else { return }
         DispatchQueue.main.async(execute: {
-            let newSection = Section(type: section, cells: items)
-            self.sections.append(newSection)
-            if let index = self.sections.index(of: newSection) {
-                let indexSet = IndexSet(integer: index)
-                self.tableView.insertSections(indexSet, with: .automatic)
-            }
+            self.present(items: items, on: section)
         })
     }
 
+    private func present(items: [StoreItem], on section: TableSection) {
+        let newSection = Section(type: section, cells: items)
+        (TableSection.cases.count > self.sections.count) ? insertSection(newSection) : reloadSection(newSection)
+    }
+
+    private var reloadIndex = 0
+    private func reloadSection(_ section: Section) {
+        guard reloadIndex < TableSection.cases.count else {
+            reloadIndex = 0
+            return
+        }
+        self.sections[reloadIndex] = section
+        if let index = self.sections.index(of: section) {
+            let indexSet = IndexSet(integer: index)
+            self.tableView.reloadSections(indexSet, with: .automatic)
+        }
+        defer {
+            reloadIndex += 1
+        }
+    }
+
+    private func insertSection(_ newSection: Section) {
+        self.sections.append(newSection)
+        if let index = self.sections.index(of: newSection) {
+            let indexSet = IndexSet(integer: index)
+            self.tableView.insertSections(indexSet, with: .automatic)
+        }
+    }
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         return self.sections.count
     }
@@ -101,6 +162,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             as? DetailViewController {
             detailVC.detailHash = row.detailHash
             detailVC.delegate = self
+            detailVC.reachabilityMonitor = self.reachabilityMonitor
             self.navigationController?.pushViewController(detailVC, animated: true)
         }
     }
