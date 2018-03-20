d6eb871891973f4acf856b11e4032b2472168219
undervineg <undervineg@gmail.com>
2018-03-20 10:58:09 +0900

    [추가] 네트워크 연결 여부에 따라 윈도우 레벨에서 테두리 색상 변경하도록 수정

--- StoreApp/StoreApp/Supporting Files/AppDelegate.swift
+++ StoreApp/StoreApp/Supporting Files/AppDelegate.swift
@@ -12,21 +12,37 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var reachabilityMonitor: ReachabilityMonitor?
+    var reachabilityMonitor: ReachabilityMonitor? {
+        didSet {
+            NotificationCenter.default.addObserver(self, selector: #selector(onConnectionChanged(_:)),
+                                                   name: .connectionChanged, object: nil)
+        }
+    }
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         StatusBar.setStatusBarBackgroundColor(color: UIColor.white)
         reachabilityMonitor = ReachabilityMonitor(hostName: Server.remote.api.hostName!)
-        if var mainVC = window?.rootViewController?.childViewControllers.first as? ReachabilityDetectable {
-            mainVC.reachabilityMonitor = reachabilityMonitor
-        }
         reachabilityMonitor?.startMonitoring()
         return true
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         reachabilityMonitor?.stopMonitoring()
+    }
+
+    @objc private func onConnectionChanged(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+            let isNetworkAvailable = userInfo["NetworkStatus"] as? Bool else { return }
+        drawBorder(isNetworkAvailable)
+        NotificationCenter.default.post(name: .onReconnected, object: nil)
+    }
+
+    func drawBorder(_ isNetworkAvailable: Bool) {
+        let view = window?.rootViewController?.view
+        let color: UIColor = (isNetworkAvailable) ? .green : .red
+        view?.layer.borderColor = color.cgColor
+        view?.layer.borderWidth = 5
     }
 
 }
