6d88b8ff0a31ffdcf57a6f4872589a6222bd8049
ChocOZerO <nayalth@gmail.com>
2018-04-10 11:17:42 +0900

    [Delete]Cocoapods 관련 파일 제거

--- StoreApp/Pods/Target Support Files/Pods-StoreApp/Pods-StoreApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StoreApp : NSObject
-@end
-@implementation PodsDummy_Pods_StoreApp
-@end
--- StoreApp/Pods/Target Support Files/Pods-StoreAppUITests/Pods-StoreAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StoreAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_StoreAppUITests
-@end
