84357afed21ba2df3c475ac5ab3e549c66a3cc56
ChocOZerO <nayalth@gmail.com>
2018-05-14 14:33:06 +0900

    [Update]이미지 추가 #24

--- StoreApp/StoreApp/ViewControllers/DetailViewController.swift
+++ StoreApp/StoreApp/ViewControllers/DetailViewController.swift
@@ -13,6 +13,7 @@ class DetailViewController: UIViewController {
     var detailTitle: String?
     var detailDescription: String?
 
+    @IBOutlet weak var mainScrollView: UIScrollView!
     @IBOutlet weak var thumbnailsView: UIScrollView!
     @IBOutlet weak var contentsView: UIView!
 
@@ -50,10 +51,66 @@ class DetailViewController: UIViewController {
         guard let userInfo = notification.userInfo as? [String: Any] else { return }
         guard let detailInformation = userInfo["detailInformation"] as? DetailInformation else { return }
         DispatchQueue.main.async {
+            self.setThumbnailImages(with: detailInformation.thumbImages)
             self.contentsPoint.text = detailInformation.point
             self.contentsDeliveryInfo.text = detailInformation.deliveryInfo
             self.contentsDeliveryFee.text = detailInformation.deliveryFee
             self.contentsPrice.text = detailInformation.prices[0]
+            self.setDetailSection(with: detailInformation.detailSection)
+        }
+    }
+
+    private func setThumbnailImages(with urlStrings: [String]) {
+        thumbnailsView.isPagingEnabled = true
+        for index in urlStrings.indices {
+            guard let url = URL(string: urlStrings[index]) else { return }
+            let urlSession = URLSession.shared
+            urlSession.dataTask(with: url, completionHandler: { (data, response, error) in
+                if let data = data {
+                    DispatchQueue.main.async {
+                        let topImage = UIImage(data: data)
+                        let imageFrame = CGRect(x: self.thumbnailsView.frame.size.width * CGFloat(index),
+                                                y: self.thumbnailsView.frame.origin.y,
+                                                width: self.thumbnailsView.frame.size.width,
+                                                height: self.thumbnailsView.frame.size.height)
+                        let topImageView = UIImageView(frame: imageFrame)
+                        topImageView.image = topImage
+                        topImageView.contentMode = .scaleAspectFill
+                        self.thumbnailsView.addSubview(topImageView)
+                        let contentWidth = UIScreen.main.bounds.width * CGFloat(urlStrings.count)
+                        self.thumbnailsView.contentSize = CGSize(width: contentWidth,
+                                                                 height: self.thumbnailsView.frame.size.height)
+                    }
+                }
+            }).resume()
+        }
+    }
+
+    private func setDetailSection(with urlStrings: [String]) {
+        var detailSectionHeight: CGFloat = 0
+        for index in urlStrings.indices {
+            guard let url = URL(string: urlStrings[index]) else { return }
+            let urlSession = URLSession.shared
+            urlSession.dataTask(with: url, completionHandler: { (data, response, error) in
+                if let data = data {
+                    DispatchQueue.main.async {
+                        guard let detailImage = UIImage(data: data) else { return }
+                        let yOrigin = self.thumbnailsView.frame.height + self.contentsView.frame.height
+                        let imageRatio = UIScreen.main.bounds.width / detailImage.size.width
+                        let imageFrame = CGRect(x: 0,
+                                                y: detailSectionHeight + yOrigin,
+                                                width: detailImage.size.width * imageRatio,
+                                                height: detailImage.size.height * imageRatio)
+                        let detailImageView = UIImageView(frame: imageFrame)
+                        detailImageView.image = detailImage
+                        detailImageView.contentMode = .scaleAspectFit
+                        self.mainScrollView.addSubview(detailImageView)
+                        detailSectionHeight += detailImage.size.height * imageRatio
+                        self.mainScrollView.contentSize = CGSize(width: UIScreen.main.bounds.width,
+                                                                 height: yOrigin + detailSectionHeight)
+                    }
+                }
+            }).resume()
         }
     }
 
