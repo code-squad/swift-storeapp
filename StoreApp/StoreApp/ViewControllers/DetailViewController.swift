d9340a5012f413dfacbfc4cd974330a9501ef134
ChocOZerO <nayalth@gmail.com>
2018-05-15 18:33:58 +0900

    [Update]네트워크와 UI처리 로직 분리 #27

--- StoreApp/StoreApp/ViewControllers/DetailViewController.swift
+++ StoreApp/StoreApp/ViewControllers/DetailViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
     private var storeItem: StoreItem?
+    private var communicator: Communicator = Communicator()
+    private var detailSectionHeight: CGFloat = 0
 
     @IBOutlet weak var mainScrollView: UIScrollView!
     @IBOutlet weak var thumbnailsView: UIScrollView!
@@ -27,6 +29,8 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         addObservers()
         showDetail()
+        thumbnailsView.isPagingEnabled = true
+        detailSectionHeight = thumbnailsView.frame.height + contentsView.frame.height
     }
 
     func setStoreItem(with storeItem: StoreItem) {
@@ -40,44 +44,51 @@ class DetailViewController: UIViewController {
             name: .detailInformation,
             object: nil
         )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(showThumbImages(notification:)),
+            name: .thumbImages,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(showDetailSection(notification:)),
+            name: .detailSection,
+            object: nil
+        )
     }
 
     private func showDetail() {
         guard let storeItem = storeItem else { return }
         contentsTitle.text = storeItem.title
         contentsDescription.text = storeItem.description
-        let communicator = Communicator()
         communicator.detailInformation(with: storeItem.detailHash)
     }
 
     @objc private func showDetail(notification: Notification) {
         guard let userInfo = notification.userInfo as? [String: Any] else { return }
         guard let detailInformation = userInfo["detailInformation"] as? DetailInformation else { return }
+        communicator.thumbImages(with: detailInformation.thumbImages)
+        communicator.detailSection(with: detailInformation.detailSection)
         DispatchQueue.main.async {
-            self.setThumbnailImages(with: detailInformation.thumbImages)
             self.contentsPoint.text = detailInformation.point
             self.contentsDeliveryInfo.text = detailInformation.deliveryInfo
             self.contentsDeliveryFee.text = detailInformation.deliveryFee
             self.contentsPrice.text = detailInformation.prices[0]
-            self.setDetailSection(with: detailInformation.detailSection)
         }
     }
 
-    private func setThumbnailImages(with urlStrings: [String]) {
-        thumbnailsView.isPagingEnabled = true
-        for index in urlStrings.indices {
-            guard let url = URL(string: urlStrings[index]) else { return }
-            let urlSession = URLSession.shared
-            urlSession.dataTask(with: url, completionHandler: { (data, response, error) in
-                guard let data = data else { return }
-                DispatchQueue.main.async {
-                    let topImageView = self.getThumbnailImageView(from: data, index: index)
-                    self.thumbnailsView.addSubview(topImageView)
-                    let contentWidth = UIScreen.main.bounds.width * CGFloat(urlStrings.count)
-                    self.thumbnailsView.contentSize = CGSize(width: contentWidth,
-                                                             height: self.thumbnailsView.frame.size.height)
-                }
-            }).resume()
+    @objc private func showThumbImages(notification: Notification) {
+        DispatchQueue.main.async {
+            guard let userInfo = notification.userInfo as? [String: Any] else { return }
+            guard let data = userInfo["data"] as? Data else { return }
+            guard let index = userInfo["index"] as? Int else { return }
+            guard let totalCount = userInfo["totalCount"] as? Int else { return }
+            let topImageView = self.getThumbnailImageView(from: data, index: index)
+            self.thumbnailsView.addSubview(topImageView)
+            let contentWidth = UIScreen.main.bounds.width * CGFloat(totalCount)
+            self.thumbnailsView.contentSize = CGSize(width: contentWidth,
+                                                     height: self.thumbnailsView.frame.size.height)
         }
     }
 
@@ -93,23 +104,17 @@ class DetailViewController: UIViewController {
         return topImageView
     }
 
-    private func setDetailSection(with urlStrings: [String]) {
-        var detailSectionHeight = self.thumbnailsView.frame.height + self.contentsView.frame.height
-        for index in urlStrings.indices {
-            guard let url = URL(string: urlStrings[index]) else { return }
-            let urlSession = URLSession.shared
-            urlSession.dataTask(with: url, completionHandler: { (data, response, error) in
-                guard let data = data else { return }
-                DispatchQueue.main.async {
-                    guard let detailImage = UIImage(data: data) else { return }
-                    let imageRatio = UIScreen.main.bounds.width / detailImage.size.width
-                    let detailImageView = self.getImageView(from: detailImage,
-                                                            yPosition: detailSectionHeight,
-                                                            ratio: imageRatio)
-                    detailSectionHeight += detailImage.size.height * imageRatio
-                    self.addImageViewToScrollView(with: detailImageView, detailSectionHeight: detailSectionHeight)
-                }
-            }).resume()
+    @objc private func showDetailSection(notification: Notification) {
+        DispatchQueue.main.async {
+            guard let userInfo = notification.userInfo as? [String: Any] else { return }
+            guard let data = userInfo["data"] as? Data else { return }
+            guard let detailImage = UIImage(data: data) else { return }
+            let imageRatio = UIScreen.main.bounds.width / detailImage.size.width
+            let detailImageView = self.getImageView(from: detailImage,
+                                                    yPosition: self.detailSectionHeight,
+                                                    ratio: imageRatio)
+            self.detailSectionHeight += detailImage.size.height * imageRatio
+            self.addImageViewToScrollView(with: detailImageView, detailSectionHeight: self.detailSectionHeight)
         }
     }
 
