aae88a453df2bcd7a0bf400a7d93a6e489f1fea5
ChocOZerO <nayalth@gmail.com>
2018-05-18 12:46:00 +0900

    [Update]인터넷 연결 여부 표현하기 #30

--- StoreApp/StoreApp/AppDelegate.swift
+++ StoreApp/StoreApp/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        ReachabilityManager.sharedInstance.start()
         return true
     }
 
@@ -38,7 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        ReachabilityManager.sharedInstance.stop()
     }
 
 
