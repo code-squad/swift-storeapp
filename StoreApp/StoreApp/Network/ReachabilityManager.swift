a611a865e6a0a5e31445249797e7abd499670a10
ChocOZerO <nayalth@gmail.com>
2018-05-18 11:09:25 +0900

    [Update]Reachability bridging #28

--- StoreApp/StoreApp/Network/ReachabilityManager.swift
+++ StoreApp/StoreApp/Network/ReachabilityManager.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class ReachabilityManager {
+    static let sharedInstance = ReachabilityManager()
+    private var networkManager: Reachability?
+
+    private init() {
+        networkManager = Reachability.init(hostName: Keyword.Network.hostName.value)
+    }
+
+    func isConnected() -> Bool {
+        guard let networkManager = networkManager else { return false }
+        return networkManager.currentReachabilityStatus().rawValue != 0
+    }
+
+    func start() {
+        guard let networkManager = networkManager else { return }
+        networkManager.startNotifier()
+    }
+
+    func stop() {
+        guard let networkManager = networkManager else { return }
+        networkManager.stopNotifier()
+    }
+}
