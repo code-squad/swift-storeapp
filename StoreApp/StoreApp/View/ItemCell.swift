71e507774c0eb8faef0109302319124b0d3838c9
Jaewook Kwon <somedd@naver.com>
2018-05-22 16:51:33 +0900

    Add a method setting menuImage from downloaded image or cache image. #15

--- StoreApp/StoreApp/View/ItemCell.swift
+++ StoreApp/StoreApp/View/ItemCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ItemCell: UITableViewCell {
-
+    
     @IBOutlet weak var menuImage: UIImageView!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var detail: UILabel!
@@ -21,5 +21,24 @@ class ItemCell: UITableViewCell {
         detail.text = item.description
         price.text = item.s_price
         eventBadge.text = item.badge?.reduce("", +)
+        setMenuImage(item.image)
+    }
+    
+    private func setMenuImage(_ menuImageURL: String ) {
+        guard let documentDirectoryPath = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else { return }
+        guard let imageURL = URL(string: menuImageURL) else { return }
+        let fileURL = documentDirectoryPath.appendingPathComponent(imageURL.lastPathComponent)
+        if FileManager.default.fileExists (atPath: fileURL.path) {
+            guard let data = FileManager.default.contents(atPath: fileURL.path) else { return }
+            menuImage.image = UIImage(data: data)
+        } else {
+            DispatchQueue.global().async {
+                guard let data = try? Data(contentsOf: imageURL) else { return }
+                FileManager.default.createFile(atPath: fileURL.path, contents: data, attributes: nil)
+                DispatchQueue.main.async {
+                    self.menuImage.image = UIImage(data: data)
+                }
+            }
+        }
     }
 }
