746bc7ebb691e26af0585d1d3f715abd2fc6543b
Jaewook Kwon <somedd@naver.com>
2018-05-23 16:35:39 +0900

    Add Custom StackView, Label related Price. #9

---
+++ StoreApp/StoreApp/View/BadgeLabel.swift
@@ -0,0 +1,21 @@
+//
+//  BadgeLabel.swift
+//  StoreApp
+//
+//  Created by 권재욱 on 2018. 5. 23..
+//  Copyright © 2018년 권재욱. All rights reserved.
+//
+
+import UIKit
+
+class BadgeLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
