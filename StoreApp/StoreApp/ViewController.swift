bd7f97c465aed16145a592dee84914da96b5505e
Jaewook Kwon <somedd@naver.com>
2018-05-24 16:47:59 +0900

    Add a folder related Extension.

--- StoreApp/StoreApp/ViewController.swift
+++ StoreApp/StoreApp/ViewController.swift
@@ -63,35 +63,3 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
 }
-
-@IBDesignable extension UIView {
-    @IBInspectable var borderColor: UIColor? {
-        set {
-            layer.borderColor = newValue?.cgColor
-        }
-        get {
-            guard let color = layer.borderColor else {
-                return nil
-            }
-            return UIColor(cgColor: color)
-        }
-    }
-    @IBInspectable var borderWidth: CGFloat {
-        set {
-            layer.borderWidth = newValue
-        }
-        get {
-            return layer.borderWidth
-        }
-    }
-    @IBInspectable var cornerRadius: CGFloat {
-        set {
-            layer.cornerRadius = newValue
-            clipsToBounds = newValue > 0
-        }
-        get {
-            return layer.cornerRadius
-        }
-    }
-}
-
