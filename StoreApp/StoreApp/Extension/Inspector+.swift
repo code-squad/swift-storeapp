323a3e2786e899197559994e9fa6714ebe46682c
Jaewook Kwon <somedd@naver.com>
2018-05-24 16:47:59 +0900

    Add a folder related Extension.

---
+++ StoreApp/StoreApp/Extension/Inspector+.swift
@@ -0,0 +1,40 @@
+//
+//  UIView+.swift
+//  StoreApp
+//
+//  Created by 권재욱 on 2018. 5. 24..
+//  Copyright © 2018년 권재욱. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: color)
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
