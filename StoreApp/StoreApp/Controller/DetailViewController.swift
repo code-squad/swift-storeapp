263a2673163daead1ac1f225eaca6c8f9c37eed6
Jaewook Kwon <somedd@naver.com>
2018-05-25 22:26:15 +0900

    Feat: Edit VC for passing a value related detail_hash of ItemCell. #21

--- StoreApp/StoreApp/Controller/DetailViewController.swift
+++ StoreApp/StoreApp/Controller/DetailViewController.swift
@@ -9,27 +9,11 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    var detailHash: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
