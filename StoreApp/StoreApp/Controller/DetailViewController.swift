b4d55e2fede8f4f38954418c4bd3eeb9bf6b30a5
Jaewook Kwon <somedd@naver.com>
2018-05-25 17:03:02 +0900

    Feat: Add a new VC and View of detail item view. #18

---
+++ StoreApp/StoreApp/Controller/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  StoreApp
+//
+//  Created by 권재욱 on 2018. 5. 24..
+//  Copyright © 2018년 권재욱. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
