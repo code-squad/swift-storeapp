daa81fadf94a38b925a59722a18b5e74a6b61bfd
Jaewook Kwon <somedd@naver.com>
2018-05-28 11:58:21 +0900

    Feat: Add Outlets to DetailVC. #18

--- StoreApp/StoreApp/Controller/DetailViewController.swift
+++ StoreApp/StoreApp/Controller/DetailViewController.swift
@@ -12,6 +12,16 @@ class DetailViewController: UIViewController {
     
     var detailHash: String?
 
+    @IBOutlet weak var mainScrollView: UIScrollView!
+    @IBOutlet weak var thumbnailsView: UIScrollView!
+    @IBOutlet weak var detailInfoView: UIView!
+    @IBOutlet weak var detailTitle: UILabel!
+    @IBOutlet weak var detailDescription: UILabel!
+    @IBOutlet weak var detailPrice: UILabel!
+    @IBOutlet weak var detailPoint: UILabel!
+    @IBOutlet weak var detailDeliveryInfo: UILabel!
+    @IBOutlet weak var detailDeliveryFee: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
