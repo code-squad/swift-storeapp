55e2fad6004879056a74f5ed4918996e2de6de88
Jaewook Kwon <somedd@naver.com>
2018-06-08 12:24:37 +0900

    Feat: Add a method drawing a border depending on connection. #22

--- StoreApp/StoreApp/AppDelegate.swift
+++ StoreApp/StoreApp/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Toaster
+import Reachability
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +16,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        NotificationCenter.default.addObserver(self, selector: #selector(drawConnectionBorder(notification:)), name: .connection, object: nil)
         return true
+    }
+    
+    @objc func drawConnectionBorder(notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        guard let connection = userInfo[Keyword.Observer.connection.name] as? Reachability.Connection else { return }
+        let view = window?.rootViewController?.view
+        let borderAnimation = generateBorderAnimation()
+        switch connection {
+        case .cellular, .wifi:
+            view?.layer.borderColor = UIColor.green.cgColor
+            Toast(text: Keyword.toaster.connection.message).show()
+            NotificationCenter.default.post(name: .reloadSections, object: nil)
+        case .none:
+            view?.layer.borderColor = UIColor.red.cgColor
+            Toast(text: Keyword.toaster.unreachable.message).show()
+        }
+        view?.layer.add(borderAnimation, forKey: Keyword.borderAnimation.key.value as? String)
+        view?.setNeedsLayout()
+    }
+    
+    private func generateBorderAnimation() -> CABasicAnimation {
+        let borderAnimation: CABasicAnimation = CABasicAnimation(keyPath: Keyword.borderAnimation.keyPath.value as? String)
+        borderAnimation.fromValue = Keyword.borderAnimation.from.value
+        borderAnimation.toValue = Keyword.borderAnimation.to.value
+        borderAnimation.duration = Keyword.borderAnimation.duration.value as! CFTimeInterval
+        return borderAnimation
     }
 
 }
