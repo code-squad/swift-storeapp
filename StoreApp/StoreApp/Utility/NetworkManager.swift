69e096588f33e43efe2b3ae0f7bdd1c566c8c63e
Jaewook Kwon <somedd@naver.com>
2018-06-08 12:21:24 +0900

    Style: Delete unuse methods. #23

--- StoreApp/StoreApp/Utility/NetworkManager.swift
+++ StoreApp/StoreApp/Utility/NetworkManager.swift
@@ -21,49 +21,13 @@ class NetworkManager: NSObject {
         do {
             try reachability.startNotifier()
         } catch {
-            print("Unable to start notifier")
+            print(error.localizedDescription)
         }
     }
     
     @objc func networkStatusChanged(_ notification: Notification) {
-        
-    }
-    
-    static func stopNotifier() -> Void {
-        do {
-            // Stop the network status notifier
-            try (NetworkManager.sharedInstance.reachability).startNotifier()
-        } catch {
-            print("Error stopping notifier")
-        }
-    }
-    
-    // Network is reachable
-    static func isReachable(completed: @escaping (NetworkManager) -> Void) {
-        if (NetworkManager.sharedInstance.reachability).connection != .none {
-            completed(NetworkManager.sharedInstance)
-        }
-    }
-    
-    // Network is unreachable
-    static func isUnreachable(completed: @escaping (NetworkManager) -> Void) {
-        if (NetworkManager.sharedInstance.reachability).connection == .none {
-            completed(NetworkManager.sharedInstance)
-        }
-    }
-    
-    // Network is reachable via WWAN/Cellular
-    static func isReachableViaWWAN(completed: @escaping (NetworkManager) -> Void) {
-        if (NetworkManager.sharedInstance.reachability).connection == .cellular {
-            completed(NetworkManager.sharedInstance)
-        }
-    }
-    
-    // Network is reachable via WiFi
-    static func isReachableViaWiFi(completed: @escaping (NetworkManager) -> Void) {
-        if (NetworkManager.sharedInstance.reachability).connection == .wifi {
-            completed(NetworkManager.sharedInstance)
-        }
+        guard let network = notification.object as? Reachability else { return }
+        NotificationCenter.default.post(name: .connection, object: self, userInfo: [Keyword.Observer.connection.name: network.connection])
     }
     
 }
