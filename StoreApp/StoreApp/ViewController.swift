36c3f31da640ff28967cf71b74ac7d4023106b4d
Mijin Jeon <mayihelpu27@gmail.com>
2018-06-26 21:31:51 +0900

    Create project and add main.json

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by YOUTH2 on 2018. 6. 26..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
