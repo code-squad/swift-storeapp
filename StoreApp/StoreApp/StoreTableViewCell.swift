5c080228f3ab9ca9292ac851c50693bad8d88582
Mijin Jeon <mayihelpu27@gmail.com>
2018-06-28 17:37:49 +0900

    Feat: Basic Implement for tableView, custom cell #2

---
+++ StoreApp/StoreApp/StoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoreTableViewCell.swift
+//  StoreApp
+//
+//  Created by YOUTH2 on 2018. 6. 28..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class StoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
