609529d1198f349eb785b1fda6341a6895e85810
백인철 <misocharles@gmail.com>
2018-07-08 12:19:29 +0900

    Add StoreItem 모델 객체
    - main.json 파일을 다운로드해서 프로젝트에 복사하고 JSONDecoder를 활용해서 내부에 Array<StoreItem> 타입으로 변환하는 DataSource에서 사용할 모델 객체를 만든다.
    - subscript로 배열에 index로 접근하면 StoreItem 구조체를 반환한다.
    - StoreItem은 Decodable 프로토콜을 채택하고, main.json에 있는 키와 값을 매핑해서 속성으로 갖도록 구현한다.

---
+++ StoreApp/StoreApp/StoreItem.swift
@@ -0,0 +1,40 @@
+//
+//  StoreItem.swift
+//  StoreApp
+//
+//  Created by 백인철 on 2018. 7. 8..
+//  Copyright © 2018년 Incheol. All rights reserved.
+//
+
+import Foundation
+
+struct StoreItems {
+    private var items: [StoreItem]
+    init() {
+        guard let mainJsonFilePath = Bundle.main.path(forResource: "main", ofType: "json") else {
+            items = []
+            return
+        }
+        do {
+            let data = try Data.init(contentsOf: URL.init(fileURLWithPath: mainJsonFilePath))
+            let json = try JSONDecoder().decode([StoreItem].self, from: data)
+            self.items = json
+        } catch {
+            // error
+            items = []
+        }
+    }
+    
+    subscript(index: Int) -> StoreItem {
+        return self.items[index]
+    }
+}
+
+struct StoreItem: Decodable {
+    var title: String?
+    var description: String?
+    var n_price: String?
+    var s_price: String?
+    var badge: [String]?
+    var image: String?
+}
