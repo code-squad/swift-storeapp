f4fc1716041720db56cb9227b3c916702496d3a7
YUAMING <miennes3@gmail.com>
2018-07-09 22:36:53 +0900

    역할별로 폴더 분류

---
+++ StoreApp/StoreApp/View/BadgeContainer.swift
@@ -0,0 +1,13 @@
+//
+//  BadgeContainer.swift
+//  StoreApp
+//
+//  Created by yuaming on 09/07/2018.
+//  Copyright © 2018 yuaming. All rights reserved.
+//
+
+import UIKit
+
+class BadgeContainer: UIView {
+  
+}
