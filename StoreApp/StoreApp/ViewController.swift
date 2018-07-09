b8dd35f949fb41ec2614b42e6cb9e19f096d9829
YUAMING <miennes3@gmail.com>
2018-07-09 13:52:53 +0900

    프로젝트 추가

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by yuaming on 09/07/2018.
+//  Copyright © 2018 yuaming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
