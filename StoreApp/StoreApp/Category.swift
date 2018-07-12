edb9dec621b4488f0da26fe0f226b8aedb690d55
Mijin Jeon <mayihelpu27@gmail.com>
2018-07-12 15:10:53 +0900

    Feat: Add Category enum #4 - Save names of json files for handling StoreItems by key(section header)

---
+++ StoreApp/StoreApp/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  StoreApp
+//
+//  Created by YOUTH2 on 2018. 7. 12..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import Foundation
+
+enum Category: String {
+    case main
+    case soup
+    case side
+}
