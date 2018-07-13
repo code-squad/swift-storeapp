e77c5deb73319667bd6ac422d83b2d6ba99977f3
Mijin Jeon <mayihelpu27@gmail.com>
2018-07-13 14:38:42 +0900

    Feat: Add CustomHeaderView(UITableViewCell)

---
+++ StoreApp/StoreApp/View/CustomHeaderView.swift
@@ -0,0 +1,35 @@
+//
+//  CustomHeaderView.swift
+//  StoreApp
+//
+//  Created by YOUTH2 on 2018. 7. 13..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class CustomHeaderView: UITableViewCell {
+
+    @IBOutlet weak var shortLabel: UILabel!
+    @IBOutlet weak var longLabel: UILabel!
+
+    var data: Category! {
+        didSet {
+            self.shortLabel.text = data.description.short
+            self.longLabel.text = data.description.long
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = UIColor.white
+        shortLabel.textColor = UIColor.gray
+        shortLabel.layer.borderColor = UIColor.gray.cgColor
+        shortLabel.layer.borderWidth = 0.5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
