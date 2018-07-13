7fabbbbf30dfbbd3a09544096153dd9560f8b124
Mijin Jeon <mayihelpu27@gmail.com>
2018-07-13 14:15:46 +0900

    Feat: Add Keyword enum

---
+++ StoreApp/StoreApp/Model/Keyword.swift
@@ -0,0 +1,15 @@
+//
+//  Keyword.swift
+//  StoreApp
+//
+//  Created by YOUTH2 on 2018. 7. 13..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import Foundation
+
+enum Keyword: String {
+    case HeaderView
+    case headerView
+    case itemCell
+}
