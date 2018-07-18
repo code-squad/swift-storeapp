8fb64cc3b4a9788b6c1a7e0bcd679eb63bd5664b
Mijin Jeon <mayihelpu27@gmail.com>
2018-07-18 18:58:31 +0900

    Feat: Set delivery, discount badge enums

--- StoreApp/StoreApp/Model/Keyword.swift
+++ StoreApp/StoreApp/Model/Keyword.swift
@@ -14,3 +14,31 @@ enum Keyword: String {
     case itemCell
     case customHeader
 }
+
+enum DiscountBadge: String {
+    case Launching = "론칭특가"
+    case Event = "이벤트특가"
+}
+
+enum DeliveryBadge: String {
+    case Dawn = "새벽배송"
+    case Nation = "전국택배"
+}
+
+extension String {
+    func discountBadge() -> DiscountBadge? {
+        switch self {
+        case "론칭특가" : return .Launching
+        case "이벤트특가" : return .Event
+        default : return nil
+        }
+    }
+
+    func deliveryBadge() -> DeliveryBadge? {
+        switch self {
+        case "새벽배송": return .Dawn
+        case "전국택배": return .Nation
+        default : return nil
+        }
+    }
+}
