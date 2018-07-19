9208ea43b66c9b20d88c3ef24567d653b5bd6a24
YUAMING <miennes3@gmail.com>
2018-07-19 11:17:01 +0900

    #22: ImageLoaderable protocol, extension 개발
    * DetailSectionImage, Thumbnail 중복 코드 제거

---
+++ StoreApp/StoreApp/Models/ImageLoaderable.swift
@@ -0,0 +1,45 @@
+//
+//  ImageLoaderable.swift
+//  StoreApp
+//
+//  Created by yuaming on 19/07/2018.
+//  Copyright © 2018 yuaming. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ImageLoaderable: class {
+  var image: UIImage? { set get }
+}
+
+extension ImageLoaderable {
+  func loadImageData(_ urlString: String?) {
+    guard let urlString = urlString else { return }
+    
+    let dispatchQueue = DispatchQueue.global(qos: .userInitiated)
+    dispatchQueue.async {
+      if let cachedData = ImageCache.fetchData(urlString) {
+        self.image = UIImage(data: cachedData)
+      } else {
+        self.requestImageData(urlString)
+      }
+    }
+  }
+  
+  fileprivate func requestImageData(_ urlString: String) {
+    guard let url = API.shared.makeUrl(urlString) else { return }
+    
+    API.shared.sendRequest(withUrl: url) { resultType in
+      switch resultType {
+      case .success(let data):
+        guard let data = data else { return }
+        ImageCache.store(urlString, content: data)
+        self.image = UIImage(data: data)
+      case .error(let error):
+        print(error.localizedDescription)
+      }
+    }
+  }
+}
+
