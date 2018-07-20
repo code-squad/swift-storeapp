b24d2c913e1779c3213fe304ed32cf7efb5bb307
YUAMING <miennes3@gmail.com>
2018-07-20 13:51:43 +0900

    #29: 네트워크 연결 유무에 따라 테두리 색깔로 구분하기

--- StoreApp/StoreApp/AppDelegate.swift
+++ StoreApp/StoreApp/AppDelegate.swift
@@ -14,11 +14,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
+    NotificationCenter.default.addObserver(self, selector: #selector(canConnect(_:)), name: Notification.Name.isConnected, object: nil)
+    
+    NetworkManager.shared.startObserver()
+    setStatusBarOption()
+    
+    return true
+  }
+  
+  func applicationDidEnterBackground(_ application: UIApplication) {
+    NetworkManager.shared.stopObserver()
+  }
+  
+  @objc fileprivate func canConnect(_ notification: Notification) {
+    guard let userInfo = notification.userInfo,
+      let isConnected = userInfo[Constants.isConnected] as? Bool else { return }
+    
+    drawBorder(isConnected: isConnected)
+  }
+  
+  fileprivate func drawBorder(isConnected: Bool) {
+    let rootView = window?.rootViewController?.view
+    rootView?.layer.borderColor = isConnected ? UIColor.green.cgColor : UIColor.red.cgColor
+    rootView?.layer.borderWidth = 3
+  }
+  
+  fileprivate func setStatusBarOption() {
     if let statusBar = UIApplication.shared.value(forKey: "statusBar") as? UIView {
       statusBar.backgroundColor = UIColor.white
     }
-    
-    return true
   }
 }
 
