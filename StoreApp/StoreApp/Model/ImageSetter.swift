d35fe59746fd4f6e9619992f34cabcf025812f19
Mijin Jeon <mayihelpu27@gmail.com>
2018-07-26 15:15:30 +0900

    Feat: Show itemImage on cell #9, #10 - Add ImageSetter class for downloading and saving images in cache directory

--- StoreApp/StoreApp/Model/ImageSetter.swift
+++ StoreApp/StoreApp/Model/ImageSetter.swift
@@ -9,15 +9,36 @@
 import Foundation
 
 class ImageSetter {
+    static let fileManager = FileManager.default
 
-    class func download(with url: String, handler: @escaping((String) -> Void)) {
-        URLSession.shared.downloadTask(with: URL(string: url)!) { (tmpLocation, response, error) in
-            if let error = error {
-                print("Image download Error log: \(error)\n")
-            }
-            if let response = response as? HTTPURLResponse, response.statusCode == 200, let tmpLocation = tmpLocation {
-                handler(tmpLocation.path)
-            }
-        }.resume()
+    class func download(with url: String, handler: @escaping((Data) -> Void)) {
+        let cacheURL = fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        let imageSavingPath = cacheURL.appendingPathComponent(URL(string: url)!.lastPathComponent)
+
+        if let imageData = existFile(at: imageSavingPath) {
+            handler(imageData)
+        } else {
+            URLSession.shared.downloadTask(with: URL(string: url)!) { (tmpLocation, response, error) in
+                if let error = error {
+                    print("Image download Error log: \(error)\n")
+                }
+                if let response = response as? HTTPURLResponse, response.statusCode == 200, let tmpLocation = tmpLocation {
+                    do {
+                        try fileManager.moveItem(at: tmpLocation, to: imageSavingPath)
+                        if let imageData = try? Data(contentsOf: imageSavingPath) {
+                            handler(imageData)
+                        } else {
+                            print("MOVE Error!")
+                        }
+                    } catch { print("MOVE Error!") }
+                }
+            }.resume()
+        }
+    }
+
+    class func existFile(at imageSavingPath: URL) -> Data? {
+        guard FileManager().fileExists(atPath: imageSavingPath.path) else { return nil }
+            let existData = try? Data(contentsOf: imageSavingPath)
+            return existData
     }
 }
