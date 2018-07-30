ee758472c4a8eaeb205a7fc2e5552504b7c418a2
Mijin Jeon <mayihelpu27@gmail.com>
2018-07-30 19:50:32 +0900

    Feat: Implement ItemViewController #14 - Set properties by DetaillHash model - Adjust layout and modify constraint

--- StoreApp/StoreApp/ItemViewController.swift
+++ StoreApp/StoreApp/ItemViewController.swift
@@ -9,22 +9,65 @@
 import UIKit
 
 class ItemViewController: UIViewController {
-
     @IBOutlet weak var ItemImages: UIScrollView!
-    @IBOutlet weak var titleLabels: UIStackView!
-    @IBOutlet weak var detailLabels: UIStackView!
-    @IBOutlet weak var buyButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabels: UIStackView!
+    @IBOutlet weak var pointLabel: UILabel!
+    @IBOutlet weak var deliveryFee: UILabel!
+    @IBOutlet weak var deliveryInfo: UILabel!
     @IBOutlet weak var detailView: UIScrollView!
-    var itemHashData: DetailItemInfo!
+    @IBOutlet weak var buyButton: UIButton!
+    var itemData: DetailHash! {
+        didSet {
+            self.detailInfo = itemData.detailItemInfo()
+        }
+    }
+    var detailInfo: DetailItemInfo!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        guard itemData != nil else { return }
+        setTitleLabels()
+        setPriceLabel()
+        setPointLabel()
+        setDeliveryInfoLabel()
+        setDeliveryFeeLabel()
+    }
 
+    private func setTitleLabels() {
+        titleLabel.text = itemData.itemTitle
+        descriptionLabel.text = detailInfo.productDescription
+    }
+
+    private func setPriceLabel() {
+        guard let nPriceLabel = priceLabels.arrangedSubviews[0] as? UILabel else {return}
+        guard let sPriceLabel = priceLabels.arrangedSubviews[1] as? UILabel else {return}
+
+        if detailInfo.prices.count > 1 {
+            nPriceLabel.text = detailInfo.prices[0]
+            sPriceLabel.text = detailInfo.prices[1]
+        } else {
+            nPriceLabel.isHidden = true
+            sPriceLabel.text = detailInfo.prices[0]
+        }
+    }
+
+    private func setPointLabel() {
+        pointLabel.text = detailInfo.point
+    }
+
+    private func setDeliveryInfoLabel() {
+        deliveryInfo.numberOfLines = 0
+        deliveryInfo.text = detailInfo.deliveryInfo
+    }
+
+    private func setDeliveryFeeLabel() {
+        deliveryFee.text = detailInfo.deliveryFee
     }
 
     override func didReceiveMemoryWarning() {
