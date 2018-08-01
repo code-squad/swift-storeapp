b4c2b55a2b75916cf9ecf760cd083159a69c487f
Mijin Jeon <mayihelpu27@gmail.com>
2018-08-01 18:27:25 +0900

    Feat: Add IBAction method for order button #18 - First Basic implement for order feature in ItemViewController - Add OrderDelegate protocol - Connect IBAction method with order button

---
+++ StoreApp/StoreApp/Model/OrderDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  OrderDelegate.swift
+//  StoreApp
+//
+//  Created by YOUTH2 on 2018. 8. 1..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import Foundation
+
+protocol OrderDelegate {
+    func order()
+}
