e304b95844f54e2d3027aa39b13554f04e084a2a
Mijin Jeon <mayihelpu27@gmail.com>
2018-08-08 17:56:59 +0900

    Fix: Modify logic on stackview
    - Fix logic implement stackview of badge and prices
    - Add prepareForReuse()

--- StoreApp/StoreApp/View/StoreTableViewCell.swift
+++ StoreApp/StoreApp/View/StoreTableViewCell.swift
@@ -24,13 +24,7 @@ class StoreTableViewCell: UITableViewCell {
             self.itemDescription.text = itemData.description
             self.setPriceLabels(nPrice: itemData.n_price, sPrice: itemData.s_price)
             self.setItemImage(imageURL: self.itemData.image)
-            guard let badges = itemData.badge else {
-                self.badgeStack.isHidden = true
-                return
-            }
-            guard badgeStack.subviews.isEmpty else { return }
-            self.setDiscountBadge(texts: badges)
-            
+            self.setBadge(labels: itemData.badge)
         }
     }
 
@@ -42,6 +36,11 @@ class StoreTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         setItemImageConfig()
+    }
+    
+    override func prepareForReuse() {
+        self.badgeStack.isHidden = false
+        self.prices.arrangedSubviews[0].isHidden = false
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -74,18 +73,36 @@ class StoreTableViewCell: UITableViewCell {
     private func setPriceLabels(nPrice: String?, sPrice: String) {
         guard let nPriceLabel = self.prices.arrangedSubviews[0] as? UILabel else { return }
         guard let sPriceLabel = self.prices.arrangedSubviews[1] as? UILabel else { return }
-        if let normalPrice = nPrice {
-            nPriceLabel.text = normalPrice
-        } else {
+        if let nPrice = nPrice {
+            if !nPrice.isEmpty { // nPrice가 있을때(가격이 두개일때)
+                nPriceLabel.text = nPrice
+                sPriceLabel.text = sPrice
+            } else { // nPrice가 없을때(가격이 한개일때)
+                nPriceLabel.isHidden = true
+            }
+        } else { // nPrice가 없을때(가격이 한개일때)
             nPriceLabel.isHidden = true
+            sPriceLabel.text = sPrice
         }
-        sPriceLabel.text = sPrice
     }
 
     private func setDiscountBadge(texts: [String]) {
         let badges = texts.compactMap{ $0.discountBadge() }
         let badgeLabels = badges.map{ RoundedEdgeLabel().custom(withText: $0) }
         self.badgeStack.addArrangedSubviews(badgeLabels)
+    }
+
+
+    private func setBadge(labels: [String]?) {
+        guard let badges = itemData.badge else {
+            self.badgeStack.isHidden = true
+            return
+        }
+        if badges.count == 0 {
+            self.badgeStack.isHidden = true
+        }
+        guard badgeStack.subviews.isEmpty else { return }
+        self.setDiscountBadge(texts: badges)
     }
 
 }
