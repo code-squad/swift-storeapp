e26860c89eec15bf7c52f388653da2b14ffce7a9
Moon <myssun0325@gmail.com>
2018-08-13 12:27:34 +0900

    Create StoreApp Project

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by moon on 2018. 8. 13..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
