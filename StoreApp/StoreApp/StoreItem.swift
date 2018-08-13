a7c0ebe17f1102ac378ee9e2e790e5dc8a2f5782
Moon <myssun0325@gmail.com>
2018-08-13 16:34:54 +0900

    Create StoreItem Data Object

---
+++ StoreApp/StoreApp/StoreItem.swift
@@ -0,0 +1,33 @@
+//
+//  StoreItem.swift
+//  StoreApp
+//
+//  Created by moon on 2018. 8. 13..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import Foundation
+
+struct StoreItem: Decodable {
+    let detailHash: String
+    let imageURL: String
+    let alt: String
+    let deliveryType: [String]
+    let title: String
+    let description: String
+    let normalPrice: String?
+    let salePrice: String
+    let badge: [String]?
+    
+    enum CodingKeys: String, CodingKey {
+        case detailHash = "detail_hash"
+        case imageURL = "image"
+        case alt
+        case deliveryType = "delivery_type"
+        case title
+        case description
+        case normalPrice = "n_price"
+        case salePrice = "s_price"
+        case badge
+    }
+}
