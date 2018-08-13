7b54045afc1ac3d4e90367198757b9887a46fc9f
Moon <myssun0325@gmail.com>
2018-08-14 02:46:55 +0900

    Grouping files

--- StoreApp/StoreApp/View/StoreItemTableViewCell.swift
+++ StoreApp/StoreApp/View/StoreItemTableViewCell.swift
@@ -12,8 +12,12 @@ class StoreItemTableViewCell: UITableViewCell {
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var normalPriceLabel: UILabel!
-    @IBOutlet weak var salePriceLabel: UILabel!
-    @IBOutlet weak var firstBadgeLabel: UILabel!
-    @IBOutlet weak var secondBadgeLabel: UILabel!
+    @IBOutlet weak var normalPriceLabel: PriceLabel!
+    @IBOutlet weak var salePriceLabel: PriceLabel!
+    @IBOutlet weak var badgeStackView: UIStackView!
+    
+    override func prepareForReuse() {
+        normalPriceLabel.text = nil
+        badgeStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+    }
 }
