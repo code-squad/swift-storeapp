cfc59ba9c2754f42e6c99459d1d2881f92844921
Moon <myssun0325@gmail.com>
2018-08-13 17:09:10 +0900

    Add StoreItemTableViewCell (Custom Cell Class)

---
+++ StoreApp/StoreApp/StoreItemTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  StoreItemTableViewCell.swift
+//  StoreApp
+//
+//  Created by moon on 2018. 8. 13..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import UIKit
+
+class StoreItemTableViewCell: UITableViewCell {
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var normalPriceLabel: UILabel!
+    @IBOutlet weak var salePriceLabel: UILabel!
+    @IBOutlet weak var firstBadgeLabel: UILabel!
+    @IBOutlet weak var secondBadgeLabel: UILabel!
+}
