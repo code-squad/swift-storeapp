3d7f8a2e8b5ab1fcac1d74d0865315e704b75a85
cocomilktea <coco_milktea@naver.com>
2018-08-14 15:40:06 +0900

    storyboard Autolayout 적용

--- StoreApp/StoreApp/View/ItemCell.swift
+++ StoreApp/StoreApp/View/ItemCell.swift
@@ -15,17 +15,33 @@ class ItemCell: UITableViewCell {
     @IBOutlet weak var itemDescription: UILabel!
     @IBOutlet weak var nPrice: UILabel!
     @IBOutlet weak var sPrice: UILabel!
-    @IBOutlet weak var eventBadge: UILabel!
+    @IBOutlet weak var badgeStackView: UIStackView!
     
     func reset(_ item: StoreItem) {
         itemTitle.text = item.title
         itemDescription.text = item.description
-        nPrice.text = item.nPrice
         sPrice.text = item.sPrice
-        eventBadge.text = item.badge?.reduce("", +)
-        if item.badge != nil {
-            eventBadge.backgroundColor = UIColor.purple
+        
+        if let price = item.nPrice {
+            nPrice.text = price
+            nPrice.isHidden = false
+        } else {
+            nPrice.isHidden = true
         }
+        
+        guard let badges = item.badge else { return }
+
+        for badge in badges {
+            let newBadge = UILabel(frame: .zero)
+            newBadge.text = badge
+            newBadge.backgroundColor = .purple
+            newBadge.textColor = .white
+            badgeStackView.addArrangedSubview(newBadge)
+        }
+    }
+    
+    override func prepareForReuse() {
+        self.badgeStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
     }
     
 }
