5dada4de312bc7fdd8994966f645180c2381978a
Moon <myssun0325@gmail.com>
2018-08-14 12:27:15 +0900

    Refactor: move setup cell method
    from ViewController to StoreItemTableViewCell

--- StoreApp/StoreApp/View/StoreItemTableViewCell.swift
+++ StoreApp/StoreApp/View/StoreItemTableViewCell.swift
@@ -12,13 +12,35 @@ class StoreItemTableViewCell: UITableViewCell {
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var normalPriceLabel: PriceLabel!
-    @IBOutlet weak var salePriceLabel: PriceLabel!
+    @IBOutlet weak var priceStackView: UIStackView!
     @IBOutlet weak var badgeStackView: UIStackView!
     
     override func prepareForReuse() {
-        normalPriceLabel.isHidden = false
-        normalPriceLabel.text = nil
+        priceStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
         badgeStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
     }
+    
+    func set(storeItem: StoreItem) {
+        titleLabel.text = storeItem.title
+        descriptionLabel.text = storeItem.description
+        setNormalPrice(storeItem.normalPrice)
+        setSalePrice(storeItem.salePrice)
+        setBadges(storeItem.badges)
+    }
+        
+    private func setNormalPrice(_ normalPrice: String?) {
+        guard let normalPrice = normalPrice else { return }
+        priceStackView.addArrangedSubview(PriceLabel(frame: .zero, normalPrice: normalPrice))
+    }
+    
+    private func setSalePrice(_ salePrice: String) {
+        let salePriceLabel = PriceLabel(frame: .zero, salePrice: salePrice)
+        priceStackView.addArrangedSubview(salePriceLabel)
+    }
+    
+    private func setBadges(_ badges: [String]?) {
+        guard let badges = badges else { return }
+        badges.forEach { badgeStackView.addArrangedSubview(BadgeLabel(frame: .zero, text: $0)) }
+    }
+    
 }
