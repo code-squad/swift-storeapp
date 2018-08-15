57304e595d625cf08fc765dd43cbbd83ea797031
Moon <myssun0325@gmail.com>
2018-08-15 21:05:03 +0900

    Apply SectionInfo

--- StoreApp/StoreApp/Model/SectionInfo.swift
+++ StoreApp/StoreApp/Model/SectionInfo.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct SectionInfo {
+    private var storeItemLists: [StoreItemList]
+    
+    init(categories: [FoodCategory]) {
+        self.storeItemLists = categories.compactMap { StoreItemList($0) }
+    }
+    
+    var count: Int {
+        return storeItemLists.count
+    }
+    
+    subscript(index: Int) -> StoreItemList {
+        return storeItemLists[index]
+    }
+}
