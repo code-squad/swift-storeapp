07d6f4b643872bbec11b352e6667d5cce8318baa
Moon <myssun0325@gmail.com>
2018-08-15 19:08:39 +0900

    Fix FoodCategory file search bug

--- StoreApp/StoreApp/Model/FoodCategory.swift
+++ StoreApp/StoreApp/Model/FoodCategory.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+enum FoodCategory: String {
+    case main
+    case soup
+    case side
+    
+    var title: String {
+        switch self {
+        case .main: return "메인반찬"
+        case .soup: return "국∙찌게"
+        case .side: return "밑반찬"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .main: return "한그릇 뚝딱 메인 요리"
+        case .soup: return "김이 모락모락 국.찌게"
+        case .side: return "언제 먹어도 든든한 밑반찬"
+        }
+    }
+}
