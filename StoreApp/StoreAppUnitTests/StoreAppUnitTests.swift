bf7cc205595ba3fb2946ef719a9e2037ac76eb8f
Moon <myssun0325@gmail.com>
2018-08-15 17:03:36 +0900

    Unit Test StoreItemList Initializer from all json files

---
+++ StoreApp/StoreAppUnitTests/StoreAppUnitTests.swift
@@ -0,0 +1,37 @@
+//
+//  StoreAppUnitTests.swift
+//  StoreAppUnitTests
+//
+//  Created by moon on 2018. 8. 15..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import XCTest
+
+class StoreAppUnitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testInitStorItemListFrom_main_success() {
+        let storeItemList = StoreItemList(jsonFileName: "main")
+        XCTAssertNotNil(storeItemList)
+    }
+    
+    func testInitStoreItemListFrom_side_success() {
+        let storeItemList = StoreItemList(jsonFileName: "side")
+        XCTAssertNotNil(storeItemList)
+    }
+    
+    func testInitStoreItemListFrom_soup_success() {
+        let storeItemList = StoreItemList(jsonFileName: "soup")
+        XCTAssertNotNil(storeItemList)
+    }
+}
