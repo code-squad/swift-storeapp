a0c91f23ccfc0b42c929a1872ae9cc806004b3b9
cocomilktea <coco_milktea@naver.com>
2018-08-16 07:05:42 +0900

    Custom HeaderCell Table View 적용

--- StoreApp/StoreApp/ViewController.swift
+++ StoreApp/StoreApp/ViewController.swift
@@ -12,11 +12,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var storeItems = StoreItems()
+    let headers = Headers().getHeaders()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storeItems.extractJSONData()
         tableView.dataSource = self
         tableView.delegate = self
         tableView.rowHeight = 110
@@ -31,23 +30,51 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return storeItems.itemCount
+        return headers[section].storeItems.itemCount
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let itemCell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath) as? ItemCell else {
+        let cellIdentifier = "itemCell"
+        guard let itemCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ItemCell else {
             return UITableViewCell()
         }
-        itemCell.reset(storeItems[indexPath.row])
+        itemCell.reset(headers[indexPath.section].storeItems[indexPath.row])
         return itemCell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return headers.count
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let cellIdentifier = "headerCell"
+        guard let sectionHeader = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? HeaderCell else { return nil }
+        sectionHeader.titleLabel.text = headers[section].title
+        sectionHeader.descriptionLabel.text = headers[section].subTitle
+        return sectionHeader
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let height: CGFloat = 90
+        return height
     }
     
 }
 
 extension ViewController: UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let scrollHeight = tableView.rowHeight
         
+        if scrollView.contentOffset.y <= scrollHeight {
+            if scrollView.contentOffset.y >= 0 {
+                scrollView.contentInset = UIEdgeInsetsMake(-scrollView.contentOffset.y, 0, 0, 0)
+            }
+        } else if scrollView.contentOffset.y >= scrollHeight {
+            scrollView.contentInset = UIEdgeInsetsMake(-scrollHeight, 0, 0, 0)
+        }
     }
+    
 }
+
 
