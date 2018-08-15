b6165e48461aad2889fff5c7081146beaf569e17
cocomilktea <coco_milktea@naver.com>
2018-08-15 16:31:18 +0900

    SectionHeader 구현

---
+++ StoreApp/StoreApp/Model/SectionHeader.swift
@@ -0,0 +1,54 @@
+//
+//  SectionHeader.swift
+//  StoreApp
+//
+//  Created by 김수현 on 2018. 8. 15..
+//  Copyright © 2018년 김수현. All rights reserved.
+//
+
+import Foundation
+
+struct SectionHeader {
+    private let title: String
+    private let subTitle: String
+    private let storeItems: StoreItems
+    
+    init(_ header: Header) {
+        self.title = header.title
+        self.subTitle = header.subTitle
+        self.storeItems = StoreItems()
+    }
+    
+    enum Header {
+        case main
+        case soup
+        case side
+        
+        var fileName: String {
+            switch self {
+            case .main: return "main"
+            case .soup: return "soup"
+            case .side: return "side"
+            }
+        }
+        
+        var title: String {
+            switch self {
+            case .main: return "메인반찬"
+            case .soup: return "국.찌게"
+            case .side: return "밑반찬"
+            }
+        }
+        
+        var subTitle: String {
+            switch self {
+            case .main: return "한그릇 뚝딱 메인 요리"
+            case .soup: return "김이 모락모락 국.찌게"
+            case .side: return "언제 먹어도 든든한 밑반찬"
+            }
+        }
+    }
+    
+}
+
+
