78c7b07728b4ce219572cf05cc6e8bd5cc52cd39
cocomilktea <coco_milktea@naver.com>
2018-08-15 16:47:14 +0900

    HeaderCell 클래스 구현

---
+++ StoreApp/StoreApp/View/HeaderCell.swift
@@ -0,0 +1,16 @@
+//
+//  HeaderCell.swift
+//  StoreApp
+//
+//  Created by 김수현 on 2018. 8. 15..
+//  Copyright © 2018년 김수현. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
