2554e4d7caff11c6e83ccbc5035a61bb8075a90f
Moon <myssun0325@gmail.com>
2018-08-26 02:20:40 +0900

    Implement push navigation and hide navigationbar

---
+++ StoreApp/StoreApp/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StoreItemDetailViewController.swift
+//  StoreApp
+//
+//  Created by moon on 2018. 8. 26..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
