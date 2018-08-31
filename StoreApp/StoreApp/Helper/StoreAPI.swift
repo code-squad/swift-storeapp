992d144f70a914698d6f876ce3a17abbe6d031a8
Moon <myssun0325@gmail.com>
2018-08-31 11:09:05 +0900

    피드백반영: Hook Information 감추기

--- StoreApp/StoreApp/Helper/StoreAPI.swift
+++ StoreApp/StoreApp/Helper/StoreAPI.swift
@@ -13,6 +13,7 @@ struct StoreAPI {
     private static let portNumber = 8080
     private static let path = "/woowa"
     private static let successCode = 200
+    private static let hookInfoPlistName = "HookInfo"
     
     static func makeURL(from lastComponent: String) -> URL? {
         guard var components = URLComponents(string: baseURL) else { return nil }
@@ -51,20 +52,46 @@ struct StoreAPI {
     }
     
     static func postHook(who: String, price: String, menu: String) {
-        guard let url = URL(string: PostInfo.hookURL) else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = PostInfo.httpMethod
-        request.addValue(PostInfo.headerValue, forHTTPHeaderField: PostInfo.httpHeaderField)
+        guard let hookInfo = makeHookInfo() else { return }
+        guard var request = makeRequest(from: hookInfo) else { return }
         let parameters = ["text": "\(who)이 \(menu)를 주문했습니다. 가격: \(price)"]
         guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
         request.httpBody = httpBody
         URLSession.shared.dataTask(with: request).resume()
     }
     
-    fileprivate struct PostInfo {
-        static let hookURL = "https://hooks.slack.com/services/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"
-        static let httpMethod = "POST"
-        static let headerValue = "application/json"
-        static let httpHeaderField = "Content-Type"
+    private static func makeRequest(from hookInfo: HookInfo) -> URLRequest? {
+        guard let url = URL(string: hookInfo.hookURL) else { return nil }
+        var request = URLRequest(url: url)
+        request.httpMethod = hookInfo.httpMethod
+        request.addValue(hookInfo.headerValue, forHTTPHeaderField: hookInfo.httpHeaderField)
+        return request
+    }
+    
+    private static func makeHookInfo() -> HookInfo? {
+        guard let path = Bundle.main.path(forResource: hookInfoPlistName, ofType: "plist") else { return nil }
+        let plistURL = URL(fileURLWithPath: path)
+        guard let plistData = try? Data(contentsOf: plistURL) else { return nil }
+        return decodeHookInfoData(plistData)
+    }
+    
+    private static func decodeHookInfoData(_ data: Data) -> HookInfo? {
+        let decoder = PropertyListDecoder()
+        guard let hookInfo = try? decoder.decode(HookInfo.self, from: data) else { return nil }
+        return hookInfo
+    }
+}
+
+struct HookInfo: Decodable {
+    var hookURL: String
+    var httpMethod: String
+    var headerValue: String
+    var httpHeaderField: String
+    
+    enum CodingKeys: String, CodingKey {
+        case hookURL
+        case httpMethod
+        case headerValue
+        case httpHeaderField
     }
 }
