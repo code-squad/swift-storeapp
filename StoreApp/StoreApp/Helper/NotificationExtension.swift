0d4ee73d303d0a3c18e7d4df37c5e5ee70da62b8
Moon <myssun0325@gmail.com>
2018-09-10 05:12:03 +0900

    Reacahbility Listener 객체 추가

--- StoreApp/StoreApp/Helper/NotificationExtension.swift
+++ StoreApp/StoreApp/Helper/NotificationExtension.swift
@@ -10,4 +10,5 @@ import Foundation
 
 extension Notification.Name {
     static let didStoreItemsSet = Notification.Name("didStoreItemsSet")
+    static let connectionDidChanged = Notification.Name("connectionDidChanged")
 }
