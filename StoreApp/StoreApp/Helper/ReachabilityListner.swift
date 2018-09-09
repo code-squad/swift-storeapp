398a6aa4f4e8300cc5b20c47695b8402121e0032
Moon <myssun0325@gmail.com>
2018-09-10 05:12:03 +0900

    Reacahbility Listener 객체 추가

---
+++ StoreApp/StoreApp/Helper/ReachabilityListner.swift
@@ -0,0 +1,31 @@
+//
+//  ReachabilityListner.swift
+//  StoreApp
+//
+//  Created by moon on 2018. 9. 10..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ReachabilityListener {
+    private let networkReachbilityManager: NetworkReachabilityManager
+    
+    init?(host: String) {
+        guard let networkReachbilityManager = NetworkReachabilityManager(host: host) else { return nil }
+        self.networkReachbilityManager = networkReachbilityManager
+    }
+    
+    func startListening() {
+        networkReachbilityManager.listener = { _ in
+            NotificationCenter.default.post(name: .connectionDidChanged, object: self,
+                                            userInfo: ["connectionInfo": self.networkReachbilityManager.isReachable])
+        }
+        networkReachbilityManager.startListening()
+    }
+    
+    func stopListening() {
+        networkReachbilityManager.stopListening()
+    }
+}
