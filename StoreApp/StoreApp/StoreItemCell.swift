0c8f7608f3bc860e6cb11af150adc14901c170b1
oingbong <oingbong@gmail.com>
2018-12-06 17:25:31 +0900

    Add Custom Cell view & Connect StoreItemCell

---
+++ StoreApp/StoreApp/StoreItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  StoreItemCell.swift
+//  
+//
+//  Created by oingbong on 06/12/2018.
+//
+
+import UIKit
+
+class StoreItemCell: UITableViewCell {
+    @IBOutlet weak var menuImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var badgeLabel: UILabel!
+    
+}
