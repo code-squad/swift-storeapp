480b3ed3d40ed98a88050106bdc0012880b07085
oingbong <oingbong@gmail.com>
2018-12-06 17:01:08 +0900

    init .gitignore & swiftLint

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by oingbong on 06/12/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
