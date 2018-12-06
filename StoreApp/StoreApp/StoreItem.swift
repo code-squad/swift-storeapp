1c5676533f327d595d7d52a4a3e113ec7c96c699
oingbong <oingbong@gmail.com>
2018-12-06 17:10:48 +0900

    Add StoreItems & StoreItem

---
+++ StoreApp/StoreApp/StoreItem.swift
@@ -0,0 +1,19 @@
+//
+//  StoreItem.swift
+//  StoreApp
+//
+//  Created by oingbong on 06/12/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import Foundation
+
+struct StoreItem: Decodable {
+    var detail_hash: String
+    var image: String
+    var alt: String
+    var delivery_type: String
+    var title: String
+    var description: String
+    var s_price: String
+}
