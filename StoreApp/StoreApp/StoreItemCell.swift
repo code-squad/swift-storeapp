a39fd360cc4ac03633a49f2757ba15259e3ab7f2
oingbong <oingbong@gmail.com>
2018-12-07 16:33:03 +0900

    Edit duplicate func & Renamed label

--- StoreApp/StoreApp/StoreItemCell.swift
+++ StoreApp/StoreApp/StoreItemCell.swift
@@ -11,58 +11,54 @@ class StoreItemCell: UITableViewCell {
     @IBOutlet weak var menuImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descLabel: UILabel!
-    @IBOutlet weak var nPrice: UILabel!
-    @IBOutlet weak var sPrice: UILabel!
-    @IBOutlet weak var badgeFirst: UILabel!
-    @IBOutlet weak var badgeSecond: UILabel!
+    @IBOutlet weak var nPriceLabel: UILabel!
+    @IBOutlet weak var sPriceLabel: UILabel!
+    @IBOutlet weak var badgeFirstLabel: UILabel!
+    @IBOutlet weak var badgeSecondLabel: UILabel!
     @IBOutlet weak var priceStackView: UIStackView!
     @IBOutlet weak var badgeStackView: UIStackView!
     
     func configure(with item: StoreItem) {
-        hiddenStackView()
+        hiddenStackViewItem()
         
         // title
-        let titleAttributedOption = AttributeOption.title.style
-        let titleAttributedString = NSMutableAttributedString(string: item.title, attributes: titleAttributedOption)
-        titleLabel.attributedText = titleAttributedString
+        titleLabel.attributedText = decorateString(option: .title, string: item.title)
         
         // desc
-        let descAttributedOption = AttributeOption.desc.style
-        let descAttributedString = NSMutableAttributedString(string: item.description, attributes: descAttributedOption)
-        descLabel.attributedText = descAttributedString
+        descLabel.attributedText = decorateString(option: .desc, string: item.description)
         
         // nPrice
         if let price = item.n_price {
             priceStackView.arrangedSubviews[0].isHidden = false
-            let attributedOption = AttributeOption.nPrice.style
-            let attributedString = NSMutableAttributedString(string: price, attributes: attributedOption)
-            nPrice.attributedText = attributedString
+            nPriceLabel.attributedText = decorateString(option: .nPrice, string: price)
         }
         
         // sPrice
         priceStackView.arrangedSubviews[1].isHidden = false
-        let attributedOption = AttributeOption.sPrice.style
-        let attributedString = NSMutableAttributedString(string: item.s_price, attributes: attributedOption)
-        sPrice.attributedText = attributedString
+        sPriceLabel.attributedText = decorateString(option: .sPrice, string: item.s_price)
         
         // Badge
         if let badgeList = item.badge, badgeList.count > 0 {
             for index in 0..<badgeList.count {
                 badgeStackView.arrangedSubviews[index].isHidden = false
-                let attributedOption = AttributeOption.badge.style
-                let attributedString = NSMutableAttributedString(string: badgeList[index], attributes: attributedOption)
                 guard let badgeLabel = badgeStackView.arrangedSubviews[index] as? UILabel else { continue }
-                badgeLabel.attributedText = attributedString
+                badgeLabel.attributedText = decorateString(option: .badge, string: badgeList[index])
             }
         }
     }
     
-    private func hiddenStackView() {
+    private func hiddenStackViewItem() {
         for subview in priceStackView.arrangedSubviews {
             subview.isHidden = true
         }
         for subview in badgeStackView.arrangedSubviews {
             subview.isHidden = true
         }
+    }
+    
+    private func decorateString(option: AttributeOption, string: String) -> NSMutableAttributedString {
+        let attributedOption = option.style
+        let attributedString = NSMutableAttributedString(string: string, attributes: attributedOption)
+        return attributedString
     }
 }
