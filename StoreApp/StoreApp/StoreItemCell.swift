54a9b231f39af3c622852622eae643283995e456
oingbong <oingbong@gmail.com>
2018-12-07 12:05:42 +0900

    Add data in TableCell

--- StoreApp/StoreApp/StoreItemCell.swift
+++ StoreApp/StoreApp/StoreItemCell.swift
@@ -14,4 +14,19 @@ class StoreItemCell: UITableViewCell {
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var badgeLabel: UILabel!
     
+    func configure(with item: StoreItem) {
+        titleLabel.text = item.title
+        descLabel.text = item.description
+        priceLabel.text = item.s_price
+        if let nPrice = item.n_price {
+            priceLabel.text = nPrice + "   " + item.s_price
+        }
+        var badge = ""
+        if let elements = item.badge {
+            for element in elements {
+                badge += element
+            }
+        }
+        badgeLabel.text = badge
+    }
 }
--- StoreApp/StoreApp/ViewController.swift
+++ StoreApp/StoreApp/ViewController.swift
@@ -9,13 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     var storeItems: StoreItems?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
         guard let jsonData = Parser.json(fileName: "main") else { return }
         guard let items = Parser.storeItems(from: jsonData) else { return }
         storeItems = StoreItems(data: items)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return storeItems?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StoreItemCell", for: indexPath) as! StoreItemCell
+        guard let items = storeItems?[indexPath.row] else { return UITableViewCell(frame: CGRect(origin: .zero, size: .zero))}
+        cell.configure(with: items)
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
