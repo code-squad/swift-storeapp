c35fd739e9085ea6102e47acccdf5a2f2a94e38c
oingbong <oingbong@gmail.com>
2018-12-11 14:26:08 +0900

    Add StoreItemGroup Model & Using for Section Group with soup, side json

---
+++ StoreApp/StoreApp/Model/StoreItemGroup.swift
@@ -0,0 +1,14 @@
+//
+//  StoreItemGroup.swift
+//  StoreApp
+//
+//  Created by oingbong on 11/12/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import Foundation
+
+struct StoreItemGroup {
+    var sectionName: String
+    var sectionObjects: [StoreItem]
+}
