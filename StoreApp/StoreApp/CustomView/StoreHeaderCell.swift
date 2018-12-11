d8f892e21b5d9b3361a8f23096cad595fe03a6e6
oingbong <oingbong@gmail.com>
2018-12-11 17:10:28 +0900

    Add TopicAttribute & Using horizontally in container & min width in autolayout

--- StoreApp/StoreApp/CustomView/StoreHeaderCell.swift
+++ StoreApp/StoreApp/CustomView/StoreHeaderCell.swift
@@ -12,8 +12,16 @@ class StoreHeaderCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descLabel: UILabel!
     
-    func configure(from topic: Topic) {
-        self.nameLabel.text = topic.koreanName
-        self.descLabel.text = topic.desc
+    func configure(from item: Topic) {
+        self.nameLabel.attributedText = decorateString(option: TopicAttribute.name, string: item.koreanName)
+        self.nameLabel.layer.borderColor = UIColor.gray.cgColor
+        self.nameLabel.layer.borderWidth = 1
+        self.descLabel.attributedText = decorateString(option: TopicAttribute.desc, string: item.desc)
+    }
+    
+    func decorateString(option: Attributable, string: String) -> NSMutableAttributedString {
+        let attributedOption = option.style
+        let attributedString = NSMutableAttributedString(string: string, attributes: attributedOption)
+        return attributedString
     }
 }
