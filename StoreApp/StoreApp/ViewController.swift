8670d8746fe6c3771acd8c9cbb667c4809d7ccfd
oingbong <oingbong@gmail.com>
2018-12-11 14:26:08 +0900

    Add StoreItemGroup Model & Using for Section Group with soup, side json

--- StoreApp/StoreApp/ViewController.swift
+++ StoreApp/StoreApp/ViewController.swift
@@ -10,26 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    var storeItems: StoreItems?
+    var storeItemGorup = [StoreItemGroup]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        guard let jsonData = Parser.json(fileName: "main") else { return }
-        guard let items = Parser.storeItems(from: jsonData) else { return }
-        storeItems = StoreItems(data: items)
+        appendItem()
+    }
+    
+    private func appendItem() {
+        let topics = ["main", "soup", "side"]
+        for topic in topics {
+            guard let mainItems = parse(topic: topic) else { continue }
+            storeItemGorup.append(StoreItemGroup(sectionName: topic, sectionObjects: mainItems))
+        }
+    }
+    
+    private func parse(topic: String) -> [StoreItem]? {
+        guard let jsonData = Parser.json(fileName: topic) else { return nil }
+        guard let items = Parser.storeItems(from: jsonData) else { return nil }
+        return items
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return storeItems?.count ?? 0
+        return storeItemGorup[section].sectionObjects.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "StoreItemCell", for: indexPath) as! StoreItemCell
-        guard let items = storeItems?[indexPath.row] else { return UITableViewCell(frame: CGRect(origin: .zero, size: .zero))}
+        let items = storeItemGorup[indexPath.section].sectionObjects[indexPath.row]
         cell.configure(from: items)
         return cell
     }
@@ -38,5 +50,15 @@ extension ViewController: UITableViewDataSource {
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
+    }
+}
+
+extension ViewController {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return storeItemGorup.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return storeItemGorup[section].sectionName
     }
 }
