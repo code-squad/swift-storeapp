81ba798f907ad32c14bc4d632c4a024ce9ccd7b3
oingbong <oingbong@gmail.com>
2018-12-13 16:31:29 +0900

    Edit ATS configure & Using URLSession for json data

--- StoreApp/StoreApp/Model/Store.swift
+++ StoreApp/StoreApp/Model/Store.swift
@@ -8,8 +8,13 @@
 
 import Foundation
 
-struct Store {
-    private var storeItemGroup = [StoreItemGroup]()
+class Store {
+    private var storeItemGroup = [StoreItemGroup]() {
+        didSet {
+            let key = Notification.Name("updateItem")
+            NotificationCenter.default.post(name: key, object: nil)
+        }
+    }
     private var topic = [Topic]()
     
     var storeItemGroupCount: Int {
@@ -21,7 +26,7 @@ struct Store {
         appendItem()
     }
     
-    private mutating func appendTopic() {
+    private func appendTopic() {
         let main = Topic(englihsName: "main", koreanName: "메인반찬", desc: "한그릇 뚝딱 메인 요리")
         let soup = Topic(englihsName: "soup", koreanName: "국.찌게", desc: "김이 모락모락 국.찌게")
         let side = Topic(englihsName: "side", koreanName: "밑반찬", desc: "언제 먹어도 든든한 밑반찬")
@@ -30,17 +35,25 @@ struct Store {
         topic.append(side)
     }
     
-    private mutating func appendItem() {
+    private func appendItem() {
         for item in topic {
-            guard let mainItems = parse(topic: item.englihsName) else { continue }
-            storeItemGroup.append(StoreItemGroup(sectionName: item.englihsName, sectionObjects: mainItems))
+            parseUrl(topic: item.englihsName)
         }
     }
     
+    // for file
     private func parse(topic: String) -> [StoreItem]? {
         guard let jsonData = Parser.json(fileName: topic) else { return nil }
         guard let items = Parser.storeItems(from: jsonData) else { return nil }
         return items
+    }
+    
+    // for url
+    private func parseUrl(topic: String) {
+        Parser.jsonUrl(fileName: topic) { (storeItems) in
+            guard let items = storeItems else { return }
+            self.storeItemGroup.append(StoreItemGroup(sectionName: topic, sectionObjects: items))
+        }
     }
     
     subscript(index: Int) -> StoreItemGroup {
