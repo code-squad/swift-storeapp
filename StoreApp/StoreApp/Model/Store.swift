354c63a68d96665fce31540c6797d4adbc246edc
oingbong <oingbong@gmail.com>
2018-12-18 20:01:51 +0900

    Edit imageDownload logic & Edit Topic type: struct to enum & Edit imageDownload param type : StoreItem to URL

--- StoreApp/StoreApp/Model/Store.swift
+++ StoreApp/StoreApp/Model/Store.swift
@@ -9,13 +9,9 @@
 import Foundation
 
 class Store {
-    private var topic = [Topic]()
     private var storeItemGroup = [StoreItemGroup]() {
         didSet {
             NotificationCenter.default.post(name: NotificationKey.updateItem, object: nil)
-            if topic.count == storeItemGroup.count {
-                imageUrl()
-            }
         }
     }
     
@@ -23,26 +19,13 @@ class Store {
         return storeItemGroup.count
     }
     
-    init() {
-        appendTopic()
-    }
-    
-    private func appendTopic() {
-        let main = Topic(englihsName: "main", koreanName: "메인반찬", desc: "한그릇 뚝딱 메인 요리")
-        let soup = Topic(englihsName: "soup", koreanName: "국.찌게", desc: "김이 모락모락 국.찌게")
-        let side = Topic(englihsName: "side", koreanName: "밑반찬", desc: "언제 먹어도 든든한 밑반찬")
-        topic.append(main)
-        topic.append(soup)
-        topic.append(side)
-    }
-    
     func appendItem() {
-        for item in topic {
-            parseUrl(topic: item.englihsName)
+        for item in Topic.allCases {
+            parseUrl(topic: item)
         }
     }
     
-    // for file
+    // MARK: for file
     private func parse(topic: String) -> [StoreItem]? {
         guard let jsonData = Parser.json(fileName: topic), let items: [StoreItem] = Parser.decode(from: jsonData) else {
             NotificationCenter.default.post(name: NotificationKey.error, object: nil)
@@ -51,37 +34,34 @@ class Store {
         return items
     }
     
-    // for url
-    private func parseUrl(topic: String) {
-        Parser.jsonUrl(fileName: topic) { (storeItems) in
+    // MARK: for url
+    private func parseUrl(topic: Topic) {
+        Parser.jsonUrl(fileName: topic.englihsName) { (storeItems) in
             guard let items = storeItems, let storeItems: [StoreItem] = Parser.decode(from: items) else {
                 NotificationCenter.default.post(name: NotificationKey.error, object: nil)
                 return
             }
-            self.storeItemGroup.append(StoreItemGroup(sectionName: topic, sectionObjects: storeItems))
+            let storeItemGroup = StoreItemGroup(sectionName: topic.englihsName, sectionObjects: storeItems)
+            self.storeItemGroup.append(storeItemGroup)
+            self.imageUrl(at: topic.rawValue, with: storeItemGroup)
         }
     }
     
-    // for image
-    private func imageUrl() {
-        for groupIndex in 0..<storeItemGroup.count {
-            for index in 0..<storeItemGroup[groupIndex].sectionObjects.count {
-                DispatchQueue.global().async {
-                    Parser.imageDownLoad(with: self.storeItemGroup[groupIndex].sectionObjects[index], handler: { (isSuccess) in
-                        if isSuccess {
-                            NotificationCenter.default.post(name: NotificationKey.updateItemCell, object: nil, userInfo: ["section": groupIndex, "row": index])
-                        }
-                    })
-                }
+    // MARK: for image
+    private func imageUrl(at sectionIndex: Int, with storeItemGroup: StoreItemGroup) {
+        for index in 0..<storeItemGroup.sectionObjects.count {
+            guard let url = URL(string: storeItemGroup.sectionObjects[index].image) else { return }
+            DispatchQueue.global().async {
+                Parser.imageDownLoad(with: url, handler: { (isSuccess) in
+                    if isSuccess {
+                        NotificationCenter.default.post(name: NotificationKey.updateItemCell, object: nil, userInfo: ["section": sectionIndex, "row": index])
+                    }
+                })
             }
         }
     }
-    
+
     subscript(index: Int) -> StoreItemGroup {
         return storeItemGroup[index]
-    }
-    
-    subscript(index: Int) -> Topic {
-        return topic[index]
     }
 }
--- StoreApp/StoreApp/Model/Topic.swift
+++ StoreApp/StoreApp/Model/Topic.swift
@@ -8,8 +8,32 @@
 
 import Foundation
 
-struct Topic {
-    var englihsName: String
-    var koreanName: String
-    var desc: String
+enum Topic: Int, CaseIterable {
+    case main = 0
+    case soup
+    case side
+
+    var englihsName: String {
+        switch self {
+        case .main: return "main"
+        case .soup: return "soup"
+        case .side: return "side"
+        }
+    }
+
+    var koreanName: String {
+        switch self {
+        case .main: return "메인반찬"
+        case .soup: return "국.찌게"
+        case .side: return "밑반찬"
+        }
+    }
+
+    var description: String {
+        switch self {
+        case .main: return "한그릇 뚝딱 메인 요리"
+        case .soup: return "김이 모락모락 국.찌게"
+        case .side: return "언제 먹어도 든든한 밑반찬"
+        }
+    }
 }
