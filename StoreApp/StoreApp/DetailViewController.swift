38e6cd8b68dc47b435336ca828c17ce02e1c24c9
oingbong <oingbong@gmail.com>
2018-12-20 20:53:14 +0900

    Add Detail Model and Controller

---
+++ StoreApp/StoreApp/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  StoreApp
+//
+//  Created by oingbong on 20/12/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ StoreApp/StoreApp/Model/DetailHashData.swift
@@ -0,0 +1,14 @@
+//
+//  DetailData.swift
+//  StoreApp
+//
+//  Created by oingbong on 20/12/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import Foundation
+
+struct DetailHashData: Decodable {
+    var hash: String
+    var data: DetailItem
+}
