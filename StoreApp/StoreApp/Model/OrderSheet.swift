f362d1b1d6c60d6c45c4bbef99dd9e0dca7e5867
oingbong <oingbong@gmail.com>
2018-12-23 23:32:27 +0900

    Add OrderSheet Model, Orderable Protocol for Delegate Pattern with DetailView, DetailVC

---
+++ StoreApp/StoreApp/Model/OrderSheet.swift
@@ -0,0 +1,15 @@
+//
+//  OrderSheet.swift
+//  StoreApp
+//
+//  Created by oingbong on 23/12/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import Foundation
+
+struct OrderSheet {
+    var customer: String?
+    var price: String?
+    var menu: String?
+}
