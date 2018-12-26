d625f960cae31099ca1ad01f59a8018aa035f025
oingbong <oingbong@gmail.com>
2018-12-26 13:02:41 +0900

    Delete unused Queue , Because already urlsesstion with async & Add ImageGroup Protocol with ThumbnailView, DetailSectionView

--- StoreApp/StoreApp/CustomView/DetailSubView/DetailSectionView.swift
+++ StoreApp/StoreApp/CustomView/DetailSubView/DetailSectionView.swift
@@ -8,21 +8,21 @@
 
 import UIKit
 
-class DetailSectionView: UIStackView {
+class DetailSectionView: UIStackView, ImageGroup {
     func configure(with item: DetailItem) {
         let itemCount = item.detailSection.count
         for index in 0..<itemCount {
             guard let fileName = item.detailSection[index].components(separatedBy: "/").last else { continue }
             let isExist = LocalFileManager.fileExists(fileName: fileName)
             if isExist {
-                addDetailSection(with: fileName)
+                addImages(with: fileName)
             } else {
-                downloadDetailSection(with: item.detailSection[index])
+                downloadImages(with: item.detailSection[index])
             }
         }
     }
     
-    func addDetailSection(with fileName: String) {
+    func addImages(with fileName: String) {
         guard let data = LocalFileManager.imageData(with: fileName) else { return }
         guard let image = UIImage(data: data) else { return }
         let imageView = UIImageView(image: image)
@@ -32,21 +32,13 @@ class DetailSectionView: UIStackView {
         self.addArrangedSubview(imageView)
     }
     
-    func downloadDetailSection(with imageURL: String) {
+    func downloadImages(with imageURL: String) {
         guard let url = URL(string: imageURL) else { return }
-        download(with: url) { (fileName) in
+        NetworkManager.imageDownLoad(with: url, handler: { (fileName) in
             DispatchQueue.main.async {
-                self.addDetailSection(with: fileName)
+                self.addImages(with: fileName)
             }
-        }
-    }
-    
-    private func download(with url: URL, handler: @escaping (String) -> Void) {
-        DispatchQueue.global().async {
-            NetworkManager.imageDownLoad(with: url, handler: { (fileName) in
-                handler(fileName)
-            })
-        }
+        })
     }
 }
 
