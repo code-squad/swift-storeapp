26980acaf3991245802c10c83849cfaf0567f7c3
DrakeYang <drake.the.programmer@gmail.com>
2019-03-25 19:35:02 +0900

    테이블뷰 추가, 데이터소스오브젝트 파일추가

--- StoreApp/StoreApp/View/DataSourceObject.swift
+++ StoreApp/StoreApp/View/DataSourceObject.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2019년 Drake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class DataSourceObject : NSObject, UITableViewDataSource {
+    private let defaultCellIdentifier : String = "defaultCell"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = MyCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: self.defaultCellIdentifier)
+        
+        cell.textLabel?.text = friends[indexPath.row].name
+        cell.detailTextLabel?.text = "나이 : \(friends[indexPath.row].age)  전화번호 : \(friends[indexPath.row].mobileNumber)"
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
