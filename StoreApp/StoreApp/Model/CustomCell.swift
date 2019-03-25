015ad071bd76221ca5e2badee05595017b0978d5
DrakeYang <drake.the.programmer@gmail.com>
2019-03-25 20:48:08 +0900

    데이터소스,커스텀셀 기본내용 추가

--- StoreApp/StoreApp/Model/CustomCell.swift
+++ StoreApp/StoreApp/Model/CustomCell.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2019년 Drake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CustomCell : UITableViewCell {
+    /// 이미지
+//    @IBOutlet weak var imageView: UIImage
+//    
+//    /// 타이틀
+//    @IBOutlet weak var title: UILabel!
+//    
+//    /// 상세설명
+//    @IBOutlet weak var subscription: UILabel!
+//    
+//    
+//    /// 메뉴 가격
+//    @IBOutlet weak var price: UILabel!
+//    
+//    
+//    /// 이벤트 배지
+//    @IBOutlet weak var eventBadge: UILabel!
+    
+}
