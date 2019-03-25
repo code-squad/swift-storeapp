68601962585aa71514cba130e2c585de86e168c1
DrakeYang <drake.the.programmer@gmail.com>
2019-03-25 19:34:15 +0900

    프로젝트 생성

---
+++ StoreApp/StoreApp/View/DataSourceObject.swift
@@ -0,0 +1,9 @@
+//
+//  DataSourceObject.swift
+//  StoreApp
+//
+//  Created by Yoda Codd on 2019. 3. 25..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import Foundation
---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by Yoda Codd on 2019. 3. 25..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
