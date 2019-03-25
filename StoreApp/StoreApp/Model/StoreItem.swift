f043abd78e74b5a92c4a9cf98e0c27e9ab666b04
DrakeYang <drake.the.programmer@gmail.com>
2019-03-25 20:46:18 +0900

    물품리스트 이름변경 : Food -> StoreItem

---
+++ StoreApp/StoreApp/Model/StoreItem.swift
@@ -0,0 +1,26 @@
+//
+//  Product.swift
+//  StoreApp
+//
+//  Created by Yoda Codd on 2019. 3. 25..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import Foundation
+
+class Food {
+    
+    let hash = ""
+    
+    let iamge = ""
+    
+    let alt = ""
+    
+    let delivery_type = ""
+    
+    let title = ""
+    
+    let description = ""
+    
+    let s_price = ""
+}
