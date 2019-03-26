7071a640cd503e3a60b2ed7be357667df71989b8
DrakeYang <drake.the.programmer@gmail.com>
2019-03-26 21:02:59 +0900

    StoreItem 배열에서 쓰는 함수 추가

--- StoreApp/StoreApp/Model/StoreItemManager.swift
+++ StoreApp/StoreApp/Model/StoreItemManager.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+class StoreItemManager {
+    var storeItemList : [StoreItem]
+    
+    init(storeItemList: [StoreItem]){
+        self.storeItemList = storeItemList
+    }
+    
+    /// 인덱스를 받아서 해당 객체를 리턴한다
+    func getStoreItem(index: Int) -> StoreItem {
+        return self.storeItemList[index]
+    }
+    
+    /// 카운트를 리턴한다
+    func count() -> Int {
+        return self.storeItemList.count
+    }
+}
