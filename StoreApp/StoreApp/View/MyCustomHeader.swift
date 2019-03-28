f2bea1fab7bd4ef2ebc44253f18f49ec138f0bf5
DrakeYang <drake.the.programmer@gmail.com>
2019-03-28 21:21:18 +0900

    커스텀헤더를 위한 파일추가 : MyCustomHeader.xib,swift

---
+++ StoreApp/StoreApp/View/MyCustomHeader.swift
@@ -0,0 +1,47 @@
+//
+//  MyCustomHeader.swift
+//  StoreApp
+//
+//  Created by Yoda Codd on 2019. 3. 28..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomHeader : UITableViewHeaderFooterView {
+    @IBOutlet weak var headerTitle: UILabel!
+    @IBOutlet weak var headerText: UILabel!
+    
+    
+}
+
+extension UIView {
+    
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
