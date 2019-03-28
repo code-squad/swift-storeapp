487bc594d82e87ce0b7825db7e54382ceff816df
DrakeYang <drake.the.programmer@gmail.com>
2019-03-28 21:24:04 +0900

    데이터소스가 테이블뷰 델리게이트 채택, 헤더컨텐트매니저 소유

--- StoreApp/StoreApp/Model/DataSourceObject.swift
+++ StoreApp/StoreApp/Model/DataSourceObject.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 
-class DataSourceObject : NSObject, UITableViewDataSource {
+class DataSourceObject : NSObject, UITableViewDataSource  {
     private let defaultCellIdentifier : String = "MyCustomCell"
     
-    /// 스토어아이템 맥스 카운트를 저장한다
-    var storeItemContainer : StoreItemContainer
     
+    /// 헤더컨텐트를 받는다
+    var headContentManager = MyHeaderContentManager()
+    
+    /// 스토어아이템 맥스 카운트를 저장한다
+    var storeItemContainer : StoreItemContainer = StoreItemContainer()
+    
+    override init(){}
     init(storeItemContainer: StoreItemContainer){
         self.storeItemContainer = storeItemContainer
+    }
+    
+    /// 데이터소스에 데이터 입력
+    func inputData(storeItemSlot: StoreItemSlot){
+        self.storeItemContainer.addStoreItemSlot(storeItemSlot: storeItemSlot)
     }
     
     /// 센션당 로우 카운트 리턴
@@ -42,4 +52,24 @@ class DataSourceObject : NSObject, UITableViewDataSource {
         return self.storeItemContainer.count()
     }
     
+
 }
+
+extension   DataSourceObject : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "MyCustomHeader") as! MyCustomHeader
+        
+        // 헤더컨텐트를 꺼낸다
+        let headContent = self.headContentManager.getMyHeaderContent(index: section)
+        
+        headerView.headerTitle.text = headContent.title
+        headerView.headerText.text = headContent.text
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 100
+    }
+}
+
