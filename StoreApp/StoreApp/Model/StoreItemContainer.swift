a1827a3fb7191d6dd5b0d5a2b29d528fd01ab9e5
DrakeYang <drake.the.programmer@gmail.com>
2019-03-28 18:09:33 +0900

    스토어아이템컨테이너 기본기능추가 : 카운트, 슬롯내부카운트

--- StoreApp/StoreApp/Model/StoreItemContainer.swift
+++ StoreApp/StoreApp/Model/StoreItemContainer.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+class StoreItemContainer {
+    var storeItemSlotList : [StoreItemSlot] = []
+    
+    init(){}
+    init(storeItemSlotList: [StoreItemSlot]){
+        self.storeItemSlotList = storeItemSlotList
+    }
+    
+    
+    /// 스토어아이템 슬롯을 받아서 추가한다
+    func addStoreItemSlot(storeItemSlot: StoreItemSlot) {
+        self.storeItemSlotList.append(storeItemSlot)
+    }
+    
+    /// 스토어아이템 슬롯 번호를 받아서 count 를 리턴한다
+    func getCount(number: Int) -> Int {
+        return self.storeItemSlotList[number].count()
+    }
+    
+    /// 인덱스패스를 받아서 해당 아이템을 리턴
+    func getStoreItem(indexPath: IndexPath) -> StoreItem {
+        return self.storeItemSlotList[indexPath.section].getStoreItem(index: indexPath.row)
+    }
+    
+    /// 전체 스토어아이템 슬롯의 count 리턴
+    func count() -> Int {
+        return self.storeItemSlotList.count
+    }
+}
