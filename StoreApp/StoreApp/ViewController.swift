1900cec50a4808fa41171387ac83beb1fe302925
DrakeYang <drake.the.programmer@gmail.com>
2019-03-29 22:07:42 +0900

    뷰컨 뷰디드로드 단계 함수로 분리

--- StoreApp/StoreApp/ViewController.swift
+++ StoreApp/StoreApp/ViewController.swift
@@ -26,29 +26,46 @@ class ViewController: UIViewController {
     /// 커스텀헤더 파일명
     private let nibFileName = "MyCustomHeader"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // 헤더컨텐트 매니서 데이터 입력
+    /// 헤더컨텐트 매니저 데이터를 데이터소스에 입력한다
+    func inputHeaderContent(){
+        // 헤더컨텐트 매니저 데이터 입력
         for count in 0..<allHeaderTitle.count {
             let myHeaderContent = MyHeaderContent(title: allHeaderTitle[count], text: allHedaerText[count])
             self.dataSourceObject.addHeaderContent(myHeaderContent: myHeaderContent)
         }
+    }
+    
+    /// 스토어 정보를 데이터소스에 입력한다
+    func inputStoreItemData(){
+        // JSON 에서 파일명 추출
+        for fileName in self.allJSONFileName {
+            // 파일명으로 스토어아이템 슬롯 추출
+            let storeItemSlot = StoreItemMaker.makeStoreItem(fileName: fileName)
+            // 스토어아이템 데이터 추가
+            self.dataSourceObject.inputData(storeItemSlot: storeItemSlot)
+        }
+    }
+    
+    /// 테이블뷰에 커스텀 헤더 등록
+    func inputCustomHeader(){
+        let headerNib = UINib.init(nibName: nibFileName, bundle: Bundle.main)
+        self.storeItemTableView.register(headerNib, forHeaderFooterViewReuseIdentifier: "MyCustomHeader")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 헤더컨텐트 매니저 데이터 입력
+        inputHeaderContent()
         
         // 테이블뷰에 데이터소스 입력
         self.storeItemTableView.dataSource = self.dataSourceObject
         
-        // 테이블뷰에 데이터소스 입력
-        for fileName in self.allJSONFileName {
-            let storeItemSlot = StoreItemMaker.makeStoreItem(fileName: fileName)
-            // 데이터 추가
-            self.dataSourceObject.inputData(storeItemSlot: storeItemSlot)
-        }
+        /// 스토어 정보를 데이터소스에 입력한다
+        inputStoreItemData()
         
         // 테이블뷰에 커스텀 헤더 등록
-        let headerNib = UINib.init(nibName: nibFileName, bundle: Bundle.main)
-        self.storeItemTableView.register(headerNib, forHeaderFooterViewReuseIdentifier: "MyCustomHeader")
-        
+        inputCustomHeader()        
         
         // 테이블뷰에 델리게이트 입력
         self.storeItemTableView.delegate = self.dataSourceObject
