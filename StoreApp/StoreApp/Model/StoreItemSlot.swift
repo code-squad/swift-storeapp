5380b99587220a12141c9fbb20a42c2bed447cf3
DrakeYang <drake.the.programmer@gmail.com>
2019-04-03 18:29:26 +0900

    스토어아이템슬롯이 같은타입을 받아서 내부리스트를 추가하는 함수 추가

--- StoreApp/StoreApp/Model/StoreItemSlot.swift
+++ StoreApp/StoreApp/Model/StoreItemSlot.swift
@@ -9,11 +9,13 @@
 import Foundation
 
 class StoreItemSlot {
-    var storeItemList : [StoreItem]
+    private var storeItemList : [StoreItem] = []
     
     init(storeItemList: [StoreItem]){
         self.storeItemList = storeItemList
     }
+    
+    init(){}
     
     /// 인덱스를 받아서 해당 객체를 리턴한다
     func storeItem(index: Int) -> StoreItem {
@@ -28,5 +30,21 @@ class StoreItemSlot {
     /// 인덱스를 받아서 해당 객체의 토스트 메세지를 리턴한다
     func toastMessage(index: Int) -> String {
         return self.storeItemList[index].toastMessage()
+    }
+    
+    /// 내부 리스트를 리턴한다
+    func allItems() -> [StoreItem] {
+        return self.storeItemList
+    }
+    
+    /// 슬롯을 받아서 자기자신에 추가한다
+    func add(storeItemSlot: StoreItemSlot){
+        // 리스트 추출
+        let itemList = storeItemSlot.allItems()
+        
+        // 리스트를 자기자신에 추가한다
+        for item in itemList{
+            self.storeItemList.append(item)
+        }
     }
 }
