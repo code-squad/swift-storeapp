f90039c6cbd54021279d86c50289a4014581cd8d
hngfu <djsl1018@naver.com>
2019-04-09 17:20:37 +0900

    StoreItems, StoreItem 생성

---
+++ StoreApp/StoreApp/StoreItem.swift
@@ -0,0 +1,13 @@
+//
+//  StoreItem.swift
+//  StoreApp
+//
+//  Created by 조재흥 on 19. 4. 9..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct StoreItem {
+    
+}
