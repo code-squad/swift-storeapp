7e41f855b2a53b1442d293a5f3d8c571b874d749
hngfu <djsl1018@naver.com>
2019-04-09 17:28:21 +0900

    [StoreItem] 프로퍼티 매핑해서 구현

--- StoreApp/StoreApp/StoreItem.swift
+++ StoreApp/StoreApp/StoreItem.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
-struct StoreItem {
-    
+struct StoreItem: Decodable {
+    let detail_hash: String
+    let image: String
+    let alt: String
+    let delivery_type: [String]
+    let title: String
+    let description: String
+    let n_price: String?
+    let s_price: String
+    let badge: [String]?
 }
