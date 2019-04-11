85eefbafc569fd869c22fd9481041b2c02423de5
DrakeYang <drake.the.programmer@gmail.com>
2019-04-11 22:24:34 +0900

    배지 초기값 설정, 있는경우 설정단계추가. 생성자 체크구문 세분화 - 실패시 로깅추가

--- StoreApp/StoreApp/Model/StoreItem.swift
+++ StoreApp/StoreApp/Model/StoreItem.swift
@@ -6,9 +6,10 @@
 //  Copyright © 2019년 Drake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import os
 
-class StoreItem : Codable{
+class StoreItem : Decodable {
     
     let detail_hash : String
     
@@ -16,7 +17,7 @@ class StoreItem : Codable{
     
     let alt : String
     
-    let badge : [String]
+    var badge : [String] = []
     
     let title : String
     
@@ -26,24 +27,48 @@ class StoreItem : Codable{
     
     var n_price : String = ""
     
+    var imageFileURL = ""
+    
     init?(json: [String: Any]) {
-        guard
-            let detail_hash = json["detail_hash"] as? String,
-            let image = json["image"] as? String,
-            let alt = json["alt"] as? String,
-            let badge = json["badge"] as? [String],
-            let title = json["title"] as? String,
-            let description = json["description"] as? String,
-            let s_price = json["s_price"] as? String
-        else { return nil }
+        guard  let detail_hash = json["detail_hash"] as? String else {
+            os_log("StoreItem 생성실패 : detail_hash")
+            return nil
+        }
+         guard   let image = json["image"] as? String  else {
+            os_log("StoreItem 생성실패 : image")
+            return nil
+        }
+         guard   let alt = json["alt"] as? String  else {
+            os_log("StoreItem 생성실패 : alt")
+            return nil
+        }
+        guard    let title = json["title"] as? String  else {
+            os_log("StoreItem 생성실패 : title")
+            return nil
+        }
+        guard    let description = json["description"] as? String  else {
+            os_log("StoreItem 생성실패 : description")
+            return nil
+        }
+        
+        guard    let s_price = json["s_price"] as? String  else {
+            os_log("StoreItem 생성실패 : s_price")
+            return nil
+        }
         
         self.detail_hash = detail_hash
         self.image = image
         self.alt = alt
-        self.badge = badge
+        
+        // 배지가 있을경우
+        if let badge = json["badge"] as? [String] {
+            self.badge = badge
+        }
+        
         self.title = title
         self.description = description
         self.s_price = s_price
+        
         // n_price 는 없을수도 있음. 있으면 값 입력, 없으면 기본값 유지
         if let nPrice = json["n_price"] as? String {
             self.n_price = nPrice + "  "
