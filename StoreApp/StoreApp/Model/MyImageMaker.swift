2b503f287e5fa917162800ec1bf29211b0ac9661
DrakeYang <drake.the.programmer@gmail.com>
2019-04-11 11:47:07 +0900

    url 에서 파일을 받는 객체 추가 : MyImageMaker

---
+++ StoreApp/StoreApp/Model/MyImageMaker.swift
@@ -0,0 +1,20 @@
+//
+//  ImageMaker.swift
+//  StoreApp
+//
+//  Created by Yoda Codd on 2019. 4. 9..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import Foundation
+
+class ImageMaker {
+    var maxSectionCount = 0
+    private var finishedSectionCount = 0
+    
+    func addFinishedCount(){
+        self.finishedSectionCount += 1
+    }
+    
+    
+}
