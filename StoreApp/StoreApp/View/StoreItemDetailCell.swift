cc8b6ccd8f08b4e0ec4459c1d9ed74fdfec52c8e
DrakeYang <drake.the.programmer@gmail.com>
2019-04-15 17:32:29 +0900

    상세페이지 표현할 파일 추가

---
+++ StoreApp/StoreApp/View/StoreItemDetailCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoreItemDetailCell.swift
+//  StoreApp
+//
+//  Created by Yoda Codd on 2019. 4. 15..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class StoreItemDetailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ StoreApp/StoreApp/View/StoreItemDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  StoreItemDetailView.swift
+//  StoreApp
+//
+//  Created by Yoda Codd on 2019. 4. 15..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class StoreItemDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
