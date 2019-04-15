bfaf5c481277b6bcf46a6d4dae158760e4f5a710
Dongmin.yoon <ehdals8965@naver.com>
2019-04-15 19:05:49 +0900

    Implement Read JSON Data  from file

---
+++ StoreApp/StoreApp/FileURL.swift
@@ -0,0 +1,14 @@
+//
+//  FileURL.swift
+//  StoreApp
+//
+//  Created by 윤동민 on 15/04/2019.
+//  Copyright © 2019 윤동민. All rights reserved.
+//
+
+import Foundation
+
+struct FileURL {
+    static let mainItemURL = "http://public.codesquad.kr/jk/storeapp/main.json"
+    static let projectFolderURL = "/Users/yundongmin/Codesquad/Level3/swift-storeapp/StoreApp/file"
+}
