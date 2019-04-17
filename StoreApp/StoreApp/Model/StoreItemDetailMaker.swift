a55906fbb67b99b5dbcceb1da09ab91232f0eb6d
DrakeYang <drake.the.programmer@gmail.com>
2019-04-17 16:24:22 +0900

    스토어아이템디테일 메이커 내용추가

--- StoreApp/StoreApp/Model/StoreItemDetailMaker.swift
+++ StoreApp/StoreApp/Model/StoreItemDetailMaker.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import os.log
+
+class StoreItemDetailMaker {
+    
+    /// json 배열을 받아서 스토어아이템 배열로 리턴
+    static private func makeStoreItemList(jsonData: [String : Any]) -> StoreItemDetailModel? {
+        // data 를 키로 문자열배열 추출
+        guard let data = jsonData["data"] as? [String : Any] else {
+             os_log("jsondata 에서 디테일모델용 data 생성 실패")
+            return nil
+        }
+        
+        guard let storeItemDetailModel = StoreItemDetailModel(json: data) else {
+             os_log("스토어아이템 디테일모델 생성 실패")
+            return nil
+        }
+        
+        //결과리턴
+        return storeItemDetailModel
+    }
+    
+}
