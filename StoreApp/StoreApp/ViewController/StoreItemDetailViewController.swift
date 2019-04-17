d5ece20532371a7be3e67c21f24e7630a8420ee7
DrakeYang <drake.the.programmer@gmail.com>
2019-04-17 18:05:51 +0900

    title -> detailTitle 로 수정. StoreItemDetailViewController 에서 디테일뷰 세팅 단계 추가. 실패시 부한반복

--- StoreApp/StoreApp/ViewController/StoreItemDetailViewController.swift
+++ StoreApp/StoreApp/ViewController/StoreItemDetailViewController.swift
@@ -8,41 +8,44 @@
 
 import UIKit
 import os.log
+import MyURLDataMaker
 
 class StoreItemDetailViewController: UIViewController {
+    @IBOutlet weak var detailView: StoreItemDetailView!
     
+    // 세그를 통해 넘어오는 값
     var detailHash = ""
+    var detailTitle = ""
     
-    var detailView = StoreItemDetailView()
-    
-    func prepare(segue: UIStoryboardSegue, sender: AnyObject?) {
-        
+    /// 데이터를 받아서 아이템디테일 모델을 생성하는 함수를 사용, 생성한다.
+    func makeItemDetail(data: Data){
+        // 디테일모델 생성
+        guard let storeItemdetailModel = StoreItemDetailMaker.makeStoreItemDetailModel(anyData: data) else {
+            os_log("디테일모델 작성 실패")
+            return ()
+        }
+        // title 설정
+        storeItemdetailModel.set(title: self.detailTitle)
+        // 모델을 생성해서 뷰에 적용시켜준다
+        detailView.setting(model: storeItemdetailModel)
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        self.detailHash = detailHash
         
-        os_log("detailHash : %@",self.detailHash)
-        // Do any additional setup after loading the view.
+        // 전달받은 디테일해쉬를 디코드. 실패할경우 계속 시도한다
+        while true {
+            let urlLoading = MyDetailDataLoader.makeDetailModel(detailHash: self.detailHash, completion: makeItemDetail)
+            // 성공하면 불러오기 종료
+            if urlLoading == true {
+                break
+            }
+        }
+        
+        // end of viewDidLoad
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
