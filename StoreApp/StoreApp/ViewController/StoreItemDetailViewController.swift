281c9a3e242cff9e2ea80a3d5a095113cc602fd2
DrakeYang <drake.the.programmer@gmail.com>
2019-04-17 20:27:34 +0900

    url 접속시도를 디스패치로 구현. 성공할때까지 반복

--- StoreApp/StoreApp/ViewController/StoreItemDetailViewController.swift
+++ StoreApp/StoreApp/ViewController/StoreItemDetailViewController.swift
@@ -12,13 +12,15 @@ import MyURLDataMaker
 
 class StoreItemDetailViewController: UIViewController {
     @IBOutlet weak var detailView: StoreItemDetailView!
+    @IBOutlet weak var thumbScrollView: UIScrollView!
+    
     
     // 세그를 통해 넘어오는 값
     var detailHash = ""
     var detailTitle = ""
     
     /// 데이터를 받아서 아이템디테일 모델을 생성하는 함수를 사용, 생성한다.
-    func makeItemDetail(data: Data){
+    private func makeItemDetail(data: Data){
         // 디테일모델 생성
         guard let storeItemdetailModel = StoreItemDetailMaker.makeStoreItemDetailModel(anyData: data) else {
             os_log("디테일모델 작성 실패")
@@ -30,17 +32,24 @@ class StoreItemDetailViewController: UIViewController {
         detailView.setting(model: storeItemdetailModel)
     }
     
+    /// url 점속 실패시 성공할때까지 반복
+    private func tryConnectData(detailHash: String){
+        // 실행 완료를 기다리지 않도록 디스패치 사용
+        DispatchQueue.global().sync {
+            // 전달받은 디테일해쉬를 디코드. 실패할경우 계속 시도한다
+            let urlLoading = MyDetailDataLoader.detailModelData(detailHash: self.detailHash, completion: makeItemDetail)
+            // 실패하면 다시 시도
+            if urlLoading == false {
+                tryConnectData(detailHash: detailHash)
+            }
+        }        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 전달받은 디테일해쉬를 디코드. 실패할경우 계속 시도한다
-        while true {
-            let urlLoading = MyDetailDataLoader.makeDetailModel(detailHash: self.detailHash, completion: makeItemDetail)
-            // 성공하면 불러오기 종료
-            if urlLoading == true {
-                break
-            }
-        }
+        tryConnectData(detailHash: self.detailHash)
         
         // end of viewDidLoad
     }
