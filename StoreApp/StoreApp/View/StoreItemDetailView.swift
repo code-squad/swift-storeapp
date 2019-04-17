a64a416463b2c9e42942aba3bbfee3d0affd9c70
DrakeYang <drake.the.programmer@gmail.com>
2019-04-17 18:06:24 +0900

    디테일뷰에 타이틀 적용 추가

--- StoreApp/StoreApp/View/StoreItemDetailView.swift
+++ StoreApp/StoreApp/View/StoreItemDetailView.swift
@@ -11,11 +11,20 @@ import UIKit
 class StoreItemDetailView: UIView {
     @IBOutlet weak var title:UILabel!
     @IBOutlet weak var adCopy:UILabel!
-    @IBOutlet weak var s_price:UILabel!
-    @IBOutlet weak var n_price:UILabel!
+    @IBOutlet weak var price:UILabel!
     @IBOutlet weak var point:UILabel!
     @IBOutlet weak var delivery_type:UILabel!
     @IBOutlet weak var delivery_price:UILabel!
 
-
+    
+    func setting(model: StoreItemDetailModel){
+        DispatchQueue.main.async {            
+            self.adCopy.text = model.product_description
+            self.point.text = model.point
+            self.delivery_price.text = model.delivery_fee
+            self.title.text = model.title
+        }
+    }
+    
+    
 }
