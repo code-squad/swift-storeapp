55194a3ad0750cf20b6944a6b17fc0f697e43791
DrakeYang <drake.the.programmer@gmail.com>
2019-04-18 11:01:44 +0900

    커스텀 스크롤뷰 객체 추가

---
+++ StoreApp/StoreApp/View/MyScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  MyScrollView.swift
+//  StoreApp
+//
+//  Created by Yoda Codd on 2019. 4. 17..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class MyScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
