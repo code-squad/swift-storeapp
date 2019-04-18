0d0241251e90a8185b9ab32b198fff4edd9e687e
DrakeYang <drake.the.programmer@gmail.com>
2019-04-18 11:07:38 +0900

    이미지URL 을 받아서 저장하고, 저장한 이미지의 풀패스를 탈출클로저에 넣는 함수 추가

--- StoreApp/StoreApp/Model/MyImageMaker.swift
+++ StoreApp/StoreApp/Model/MyImageMaker.swift
@@ -18,7 +18,7 @@ class MyImageMaker {
     
     // Create destination URL
     let folderUrl: URL =  FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first as URL!
-
+    
     /// 섹션 카운트 초기값 선언
     func setMaxSectionCount(count: Int){
         self.maxSectionCount = count
@@ -75,10 +75,11 @@ class MyImageMaker {
         return fileName
     }
     
+    /// 이미지주소, 섹션, 로우, 탈출클로저를 받아서 이미지 저장 진행
     func save(imageURL: String,
-                      section: Int,
-                      row: Int,
-                      completion: @escaping (_ imageURL: String, _ section: Int, _ row: Int) -> ()){
+              section: Int,
+              row: Int,
+              completion: @escaping (_ imageURL: String, _ section: Int, _ row: Int) -> ()){
         // 시작 로깅
         os_log("url 다운로드 시도 : %@",imageURL)
         
@@ -142,5 +143,75 @@ class MyImageMaker {
         task.resume()
     }
     
+    /// 이미지주소,탈출클로저를 받아서 이미지를 저장 후 저장한 이미지의 위치+파일명을 탈출클로저에 입력
+    func save(imageURL: String,
+              completion: @escaping (_ imageFullPath: String) -> ()){
+        // 시작 로깅
+        os_log("url 다운로드 시도 : %@",imageURL)
+        
+        // 파일명 추출
+        guard let fileName = extractFileNameFrom(imageURL: imageURL) else {
+            return ()
+        }
+        
+        // Create destination URL
+        let documentsUrl: URL =  self.folderUrl
+        let destinationFileUrl = documentsUrl.appendingPathComponent(fileName)
+        
+        //Create URL to the source file you want to download
+        guard let fileURL = URL(string: imageURL) else {
+            os_log("입력된 파일url을 URL 형으로 변환하지 못했습니다. fileURL : %@", imageURL)
+            return ()
+        }
+        
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        let request = URLRequest(url:fileURL)
+        
+        // downloadTask 시작
+        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
+            if let tempLocalUrl = tempLocalUrl, error == nil {
+                // 연결 시도가 성공한다면
+                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                    // 로깅용 형변환
+                    let statusCodeString = String(statusCode)
+                    // 연결 성공 로깅
+                    os_log("Successfully downloaded. Status code: %@",statusCodeString)
+                }
+                // 파일 다운로드 복사 작업 시작
+                do {
+                    // 같은이름의 파일이 있다면 받지 않는다.
+                    if FileManager.default.fileExists(atPath: destinationFileUrl.path) {
+                        os_log("기존파일 존재. 다운로드 패스 : %@", fileName)
+                    } // 없다면 새 파일 다운로드
+                    else {
+                        // 다운로드 시도
+                        try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
+                        os_log("다운로드 성공 : %@", fileName)
+                    }
+                    
+                    // 다운로드 완료
+                    completion(destinationFileUrl.path)
+                    
+                } // 파일복사 작업 실패시
+                catch (let writeError) {
+                    os_log("Error creating a file %@ : %@", destinationFileUrl.path, writeError.localizedDescription)
+                }
+                // 웹 연결 실패시
+            } else {
+                os_log("Error took place while downloading a file. Error description: %@", (error?.localizedDescription)!);
+            }
+        }
+        task.resume()
+    }
+    
+    /// 파일 url 배열을 받아서 저장
+    func saveFiles(urls: [String],
+                   completion: @escaping (_ imageFullPath: String) -> ()){
+        
+        for url in urls {
+            save(imageURL: url, completion: completion)
+        }
+    }
 }
-
