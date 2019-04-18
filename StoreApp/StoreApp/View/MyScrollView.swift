4fd515af7dd1be6adb7046c8d11dd72b72192b45
DrakeYang <drake.the.programmer@gmail.com>
2019-04-18 11:04:49 +0900

    커스텀 스크롤뷰 내용 구현. 메인 서브뷰 추가 - 다른 추가되는 뷰를 담당할 예정. 초기세팅 추가. 사진추가 함수. 이미지를 받아서 이미지뷰를 만들고, 메인서브뷰와 컨텐트 사이즈를 늘린 후 추가한다

--- StoreApp/StoreApp/View/MyScrollView.swift
+++ StoreApp/StoreApp/View/MyScrollView.swift
@@ -7,15 +7,45 @@
 //
 
 import UIKit
+import os.log
+
 
 class MyScrollView: UIScrollView {
+    
+    private var mainSubView = UIView()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    func setMainSubView(){
+        self.mainSubView.frame = CGRect(x: 0, y: 0, width: 0, height: self.bounds.height)
+        
+        self.contentSize = self.bounds.size
+        
+        self.addSubview(self.mainSubView)
     }
-    */
-
+    
+    func add(image: UIImage){
+        DispatchQueue.main.async {
+            
+            // 추가할 새 이미지뷰 생성. 사이즈는 스크롤뷰와 같음
+            let newImageView = UIImageView(frame: self.frame)
+            // 새 이미지 추출
+            newImageView.image = image
+            
+            // 새 사진의 시작점은 메인서브뷰의 가로축 끝
+            newImageView.frame.origin.x = self.mainSubView.frame.width
+            
+            // 새 메인서브뷰 가로길이 계산
+            let newWidth = self.mainSubView.frame.width + self.bounds.width
+            let newSize = CGSize(width: newWidth, height: self.bounds.height)
+            let newFrame = CGRect(origin: CGPoint(x: 0, y: 0), size: newSize)
+            // 새 길이 적용
+            self.contentSize = newSize
+            self.mainSubView.frame = newFrame
+            
+            // 새이미지뷰 추가
+            self.mainSubView.addSubview(newImageView)
+            
+            os_log("스크롤뷰에 이미지 추가 완료")
+        }
+        
+    }
 }
