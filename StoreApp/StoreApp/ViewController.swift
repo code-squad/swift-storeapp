2d2a0657c292b12432f67edc2bf63e01b85ef78e
hngfu <djsl1018@naver.com>
2019-04-19 16:01:13 +0900

    [ViewController] storeTableView 연결

--- StoreApp/StoreApp/ViewController.swift
+++ StoreApp/StoreApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storeTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
