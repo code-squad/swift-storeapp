6be0dd750567aa14d18faef8d3d595289da30d87
Dongmin.yoon <ehdals8965@naver.com>
2019-04-19 15:03:20 +0900

    Add Contents Studied at SStep5 in README FILE

--- StoreApp/StoreApp/Model/StoreItems.swift
+++ StoreApp/StoreApp/Model/StoreItems.swift
@@ -19,17 +19,7 @@ class StoreItems {
     private var soupItems: [StoreItem] = []
     private var sideItems: [StoreItem] = []
     
-    init() {
-//        guard let mainItemsData = FileReader.getTextFrom(file: FileName.mainItem),
-//              let soupItemsData = FileReader.getTextFrom(file: FileName.soupItem),
-//              let sideItemsData = FileReader.getTextFrom(file: FileName.sideItem) else { return }
-//        guard let mainItems = JSONParser.parseJSONData(from: mainItemsData),
-//              let soupItems = JSONParser.parseJSONData(from: soupItemsData),
-//              let sideItems = JSONParser.parseJSONData(from: sideItemsData) else { return }
-//        self.mainItems = mainItems
-//        self.soupItems = soupItems
-//        self.sideItems = sideItems
-    }
+    init() {}
     
     func getDataFromNetwork() {
         NetworkHandler.getData(from: ServerURL.main)
