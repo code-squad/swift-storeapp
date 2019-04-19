07ab92044ebad27d1ae7a42aadd803b2e2d0aaab
hngfu <djsl1018@naver.com>
2019-04-19 15:55:15 +0900

    [StoreItems] initialization 구현

--- StoreApp/StoreApp/StoreItems.swift
+++ StoreApp/StoreApp/StoreItems.swift
@@ -9,10 +9,18 @@
 import Foundation
 
 struct StoreItems {
-    private var storeItems = [StoreItem]()
+    private var storeItems: [StoreItem]
     
     subscript(index: Int) -> StoreItem? {
         guard 0 <= index, index < storeItems.count else { return nil }
         return storeItems[index]
+    }
+    
+    init?() {
+        guard let pathOfData = Bundle.main.path(forResource: "main", ofType: "json"),
+            let jsonData = try? Data(contentsOf: URL(fileURLWithPath: pathOfData),
+                                     options: .mappedIfSafe),
+            let items = try? JSONDecoder().decode([StoreItem].self, from: jsonData) else { return nil }
+        self.storeItems = items
     }
 }
