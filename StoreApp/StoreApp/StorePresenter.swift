8730c3bb3285bf74289232b884d1504b1958e609
hngfu <djsl1018@naver.com>
2019-04-19 16:51:31 +0900

    [StorePresenter] 생성

---
+++ StoreApp/StoreApp/StorePresenter.swift
@@ -0,0 +1,15 @@
+//
+//  StorePresenter.swift
+//  StoreApp
+//
+//  Created by 조재흥 on 19. 4. 19..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+class StorePresenter {
+
+    private let storeItems = StoreItems()
+    
+}
--- StoreApp/StoreApp/ViewController.swift
+++ StoreApp/StoreApp/ViewController.swift
@@ -16,7 +16,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
