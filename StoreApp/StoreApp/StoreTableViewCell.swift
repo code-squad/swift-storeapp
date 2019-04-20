5315deb0345be1275d68e7590b44626ac7d6c8e2
hngfu <djsl1018@naver.com>
2019-04-20 17:19:19 +0900

    [StoreTableViewCell] nib, swift file 추가

---
+++ StoreApp/StoreApp/StoreTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  StoreTableViewCell.swift
+//  StoreApp
+//
+//  Created by 조재흥 on 19. 4. 20..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class StoreTableViewCell: UITableViewCell {
+    @IBOutlet weak var menuImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailedDescriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
