7a7c796fa8d339caaf7b0e05d50fe731133605c7
hngfu <djsl1018@naver.com>
2019-04-21 15:04:26 +0900

    [StoreTableViewCell] StoreItem을 이용하여 화면을 보여주도록 show(with:) 추가

--- StoreApp/StoreApp/StoreTableViewCell.swift
+++ StoreApp/StoreApp/StoreTableViewCell.swift
@@ -15,8 +15,8 @@ class StoreTableViewCell: UITableViewCell {
     @IBOutlet weak var menuImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var detailedDescriptionLabel: UILabel!
-    @IBOutlet weak var salePriceLabel: UILabel!
     @IBOutlet weak var naturalPriceLabel: UILabel!
+    @IBOutlet weak var salePriceLabel: UILabel!
     @IBOutlet weak var badgeStackView: UIStackView!
     
     //MARK: Type
@@ -33,5 +33,44 @@ class StoreTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func show(with storeItem: StoreItem) {
+        emptyAll()
+        titleLabel.text = storeItem.title
+        detailedDescriptionLabel.text = storeItem.description
+        if let n_price = storeItem.n_price {
+            naturalPriceLabel.attributedText = n_price.strikeThrough()
+            naturalPriceLabel.isHidden = false
+        }
+        salePriceLabel.text = storeItem.s_price
+    }
+    
+    private func emptyAll() {
+        menuImageView.image = nil
+        titleLabel.text = nil
+        detailedDescriptionLabel.text = nil
+        naturalPriceLabel.text = nil
+        naturalPriceLabel.isHidden = true
+        salePriceLabel.text = nil
+        badgeStackView.removeAllArrangedSubviews()
+        badgeStackView.isHidden = true
+    }
+}
+
+extension UIStackView {
+    func removeAllArrangedSubviews() {
+        for subview in self.arrangedSubviews {
+            subview.removeFromSuperview()
+        }
+    }
+}
+
+extension String {
+    func strikeThrough() -> NSAttributedString {
+        let attributes = [NSAttributedString.Key.strikethroughStyle: 1]
+        let strikeThroughString = NSAttributedString(string: self,
+                                                     attributes: attributes)
+        return strikeThroughString
     }
 }
