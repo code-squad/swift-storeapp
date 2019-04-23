4a2fb2e5fd6745628aad55bfda3a6190dabecb8f
hngfu <djsl1018@naver.com>
2019-04-23 11:44:15 +0900

    [StoreItemManager] 리팩토링 및 구조 변경

--- StoreApp/StoreApp/Models/StoreItemManager.swift
+++ StoreApp/StoreApp/Models/StoreItemManager.swift
@@ -10,24 +10,33 @@ import Foundation
 
 struct StoreItemManager {
     
-    private let storeItems: StoreItems?
-    
-    subscript(index: Int) -> StoreItem? {
-        return storeItems?[index]
+    //MARK: - Properties
+    private var storeItems = [StoreItems]()
+
+    //MARK: - Methods
+    //MARK: Subscript
+    subscript(index: Int) -> StoreItems? {
+        guard 0 <= index, index < storeItems.count else { return nil }
+        return storeItems[index]
     }
     
+    //MARK: Initialization
     init() {
-        guard let pathOfData = Bundle.main.path(forResource: "main", ofType: "json"),
+        guard let items = fetchStoreItems(with: "main") else { return }
+        self.storeItems.append(items)
+    }
+    
+    //MARK: Private
+    private func fetchStoreItems(with resourceName: String) -> StoreItems? {
+        guard let pathOfData = Bundle.main.path(forResource: resourceName, ofType: "json"),
             let jsonData = try? Data(contentsOf: URL(fileURLWithPath: pathOfData),
                                      options: .mappedIfSafe),
-            let items = try? JSONDecoder().decode([StoreItem].self, from: jsonData) else {
-                storeItems = nil
-                return
-        }
-        self.storeItems = StoreItems(storeItems: items)
+            let items = try? JSONDecoder().decode([StoreItem].self, from: jsonData) else { return nil }
+        return StoreItems(storeItems: items)
     }
     
+    //MARK: Instance
     func itemCount() -> Int {
-        return storeItems?.count() ?? 0
+        return storeItems.count
     }
 }
