291cb0d81ac832940ba68ef78ecf108d976460e4
hngfu <djsl1018@naver.com>
2019-04-23 17:23:21 +0900

    [SectionInfo] 생성

---
+++ StoreApp/StoreApp/CustomUI/SectionInfo.swift
@@ -0,0 +1,14 @@
+//
+//  SectionInfo.swift
+//  StoreApp
+//
+//  Created by 조재흥 on 19. 4. 23..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct SectionInfo {
+    let title: String
+    let description: String
+}
