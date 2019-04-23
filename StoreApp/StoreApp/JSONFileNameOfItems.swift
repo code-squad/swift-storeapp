d60b1daf370a755d6bba1ae61301eca743051727
hngfu <djsl1018@naver.com>
2019-04-23 11:46:00 +0900

    [JSONFileNameOfItems] 추가

---
+++ StoreApp/StoreApp/JSONFileNameOfItems.swift
@@ -0,0 +1,15 @@
+//
+//  ItemFileName.swift
+//  StoreApp
+//
+//  Created by 조재흥 on 19. 4. 23..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct JSONFileNameOfItems {
+    static let names = ["main",
+                        "side",
+                        "soup",]
+}
