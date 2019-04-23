e4d67daa52ec2a0e5664f420dc992335b2b442f6
DrakeYang <drake.the.programmer@gmail.com>
2019-04-23 12:20:41 +0900

    세로스크롤을 위한 메인스클로뷰 추가

---
+++ StoreApp/StoreApp/View/MyMainScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  MyMainScrollView.swift
+//  StoreApp
+//
+//  Created by Yoda Codd on 2019. 4. 23..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class MyMainScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
