ffbdfb3cac0daff0294fd313347455acbf0c945d
hngfu <djsl1018@naver.com>
2019-04-23 11:17:57 +0900

    [StoreTableViewHeaderCell] 추가

---
+++ StoreApp/StoreApp/CustomUI/StoreTableViewHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoreTableViewHeaderCell.swift
+//  StoreApp
+//
+//  Created by 조재흥 on 19. 4. 23..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class StoreTableViewHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
