9024a499c9af7a7bc009bf006d1aae4fe7821197
DrakeYang <drake.the.programmer@gmail.com>
2019-04-23 15:33:50 +0900

    메인스크롤뷰 뷰추가함수 인자명 변경 : targetView. 성공시 로깅 추가

--- StoreApp/StoreApp/View/MyMainScrollView.swift
+++ StoreApp/StoreApp/View/MyMainScrollView.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
+import os.log
 
-class MyMainScrollView: UIScrollView {    
+class MyMainScrollView: UIScrollView {
     /// 뷰를 받아서 비율유지하면서 가로를 자신길이에 맞춘다
-    private func resize(addedView: UIView) -> CGSize {
+    private func resize(targetView: UIView) -> CGSize {
         // 결과용 사이즈 선언
         var resultSize = CGSize()
         
         // 비율 측정
-        let ratioFromAddedView = addedView.frame.height / addedView.frame.width
+        let ratioFromAddedView = targetView.frame.height / targetView.frame.width
         
         // 가로 설정
         resultSize.width = self.frame.width
@@ -28,9 +29,9 @@ class MyMainScrollView: UIScrollView {
     }
 
     /// 뷰를 받아서 해당 뷰 사이즈만큼 컨텐트 높이를 늘리고 입력뷰 사이즈 조절 후 마지막 뷰 밑에 위치시킨다
-    func get(addedView: UIView){
+    func add(targetView: UIView){
         // 추가뷰 프레임 사이즈 수정
-        addedView.frame.size = resize(addedView: addedView)
+        targetView.frame.size = resize(targetView: targetView)
         
         // 마지막 서브뷰의 바텀 선언
         var lastViewBottomValue = CGPoint(x: 0, y: 0)
@@ -45,12 +46,15 @@ class MyMainScrollView: UIScrollView {
         }
         
         // 추가뷰 y 좌표 수정
-        addedView.frame.origin = lastViewBottomValue
+        targetView.frame.origin = lastViewBottomValue
         
         // 마지막 뷰 밑에 추가
-        self.addSubview(addedView)
+        self.addSubview(targetView)
         
         // 컨텐트 높이 증가
-        self.contentSize.height += addedView.frame.height
+        self.contentSize.height += targetView.frame.height
+        
+        // 로깅
+        os_log("섹션이미지 추가 완료 : %@", fullPath)
     }
 }
