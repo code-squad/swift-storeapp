4f2dc592f3bca4613f74e33fd7ac83ce3ccf03de
DrakeYang <drake.the.programmer@gmail.com>
2019-04-24 15:19:57 +0900

    네트워크기능 이동

--- StoreApp/StoreApp/Model/StoreItemDetailMaker.swift
+++ StoreApp/StoreApp/Model/StoreItemDetailMaker.swift
@@ -10,8 +10,6 @@ import Foundation
 import os.log
 
 class StoreItemDetailMaker {
-    /// 슬랙 incoming hook URL
-    private let slackHookURL = "https://hooks.slack.com/services/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"
     
     /// json 배열을 받아서 스토어아이템 배열로 리턴
     func makeStoreItemDetailModel(anyData: Data) -> StoreItemDetailModel? {
@@ -44,65 +42,5 @@ class StoreItemDetailMaker {
         
         //결과리턴
         return detailModel
-    }
-    /// 주문결과를 받아서 JSON data 로 리턴한다
-    func makeJSONFrom(orderResult: OrderResult) -> Data? {
-        
-        let encoder = JSONEncoder()
-        
-        do {
-            let data = try encoder.encode(orderResult)
-            
-            return data
-        }
-        catch {
-            os_log("주문결과 JSON 변환 실패 : %@", orderResult.text)
-            return nil
-        }
-    }
-    
-    
-    /// 주문내역을 post 방식으로 보낸다
-    func order(orderResult: OrderResult){
-        // 주문결과를 받아서 JSON 화 한다
-        guard let orderResultJSON = makeJSONFrom(orderResult: orderResult) else { return }
-        
-        guard let orderResultDescription = String(data: orderResultJSON, encoding: .utf8) else { return }
-        
-        let uncheckedURL = self.slackHookURL
-        
-        // url 을 작성한다
-        guard let url = URL(string: uncheckedURL ) else { return () }
-        
-        // 리퀘스트 선언
-        var request = URLRequest(url: url)
-        
-        // 방식은 POST
-        request.httpMethod = "POST"
-        
-        request.httpBody = orderResultJSON
-        
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                os_log("error: %@", error as CVarArg)
-            } else {
-                if let response = response as? HTTPURLResponse {
-                    os_log("statusCode: %@", String(response.statusCode))
-                }
-                if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                    os_log("data: %@", dataString)
-                }
-            }
-        }
-        task.resume()
-    }
-}
-
-class OrderResult: Codable {
-    /// 주문결과 텍스트
-    let text : String
-    
-    init(customerName: String, price: String, menu: String){
-        self.text = "\(customerName) 님이 \(price)원 짜리 \(menu) 를 구입하셨습니다."
     }
 }
