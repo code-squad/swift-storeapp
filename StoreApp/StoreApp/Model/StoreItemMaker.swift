bb7bed120087a5a7e4a28083e16c8715c5583d91
DrakeYang <drake.the.programmer@gmail.com>
2019-04-24 15:19:57 +0900

    네트워크기능 이동

--- StoreApp/StoreApp/Model/StoreItemMaker.swift
+++ StoreApp/StoreApp/Model/StoreItemMaker.swift
@@ -10,8 +10,11 @@ import Foundation
 import os.log
 
 class StoreItemMaker {
+    /// 슬랙 incoming hook URL
+    static private let slackHookURL = "https://hooks.slack.com/services/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"
+    
     /// json 배열을 받아서 스토어아이템 배열로 리턴
-    static private func makeStoreItemList(jsonArray: Array<[String : Any]>) -> [StoreItem] {
+    class private func makeStoreItemList(jsonArray: Array<[String : Any]>) -> [StoreItem] {
         // 결과리턴배열
         var result : [StoreItem] = []
         
@@ -32,7 +35,7 @@ class StoreItemMaker {
     }
     
     /// jsonData 를 받아서 any 형으로 리턴. json 분석을 거친다.
-    static func jsonSerialization(jsonData: Data) -> Any? {
+    class func jsonSerialization(jsonData: Data) -> Any? {
         do {
             // 데이터 json 형태로 추출
             let jsonResult = try JSONSerialization.jsonObject(with: jsonData, options: .mutableLeaves)
@@ -46,7 +49,7 @@ class StoreItemMaker {
     }
     
     /// json data 를 받아서 스토어아이템 배열로 만들어서 리턴
-    static func makeStoreItemList(jsonData: Data) -> [StoreItem] {
+    class func makeStoreItemList(jsonData: Data) -> [StoreItem] {
         // 리턴용 변수
         var result : [StoreItem] = []
         // path 에서 추출시도
@@ -74,4 +77,68 @@ class StoreItemMaker {
             }
         return result
     }
+    /// 주문결과를 받아서 JSON data 로 리턴한다
+    class private func makeJSONFrom(orderResult: OrderResult) -> Data? {
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            let data = try encoder.encode(orderResult)
+            
+            return data
+        }
+        catch {
+            os_log("주문결과 JSON 변환 실패 : %@", orderResult.text)
+            return nil
+        }
+    }
+    
+    
+    /// 주문내역을 post 방식으로 보낸다
+    class func post(orderResult: OrderResult){
+        // 주문결과를 받아서 JSON 화 한다
+        guard let orderResultJSON = makeJSONFrom(orderResult: orderResult) else { return }
+        
+        guard let orderResultDescription = String(data: orderResultJSON, encoding: .utf8) else { return }
+        
+        let uncheckedURL = self.slackHookURL
+        
+        // url 을 작성한다
+        guard let url = URL(string: uncheckedURL ) else { return () }
+        
+        // 리퀘스트 선언
+        var request = URLRequest(url: url)
+        
+        // 방식은 POST
+        request.httpMethod = "POST"
+        
+        request.httpBody = orderResultJSON
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                os_log("error: %@", error as CVarArg)
+            } else {
+                if let response = response as? HTTPURLResponse {
+                    os_log("statusCode: %@", String(response.statusCode))
+                }
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    os_log("data: %@", dataString)
+                }
+            }
+        }
+        task.resume()
+    }
 }
+
+class OrderResult: Codable {
+    /// 주문결과 텍스트
+    let text : String
+    
+    init(customerName: String, price: String, menu: String){
+        self.text = "\(customerName) 님이 \(price)원 짜리 \(menu) 를 구입하셨습니다."
+        
+        os_log("%@", self.text)
+    }
+}
+
+
