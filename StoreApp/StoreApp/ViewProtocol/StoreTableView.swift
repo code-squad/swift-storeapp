8141f3bfb6088f6aad56118b0aaefc6057114d49
hngfu <djsl1018@naver.com>
2019-04-27 17:05:56 +0900

    [StoreTableView] 추가

---
+++ StoreApp/StoreApp/ViewProtocol/StoreTableView.swift
@@ -0,0 +1,13 @@
+//
+//  StoreTableView.swift
+//  StoreApp
+//
+//  Created by 조재흥 on 19. 4. 27..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+protocol StoreTableView {
+    func reload(section: Int)
+}
