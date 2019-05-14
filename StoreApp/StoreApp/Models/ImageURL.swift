50cb7d6faf554b50646a30eb68e2d27f52e1af3e
hngfu <djsl1018@naver.com>
2019-05-14 23:43:21 +0900

    [ImageURL] 생성

---
+++ StoreApp/StoreApp/Models/ImageURL.swift
@@ -0,0 +1,13 @@
+//
+//  ImageURL.swift
+//  StoreApp
+//
+//  Created by 조재흥 on 19. 5. 14..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct ImageURL {
+    let url: URL
+}
