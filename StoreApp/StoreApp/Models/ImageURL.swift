bc717f3a08ecf27baf95047b65efdd8030467562
hngfu <djsl1018@naver.com>
2019-05-15 00:41:26 +0900

    [ImageURL] downloadImage()가 성공했을때 행동을 제어하도록 수정

--- StoreApp/StoreApp/Models/ImageURL.swift
+++ StoreApp/StoreApp/Models/ImageURL.swift
@@ -17,6 +17,32 @@ struct ImageURL {
         let imagePath = cacheURL.appendingPathComponent(url.lastPathComponent)
         guard FileManager.default.fileExists(atPath: imagePath.path) == false else { return }
         let jsonFetcher = JSONDataFetcher()
-        jsonFetcher.downloadImage(with: url)
+        let downloadImage = { (fileURL: URL) -> Void in
+            try? FileManager.default.moveItem(at: fileURL, to: imagePath)
+            let userInfo = [UserInfoKey.imageName: self.url.lastPathComponent]
+            NotificationCenter.default.post(name: .imageDidDownload,
+                                            object: nil,
+                                            userInfo: userInfo)
+        }
+        jsonFetcher.download(with: url, successHandler: downloadImage)
+    }
+}
+
+extension JSONDataFetcher {
+    func download(with url: URL, successHandler: @escaping (URL) -> Void) {
+        URLSession.shared.downloadTask(with: url) { (fileURL, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let fileURL = fileURL,
+                let response = response as? HTTPURLResponse,
+                (200...299) ~= response.statusCode,
+                let cacheURL = URL.cachesDirectory() else { return }
+            
+            successHandler(fileURL)
+            
+            }.resume()
     }
 }
