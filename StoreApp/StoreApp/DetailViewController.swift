5d11f0edb2d984a71b0ee1ecc218c333fc737502
hngfu <djsl1018@naver.com>
2019-05-18 17:27:05 +0900

    [DetailViewController] 주문 버튼 연결

--- StoreApp/StoreApp/DetailViewController.swift
+++ StoreApp/StoreApp/DetailViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +19,6 @@ class DetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func touchUpOrderButton(_ sender: UIButton) {
     }
-    */
-
 }
