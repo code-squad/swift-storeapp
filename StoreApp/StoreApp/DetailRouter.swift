ddfbea7a70f0788351ddf01b2e446e14f580bf02
hngfu <djsl1018@naver.com>
2019-05-18 17:14:49 +0900

    [DetailRouter] 생성 DetailViewController의 생성 및 연결을 담당하는 객체

--- StoreApp/StoreApp/DetailRouter.swift
+++ StoreApp/StoreApp/DetailRouter.swift
@@ -6,11 +6,27 @@
 //  Copyright © 2019 hngfu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class DetailRouter {
     
-    weak var presenter: StorePresenter?
+    weak var viewController: StoreTableViewController?
     
+    init(storeViewController: StoreTableViewController?) {
+        self.viewController = storeViewController
+    }
     
+    static func assembleModule() -> UIViewController? {
+        let storyboard = UIStoryboard(name: StoryboardNames.detailViewController,
+                                      bundle: nil)
+        guard let navigationController = storyboard.instantiateViewController(withIdentifier: StoryboardNames.detailViewNavigationControllerID) as? UINavigationController,
+            let detailViewController = navigationController.topViewController as? DetailViewController else { return nil }
+        
+        return navigationController
+    }
+    
+    func presentViewController(detailHash: String) {
+        guard let detailViewController = DetailRouter.assembleModule() else { return }
+        self.viewController?.navigationController?.pushViewController(detailViewController, animated: true)
+    }
 }
