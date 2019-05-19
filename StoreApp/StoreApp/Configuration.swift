43f1f3f234cf3151f3398d85da6d2b98f045d970
hngfu <djsl1018@naver.com>
2019-05-19 17:32:50 +0900

    [Configuration.URL] 추가
    URL의 공통 부분을 저장할 객체

--- StoreApp/StoreApp/Configuration.swift
+++ StoreApp/StoreApp/Configuration.swift
@@ -8,14 +8,23 @@
 
 import Foundation
 
+//MARK: - UserInfoKey
 struct UserInfoKey {
     static let appendItems = "appendItems"
     static let indexPathWillReload = "indexPathWillReload"
     static let imageName = "imageName"
 }
 
-
+//MARK: - StoryboardNames
 struct StoryboardNames {
     static let detailViewController = "DetailViewController"
     static let detailViewControllerID = "detailViewController"
+}
+
+struct Configuration {
+    
+    //MARK: - Configuration.URL
+    struct URL {
+        static let storeItems = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/"
+    }
 }
