f0bedd069426894aa942128a9690902874fc4a36
hngfu <djsl1018@naver.com>
2019-05-19 17:34:45 +0900

    [StoreItems] 공통된 URL을 Configuration.URL에서 가져와서 사용하도록 수정

--- StoreApp/StoreApp/Models/StoreItems.swift
+++ StoreApp/StoreApp/Models/StoreItems.swift
@@ -56,8 +56,7 @@ class StoreItems {
 
 extension JSONDataFetcher {
     func fetchStoreItems(fileName: String, completion: @escaping ([StoreItem]) -> Void) {
-        let commonURL = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/"
-        guard let url = URL(string: commonURL + fileName) else { return }
+        guard let url = URL(string: Configuration.URL.storeItems + fileName) else { return }
         load(url: url) { (data) in
             let decoder = JSONDecoder()
             guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
