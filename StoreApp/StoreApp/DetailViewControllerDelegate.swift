cc128bd3a9c378fbb72769c9d6818af0c4148db8
hngfu <djsl1018@naver.com>
2019-05-19 13:35:19 +0900

    [DetailViewControllerDelegate] 생성
    주문완료시 해당 메시지를 POST하는 역할

---
+++ StoreApp/StoreApp/DetailViewControllerDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewControllerDelegate.swift
+//  StoreApp
+//
+//  Created by 조재흥 on 19. 5. 19..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+protocol DetailViewControllerDelegate {
+    func post(orderMessage: String)
+}
