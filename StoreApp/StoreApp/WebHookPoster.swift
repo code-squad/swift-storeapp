2a693426dc774d31efa54171689614c857daf505
hngfu <djsl1018@naver.com>
2019-05-23 19:03:03 +0900

    [WebHookPoster] 생성
    webhook에 post를 전담하는 객체

--- StoreApp/StoreApp/WebHookPoster.swift
+++ StoreApp/StoreApp/WebHookPoster.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+struct WebHookPoster {
+    func post(message: String) {
+        let session = URLSession.shared
+        guard let url = URL(string: Configuration.URL.webHookURL) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let json = ["text": message]
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: json, options: []) else { return }
+        let task = session.uploadTask(with: request, from: jsonData)
+        task.resume()
+    }
+}
