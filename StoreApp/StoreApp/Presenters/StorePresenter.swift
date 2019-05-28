69478e5ebce7f445d5ff521b56de81b3eabe26bb
hngfu <djsl1018@naver.com>
2019-05-28 16:02:35 +0900

    [StorePresenter] lock 제거

--- StoreApp/StoreApp/Presenters/StorePresenter.swift
+++ StoreApp/StoreApp/Presenters/StorePresenter.swift
@@ -25,9 +25,6 @@ class StorePresenter: NSObject {
     //MARK: Routers
     private let detailRouter: DetailRouter
     
-    //MARK: Helpers
-    private let storeItemsMutex = NSLock()
-    
     //MARK: - Methods
     //MARK: Initialization
     override init() {
@@ -56,11 +53,9 @@ class StorePresenter: NSObject {
             let userInfo = noti.userInfo,
             let appendItems = userInfo[UserInfoKey.appendItems] as? () -> Void else { return }
         
-        storeItemsMutex.lock()
-        appendItems()
         DispatchQueue.main.async {
+            appendItems()
             self.storeTableViewController?.reload(section: index)
-            self.storeItemsMutex.unlock()
         }
     }
     
