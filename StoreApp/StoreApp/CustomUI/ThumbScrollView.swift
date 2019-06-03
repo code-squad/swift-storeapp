6787bd99f48b0f109b0c01754a51404af6d17981
Hngfu <djsl1018@naver.com>
2019-06-03 21:25:59 +0900

    [ThumbScrollView] SDWebImage를 사용하도록 수정

--- StoreApp/StoreApp/CustomUI/ThumbScrollView.swift
+++ StoreApp/StoreApp/CustomUI/ThumbScrollView.swift
@@ -7,35 +7,35 @@
 //
 
 import UIKit
-import JSONDataFetcher
+import SDWebImage
 
 class ThumbScrollView: UIScrollView {
     
     func show(with imageURLs: [String]) {
         self.setContentSize(imageURLs.count)
         for (index, imageURL) in imageURLs.enumerated() {
-            let successHandler = { (data: Data) -> Void in
-                guard let image = UIImage(data: data) else { return }
-                DispatchQueue.main.async {
-                    let imageView = UIImageView(image: image)
-                    let xPosition = self.frame.width * CGFloat(index)
-                    imageView.frame = CGRect(x: xPosition,
-                                             y: 0,
-                                             width: self.frame.width,
-                                             height: self.frame.height)
-                    imageView.contentMode = .scaleAspectFill
-                    self.addSubview(imageView)
-                }
-            }
-            let jsonDataFetcher = JSONDataFetcher()
-            guard let url = URL(string: imageURL) else { continue }
-            jsonDataFetcher.load(url: url, completion: successHandler)
+            insertImage(to: index, with: imageURL)
         }
     }
     
     private func setContentSize(_ countOfImage: Int) {
         DispatchQueue.main.async {
             self.contentSize.width = self.frame.width * CGFloat(countOfImage)
+        }
+    }
+    
+    private func insertImage(to index: Int, with imageURL: String) {
+        guard let url = URL(string: imageURL) else { return }
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.sd_setImage(with: url)
+        DispatchQueue.main.async {
+            let xPosition = self.frame.width * CGFloat(index)
+            imageView.frame = CGRect(x: xPosition,
+                                     y: 0,
+                                     width: self.frame.width,
+                                     height: self.frame.height)
+            self.addSubview(imageView)
         }
     }
 }
