6d69398641d86b6cf5a1b655eb5a6315fea2ff3a
Hngfu <djsl1018@naver.com>
2019-06-10 21:32:45 +0900

    [BorderColorView] 추가

---
+++ StoreApp/StoreApp/ViewProtocol/BorderColorView.swift
@@ -0,0 +1,13 @@
+//
+//  BorderColorView.swift
+//  StoreApp
+//
+//  Created by 조재흥 on 19. 6. 10..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+protocol BorderColorView: AnyObject {
+    func change(borderColor: CGColor)
+}
