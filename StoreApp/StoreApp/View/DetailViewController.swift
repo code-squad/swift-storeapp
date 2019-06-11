ce12cdd295ecc8c85190e259976ef2e2917b5835
Hngfu <djsl1018@naver.com>
2019-06-11 17:28:18 +0900

    [ReachabilityViewController] 추가 및 StoreVC와 DetailVC가 상속받도록 수정

--- StoreApp/StoreApp/View/DetailViewController.swift
+++ StoreApp/StoreApp/View/DetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: ReachabilityViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
