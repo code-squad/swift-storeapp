3dd3b22e1458693790bf813f4419c72b19d3f3c3
kiljoonho <kiljh@me.com>
2019-09-07 00:17:34 +0900

    URL 을 저장할 enum 타입 생성

---
+++ StoreApp/StoreApp/URLString.swift
@@ -0,0 +1,13 @@
+//
+//  URL.swift
+//  StoreApp
+//
+//  Created by joon-ho kil on 9/6/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import Foundation
+
+enum URLString: String {
+    case StoreItem = "http://public.codesquad.kr/jk/storeapp/main.json"
+}
