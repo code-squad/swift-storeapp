01a90877de4de59b7a00507ff564562a26e6d570
kiljoonho <kiljh@me.com>
2019-09-06 13:40:55 +0900

    프로젝트 생성

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by joon-ho kil on 9/6/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
