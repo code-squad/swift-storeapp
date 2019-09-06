e3abb1f4d3472967ee1132cb09373b40a2cf22ed
kiljoonho <kiljh@me.com>
2019-09-07 00:19:00 +0900

    json 받아오는 소스 구현

--- StoreApp/StoreApp/ViewController.swift
+++ StoreApp/StoreApp/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var storeItems: StoreItems?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        storeItems = JsonReceiver.getJson()
+        
     }
-
-
 }
 
