40e0facedfe061cf60dc1f0cbb4f1caa16a077a8
kiljoonho <kiljh@me.com>
2019-09-07 00:54:41 +0900

    테이블뷰 데이터소스, 델리게이트 구현

--- StoreApp/StoreApp/ViewController.swift
+++ StoreApp/StoreApp/ViewController.swift
@@ -8,14 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var storeItems: StoreItems?
+    @IBOutlet weak var storeItemTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         storeItems = JsonReceiver.getJson()
+        storeItemTableView.delegate = self
+        storeItemTableView.dataSource = self
+        storeItemTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return storeItems?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "storeItemCell", for: indexPath) as! StoreItemTableViewCell
         
+        if let storeItem = storeItems?[indexPath.row] {
+            cell.putInfo(storeItem)
+        }
+        
+        return cell
     }
 }
 
