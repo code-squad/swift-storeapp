9ea068746f2092198cdb5b0ce033acdc5b3ce14d
kiljoonho <kiljh@me.com>
2019-09-07 00:17:50 +0900

    json 을 받아오는 객체 생성

---
+++ StoreApp/StoreApp/JsonReceiver.swift
@@ -0,0 +1,42 @@
+//
+//  JsonReceiver.swift
+//  StoreApp
+//
+//  Created by joon-ho kil on 9/6/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import Foundation
+
+struct JsonReceiver {
+    static func getJson() -> StoreItems {
+        var storeItems = StoreItems()
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        guard let url = URL(string: URLString.StoreItem.rawValue) else {
+            return storeItems
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                print("load data failed")
+                return
+            }
+            
+            print(data)
+            
+            let decoder: JSONDecoder = JSONDecoder()
+            do {
+                storeItems =  try decoder.decode(StoreItems.self, from: data)
+                semaphore.signal()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+        semaphore.wait()
+        
+        return storeItems
+    }
+}
+
