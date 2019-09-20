30631558e09f58c84a32bed618cf1919d1b7d076
kiljoonho <kiljh@me.com>
2019-09-20 14:47:05 +0900

    기존 가격 나오도록 구현

--- StoreApp/StoreApp/View/StoreItemTableViewCell.swift
+++ StoreApp/StoreApp/View/StoreItemTableViewCell.swift
@@ -12,6 +12,7 @@ class StoreItemTableViewCell: UITableViewCell {
     @IBOutlet weak var Title: UILabel!
     @IBOutlet weak var Description: UILabel!
     @IBOutlet weak var Price: UILabel!
+    @IBOutlet weak var nPrice: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,11 +23,16 @@ class StoreItemTableViewCell: UITableViewCell {
         Title.text = nil
         Description.text = nil
         Price.text = nil
+        nPrice.text = nil
     }
     
     func putInfo(_ storeItem: StoreItem) {
         self.Title.text = storeItem.title
         self.Description.text = storeItem.storeItemDescription
         self.Price.text = storeItem.sPrice
+        
+        if let nPrice = storeItem.nPrice {
+            self.nPrice.text = nPrice
+        }
     }
 }
