488169a2c3b8900d90a16f6ad30c6895bda419d1
YukJiSoo <wltn3231@naver.com>
2019-10-20 23:53:57 +0900

    Create project

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by 육지수 on 10/20/19.
+//  Copyright © 2019 JSYuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
