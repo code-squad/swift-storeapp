1f5616745d2fb3ba5e9be1b799f794dfa834ea80
YukJiSoo <wltn3231@naver.com>
2019-10-21 02:24:10 +0900

    Feat: ItemHeader view 구현

---
+++ StoreApp/StoreApp/View/ItemHeaderCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemHeaderCell.swift
+//  StoreApp
+//
+//  Created by 육지수 on 10/21/19.
+//  Copyright © 2019 JSYuk. All rights reserved.
+//
+
+import UIKit
+
+class ItemHeaderCell: UITableViewCell {
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
