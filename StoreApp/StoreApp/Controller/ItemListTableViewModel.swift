a351639df0605ae1e9867e199eca9780adc8c7cf
YukJiSoo <wltn3231@naver.com>
2019-10-22 00:32:32 +0900

    Refactor: model 역할 확장 / tableViewDataSource 분리

---
+++ StoreApp/StoreApp/Controller/ItemListTableViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ItemListTableViewModel.swift
+//  StoreApp
+//
+//  Created by 육지수 on 10/22/19.
+//  Copyright © 2019 JSYuk. All rights reserved.
+//
+
+import UIKit
+
+class ItemListTableViewModel: NSObject, UITableViewDataSource {
+    var itemList = [StoreItem]()
+
+    override init() {
+        if let itemList = StoreItem.getAllStoreItem() {
+            self.itemList = itemList
+        }
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let itemCell = tableView.dequeueReusableCell(withIdentifier: "StoreItemCell", for: indexPath) as? StoreItemCell else {
+            return UITableViewCell()
+        }
+
+        let item = self.itemList[indexPath.row]
+        itemCell.configure(with: item)
+        return itemCell
+    }
+}
