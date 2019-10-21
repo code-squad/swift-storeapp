61b97d34ffcce175c37726470174d4ba0d56181c
corykim0829 <corykim0829@gmail.com>
2019-10-22 03:12:08 +0900

    feat: badge

---
+++ StoreApp/StoreApp/Views/CustomBadgeTextView.swift
@@ -0,0 +1,29 @@
+//
+//  CustomBadgeTextView.swift
+//  StoreApp
+//
+//  Created by Cory Kim on 2019/10/22.
+//  Copyright © 2019 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class CustomBadgeTextView: UITextView {
+
+    init(text: String) {
+        super.init(frame: .zero, textContainer: nil)
+        self.text = text
+        textAlignment = .center
+        isScrollEnabled = false
+        isEditable = false
+        font = .systemFont(ofSize: 14)
+        textColor = .white
+        backgroundColor = .white
+        textContainerInset = .init(top: 2, left: 2, bottom: 2, right: 2)
+        sizeToFit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- StoreApp/StoreApp/Views/StoreItemCell.swift
+++ StoreApp/StoreApp/Views/StoreItemCell.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
+enum Badge {
+    case EventPrice, FreeGift, SpecialPrice, SoldOut, UNKNOWN
+    static func fromString(_ badgeString: String) -> Badge {
+        switch badgeString {
+        case "이벤트특가": return .EventPrice
+        case "사은품증정": return .FreeGift
+        case "론칭특가": return .SpecialPrice
+        case "품절": return .SoldOut
+        default: return .UNKNOWN
+        }
+    }
+}
+
 class StoreItemCell: UITableViewCell {
 
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var normalPriceLabel: UILabel!
+    @IBOutlet weak var badgeStackView: UIStackView!
     
     var item: StoreItem! {
         didSet {
@@ -31,6 +45,34 @@ class StoreItemCell: UITableViewCell {
             }
             attributedString.append(NSAttributedString(string: "\(spaceBetweenPrices)\(item.sPrice)", attributes: [.font: UIFont.systemFont(ofSize: 17, weight: .heavy), .foregroundColor: #colorLiteral(red: 0.1703471243, green: 0.7560165524, blue: 0.737252295, alpha: 1)]))
             self.normalPriceLabel.attributedText = attributedString
+            
+            if let badges = item.badge {
+                var badgeViews = [UITextView]()
+                
+                badges.forEach { (badgeString) in
+                    let badgeTextView = CustomBadgeTextView(text: badgeString)
+                    
+                    switch Badge.fromString(badgeString) {
+                    case .EventPrice:
+                        badgeTextView.backgroundColor = #colorLiteral(red: 0.6633180976, green: 0.4506880641, blue: 0.7471640706, alpha: 1)
+                    case .FreeGift:
+                        badgeTextView.backgroundColor = #colorLiteral(red: 0.9738679528, green: 0.7125977874, blue: 0, alpha: 1)
+                    case .SpecialPrice:
+                        badgeTextView.backgroundColor = #colorLiteral(red: 0.6633180976, green: 0.4506880641, blue: 0.7471640706, alpha: 1)
+                    case .SoldOut:
+                        badgeTextView.backgroundColor = #colorLiteral(red: 0.1999788582, green: 0.2000134587, blue: 0.1999712586, alpha: 1)
+                    case .UNKNOWN: break
+                    @unknown default: break
+                    }
+                    
+                    badgeViews.append(badgeTextView)
+                }
+                badgeStackView.arrangedSubviews.forEach({ $0.removeFromSuperview() })
+                badgeViews.forEach({ badgeStackView.addArrangedSubview($0)})
+            } else {
+                badgeStackView.arrangedSubviews.forEach({ $0.removeFromSuperview() })
+                badgeStackView.addArrangedSubview(CustomBadgeTextView(text: "none"))
+            }
         }
     }
     
