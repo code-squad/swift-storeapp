ad81488665d1575ce61ad9e1d34f7ebd0cc2a647
corykim0829 <corykim0829@gmail.com>
2019-10-23 15:15:42 +0900

    reflect feedbacks

--- StoreApp/StoreApp/Views/CustomBadgeTextView.swift
+++ StoreApp/StoreApp/Views/CustomBadgeTextView.swift
@@ -24,6 +24,20 @@ class CustomBadgeTextView: UITextView {
         sizeToFit()
     }
     
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        self.text = "NONE"
+        textAlignment = .center
+        isScrollEnabled = false
+        isEditable = false
+        isSelectable = false
+        font = .systemFont(ofSize: 14)
+        textColor = UIColor(named: "BadgeWhite")
+        backgroundColor = UIColor(named: "BadgeWhite")
+        textContainerInset = .init(top: 2, left: 2, bottom: 2, right: 2)
+        sizeToFit()
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
