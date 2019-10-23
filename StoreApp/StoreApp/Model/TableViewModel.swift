5596985e2c25f585269a584cbdfa42e6d18214aa
corykim0829 <corykim0829@gmail.com>
2019-10-23 21:15:36 +0900

    feat: decoding json files

--- StoreApp/StoreApp/Model/TableViewModel.swift
+++ StoreApp/StoreApp/Model/TableViewModel.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class TableViewModel: NSObject, UITableViewDataSource {
+    private var sections = Array<Array<StoreItem>>()
     private var storeItems = Array<StoreItem>()
     
     override init() {
@@ -18,33 +19,48 @@ class TableViewModel: NSObject, UITableViewDataSource {
     }
     
     fileprivate func decodeJSON() {
-        guard let url = Bundle.main.url(forResource: "main", withExtension: "json") else { return }
-        
-        URLSession.shared.dataTask(with: url) { (data, _, err) in
-            if let err = err {
-                print("Failed to request data", err)
-            }
+        let jsonFilenames = ["main", "soup", "side"]
+        let jsonUrls = jsonFilenames.map { Bundle.main.url(forResource: $0, withExtension: "json") }
+        jsonUrls.forEach { (url) in
+            guard let url = url else { return }
             
-            guard let data = data else { return }
-            
-            let jsonDecoder = JSONDecoder()
-            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-            
-            do {
-                self.storeItems = try jsonDecoder.decode([StoreItem].self, from: data)
-            } catch let jsonErr {
-                print("Error in Serialization", jsonErr)
-            }
-        }.resume()
+            URLSession.shared.dataTask(with: url) { (data, _, err) in
+                if let err = err {
+                    print("Failed to request data", err)
+                }
+                
+                guard let data = data else { return }
+                
+                let jsonDecoder = JSONDecoder()
+                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                do {
+                    self.sections.append(try jsonDecoder.decode([StoreItem].self, from: data))
+                } catch let jsonErr {
+                    print("Error in Serialization", jsonErr)
+                }
+            }.resume()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "headerCell") as! StoreItemHeaderCell
+        cell.setupHeader()
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return storeItems.count
+        return sections[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! StoreItemCell
-        cell.item = storeItems[indexPath.row]
+        cell.item = sections[indexPath.section][indexPath.row]
+//        cell.item = storeItems[indexPath.row]
         cell.selectionStyle = .none
         return cell
     }
