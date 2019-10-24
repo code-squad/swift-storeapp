a3c22ac09e2804fbe93469d46f05da4f2c26e619
hongdonghyun <hong4367@gmail.com>
2019-10-24 18:42:29 +0900

    collection view error

--- StoreApp/StoreApp/CustomCell.swift
+++ StoreApp/StoreApp/CustomCell.swift
@@ -15,28 +15,48 @@ class ItemCell: UITableViewCell {
     @IBOutlet weak var labelDescription: UILabel!
     @IBOutlet weak var labelSprice: UILabel!
     @IBOutlet weak var labelNprice: UILabel!
-    @IBOutlet weak var BadgeCollectionView: UICollectionView!
-    var items: [StoreItem]!
-    func config(model:StoreItem) {
-        self.items = model
+    @IBOutlet weak var badgeCollectionView: UICollectionView!
+    var item: StoreItem!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
     }
 }
 
-extension ItemCell{
+extension String {
+    func strikeThrough() -> NSAttributedString {
+        let attributeString =  NSMutableAttributedString(string: self)
+        attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0,attributeString.length))
+        return attributeString
+    }
+}
 
+extension ItemCell {
+    func config(model:StoreItem) {
+        self.item = model
+        self.badgeCollectionView.reloadData()
+    }
 }
 
 extension ItemCell: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print("123123123")
-        print("")
-        
-//        return 1
+        guard let models = self.item,
+            let badgeItems = models.badge else { return 0 }
+        return badgeItems.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ItemEventBadgeCell", for: indexPath) as? ItemEventBadgeCell,
+            let array = self.item.badge else { return ItemEventBadgeCell() }
+        self.labelTitle.text = self.item.title
+        self.labelTitle.text = self.item.title
+        self.labelDescription.text = item.Description
+        self.labelNprice.text = item.nPrice
+        self.labelNprice.attributedText = self.labelNprice.text?.strikeThrough()
+        self.labelSprice.text = item.sPrice
+        return cell
+        
     }
 }
