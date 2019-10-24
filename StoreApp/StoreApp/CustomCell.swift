ba98440924e7c51ee59b11272bf537fb69c219f7
hongdonghyun <hong4367@gmail.com>
2019-10-24 17:29:50 +0900

    Test

--- StoreApp/StoreApp/CustomCell.swift
+++ StoreApp/StoreApp/CustomCell.swift
@@ -15,5 +15,28 @@ class ItemCell: UITableViewCell {
     @IBOutlet weak var labelDescription: UILabel!
     @IBOutlet weak var labelSprice: UILabel!
     @IBOutlet weak var labelNprice: UILabel!
+    @IBOutlet weak var BadgeCollectionView: UICollectionView!
+    var items: [StoreItem]!
+    func config(model:StoreItem) {
+        self.items = model
+    }
+}
+
+extension ItemCell{
+
+}
+
+extension ItemCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("123123123")
+        print("")
+        
+//        return 1
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        
+        return UICollectionViewCell()
+    }
 }
