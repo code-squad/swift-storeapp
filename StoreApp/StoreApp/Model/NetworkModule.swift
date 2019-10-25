fb6d034c828650b44970e00a52b72573b0998285
corykim0829 <corykim0829@gmail.com>
2019-10-25 21:31:18 +0900

    refactor: getting json Data, fix: header rendering error

--- StoreApp/StoreApp/Model/NetworkModule.swift
+++ StoreApp/StoreApp/Model/NetworkModule.swift
@@ -18,34 +18,30 @@ class NetworkModule {
     private let session = URLSession.shared
     
     let jsonFilenames = ["main", "soup", "side"]
-    let baseUrl = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/"
     
     init() {
         getData()
     }
     
     func getData() {
-        let URLComponentsArray = jsonFilenames.map({ baseUrl + $0 }).map({ URLComponents(string: $0) })
-        
-        URLComponentsArray.map({ $0?.url }).forEach { (url) in
-            guard let url = url else { return }
+        jsonFilenames.forEach { (name) in
+            let baseUrl = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/"
+            guard let url = URL(string: baseUrl + name) else { return }
             
             session.dataTask(with: url) { (data, response, error) in
-                guard let data = data else { return }
-                
-                let jsonDecoder = JSONDecoder()
-                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-                
-                do {
-                    let baseAPIResponse = try jsonDecoder.decode(BaseAPIResponse<StoreItem>.self, from: data)
-//                    self.storeItemsArray.append(baseAPIResponse.body)
-                    let userInfoDict = ["storeItems": baseAPIResponse.body]
-                    NotificationCenter.default.post(name: NSNotification.Name(rawValue: "getData"), object: nil, userInfo: userInfoDict)
-                
-                } catch let jsonErr {
-                    print("Error in decoding json", jsonErr)
-                }
-            }.resume()
+                            guard let data = data else { return }
+                            
+                            let jsonDecoder = JSONDecoder()
+                            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+                            
+                            do {
+                                let baseAPIResponse = try jsonDecoder.decode(BaseAPIResponse<StoreItem>.self, from: data)
+                                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "getData"), object: nil, userInfo: ["json": baseAPIResponse.body, "section": name])
+                            
+                            } catch let jsonErr {
+                                print("Error in decoding json", jsonErr)
+                            }
+                        }.resume()
         }
     }
 }
