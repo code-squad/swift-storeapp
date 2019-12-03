9ad5a25c145871181c5ff87aa3c0864bfa360f95
부엉이 <o.o.wl.soft@gmail.com>
2019-12-04 04:07:38 +0900

    feat: SceneDeleagate 에서 ViewController 생성하는 코드 추가 #2

--- StoreApp/StoreApp/SceneDelegate.swift
+++ StoreApp/StoreApp/SceneDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Then
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,7 +18,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = UINavigationController().then {
+            $0.viewControllers.append(StoreListViewController())
+        }
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ StoreApp/StoreApp/View/StoreItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoreItemCell.swift
+//  StoreApp
+//
+//  Created by 이동영 on 2019/12/04.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import UIKit
+
+class StoreItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
