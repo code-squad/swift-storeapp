e335e4b79c74300c42455718bc53f01d10b3b7da
부엉이 <o.o.wl.soft@gmail.com>
2019-12-04 04:08:06 +0900

    feat: Attribute & Layout 구성 코드 추가 #1

--- StoreApp/StoreApp/ViewController/StoreListViewController.swift
+++ StoreApp/StoreApp/ViewController/StoreListViewController.swift
@@ -7,16 +7,46 @@
 //
 
 import UIKit
+import Then
 
 class StoreListViewController: UIViewController {
-
+    
     // MARK: - UI
     
+    private let storeTableView = UITableView(frame: .zero)
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        setUpAttribute()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        setUpLayout()
     }
 
+}
+
+// MARK: - Attrubutes & Layouts
+
+extension StoreListViewController {
+    
+    private func setUpAttribute() {
+        self.view.do {
+            $0.addSubview(storeTableView)
+        }
+    }
+    
+    private func setUpLayout() {
+        storeTableView.frame = self.view.safeAreaLayoutGuide.layoutFrame
+    }
 }
 
