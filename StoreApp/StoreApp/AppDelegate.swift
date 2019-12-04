d00f25ed6e4d9efb8a6e4e2fde8c84173ccc1930
cmindy <cmindy.soft@gmail.com>
2019-12-04 16:43:55 +0900

    chore: 초기 프로젝트 추가

---
+++ StoreApp/StoreApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  StoreApp
+//
+//  Created by CHOMINJI on 2019/12/04.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
