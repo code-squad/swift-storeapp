9636d9bc19e9de0a51b228121becba9aa5c81631
부엉이 <o.o.wl.soft@gmail.com>
2019-12-04 17:01:58 +0900

    feat: Cell & Header View 추가

---
+++ StoreApp/StoreApp/View/MenuCell.swift
@@ -0,0 +1,21 @@
+//
+//  StoreItemCell.swift
+//  StoreApp
+//
+//  Created by 이동영 on 2019/12/04.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import UIKit
+
+final class MenuCell: UITableViewCell {
+
+    // MARK: - UI
+    
+    private let menuImageView = UIImageView()
+    private let titleLabel = UILabel()
+    private let detailLabel = UILabel()
+    private let priceLabel = UILabel()
+    
+    
+}
