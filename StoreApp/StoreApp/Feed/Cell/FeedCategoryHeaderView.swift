805163c4296fc7697da682306ea515c14da3afd8
cmindy <cmindy.soft@gmail.com>
2019-12-05 21:15:01 +0900

    feat: 피드의 커스텀 카테고리 헤더뷰 추가

---
+++ StoreApp/StoreApp/Feed/Cell/FeedCategoryHeaderView.swift
@@ -0,0 +1,45 @@
+//
+//  FeedCategoryHeaderView.swift
+//  StoreApp
+//
+//  Created by CHOMINJI on 2019/12/05.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class FeedCategoryHeaderView: UITableViewHeaderFooterView {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setUpAttributes()
+    }
+}
+
+// MARK: - Attributes
+
+extension FeedCategoryHeaderView {
+    private func setUpAttributes() {
+        setUpLabels()
+    }
+    
+    private func setUpLabels() {
+        categoryLabel.font = UIFont.systemFont(ofSize: 13.0, weight: .light)
+        categoryLabel.textColor = .lightGray
+        categoryLabel.textAlignment = .center
+        categoryLabel.layer.borderColor = UIColor.lightGray.cgColor
+        categoryLabel.layer.borderWidth = 1.0
+        
+        descriptionLabel.font = UIFont.systemFont(ofSize: 20.0, weight: .bold)
+        descriptionLabel.textColor = .black
+        descriptionLabel.textAlignment = .center
+    }
+}
