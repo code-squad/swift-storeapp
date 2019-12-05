2a87ab22347a850a8bfc2f74e311dd154f72a369
cmindy <cmindy.soft@gmail.com>
2019-12-05 15:20:22 +0900

    feat: FeedListCell을 테이블 뷰에 보여주는 기능 추가

---
+++ StoreApp/StoreApp/Extensions/ReuseIdentifiers.swift
@@ -0,0 +1,21 @@
+//
+//  ReuseIdentifiers.swift
+//  StoreApp
+//
+//  Created by CHOMINJI on 2019/12/05.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableID {
+    static var reuseID: String { get }
+}
+
+extension ReusableID {
+    static var reuseID: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: ReusableID { }
