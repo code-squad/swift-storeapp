7acf521506784e6d8d78feba313522553de4075d
cmindy <cmindy.soft@gmail.com>
2019-12-05 20:26:59 +0900

    ui: FeedListCell의 아울렛을 연결하고 디자인에 따라 속성 추가

--- StoreApp/StoreApp/View/Cell/FeedListCell.swift
+++ StoreApp/StoreApp/View/Cell/FeedListCell.swift
@@ -9,16 +9,46 @@
 import UIKit
 
 class FeedListCell: UITableViewCell {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        setUpAttributes()
     }
+}
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+// MARK: - Attributes
 
-        // Configure the view for the selected state
+extension FeedListCell {
+    private func setUpAttributes() {
+        setUpImageView()
+        setUpLabels()
     }
     
+    private func setUpImageView() {
+        thumbImageView.contentMode = .scaleAspectFill
+        thumbImageView.backgroundColor = .lightGray
+    }
+    
+    private func setUpLabels() {
+        titleLabel.font = UIFont.systemFont(ofSize: 16.0, weight: .bold)
+        titleLabel.textColor = .black
+        titleLabel.numberOfLines = 1
+        
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13.0, weight: .regular)
+        descriptionLabel.textColor = .darkGray
+        
+        priceLabel.font = UIFont.systemFont(ofSize: 16.0, weight: .bold)
+        priceLabel.textColor = StoreColor.teal
+    }
+}
 }
