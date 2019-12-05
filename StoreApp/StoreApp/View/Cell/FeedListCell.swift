939abbf27c565f1ca9440c676615216c937f20c1
cmindy <cmindy.soft@gmail.com>
2019-12-05 15:01:06 +0900

    ui: 음식을 표시하는 FeedListCell과 오토레이아웃 추가

---
+++ StoreApp/StoreApp/View/Cell/FeedListCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedListCell.swift
+//  StoreApp
+//
+//  Created by CHOMINJI on 2019/12/04.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class FeedListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
