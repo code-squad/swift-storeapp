dd9a84e6dfa786b5aec82c2de5dabd38df40f867
부엉이 <o.o.wl.soft@gmail.com>
2019-12-05 20:52:17 +0900

    feat: DataAsset 추가 #3

---
+++ StoreApp/StoreApp/View/BadgeView.swift
@@ -0,0 +1,21 @@
+//
+//  BadgeView.swift
+//  StoreApp
+//
+//  Created by 이동영 on 2019/12/04.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import UIKit
+
+class BadgeView: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
