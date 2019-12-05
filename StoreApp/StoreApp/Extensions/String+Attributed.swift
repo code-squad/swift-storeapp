9882aa31d13f785905ec04114398b17b86abed5b
cmindy <cmindy.soft@gmail.com>
2019-12-05 20:19:39 +0900

    feat: 취소줄스타일을 위한 AttributedString extension 추가

---
+++ StoreApp/StoreApp/Extensions/String+Attributed.swift
@@ -0,0 +1,33 @@
+//
+//  String+Attributed.swift
+//  StoreApp
+//
+//  Created by CHOMINJI on 2019/12/05.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func centerLineStyle(text: String,
+               changeText: String,
+               centerLine: Bool?,
+               font: UIFont?,
+               color: UIColor?) -> NSAttributedString {
+        let range = (text as NSString).range(of: changeText)
+        let attributedString = NSMutableAttributedString(string: text)
+        
+        if let _ = centerLine {
+            attributedString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: range)
+        }
+        
+        if let font = font {
+            attributedString.addAttribute(NSAttributedString.Key.font, value: font, range: range)
+        }
+        
+        if let color = color {
+            attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: color , range: range)
+        }
+        return attributedString
+    }
+}
