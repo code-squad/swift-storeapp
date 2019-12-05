cddd52079922ebd3b248559f6de10b15ac8b6f75
cmindy <cmindy.soft@gmail.com>
2019-12-05 19:56:29 +0900

    chore: 컬러칩 추가

---
+++ StoreApp/StoreApp/StoreColor.swift
@@ -0,0 +1,15 @@
+//
+//  StoreColor.swift
+//  StoreApp
+//
+//  Created by CHOMINJI on 2019/12/05.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+enum StoreColor {
+    static let teal = UIColor(red: 41/255, green: 182/255, blue: 174/255, alpha: 1.0)
+    static let purple = UIColor(red: 151/255, green: 91/255, blue: 177/255, alpha: 1.0)
+    static let yellow = UIColor(red: 245/255, green: 168/255, blue: 9/255, alpha: 1.0)
+}
