8c2a3ae45bf13c5c4d675701619334da39a40209
부엉이 <o.o.wl.soft@gmail.com>
2019-12-05 20:53:00 +0900

    feat: Reuseable 프로토콜 기본 구현

--- StoreApp/StoreApp/Common/Extensions/Reusable.swift
+++ StoreApp/StoreApp/Common/Extensions/Reusable.swift
@@ -12,3 +12,10 @@ protocol Reusable {
     
     static var reuseId: String { get }
 }
+
+extension Reusable {
+    
+    static var reuseId: String {
+        return String(describing: self)
+    }
+}
