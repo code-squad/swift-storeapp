cdd08ea0d91c91d505b0270b1faef11546b08efd
부엉이 <o.o.wl.soft@gmail.com>
2019-12-05 21:00:27 +0900

    feat: Cell setUpConstraints 구현 #1

--- StoreApp/StoreApp/View/MenuCell.swift
+++ StoreApp/StoreApp/View/MenuCell.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-final class MenuCell: UITableViewCell {
-
+final class MenuCell: UITableViewCell, Reusable {
+    
     // MARK: - UI
     
     private let menuImageView = UIImageView()
@@ -82,4 +82,41 @@ extension MenuCell {
         }
     }
     
+    private func setUpConstraints() {
+        menuImageView.snp.updateConstraints {
+            $0.width.equalToSuperview().dividedBy(4)
+            $0.top.leading.bottom.equalToSuperview().inset(10)
+            $0.height.equalTo(menuImageView.snp.width)
+        }
+        
+        titleLabel.snp.updateConstraints {
+            $0.top.trailing.equalToSuperview().inset(10)
+            $0.leading.equalTo(menuImageView.snp.trailing).offset(10)
+            $0.height.equalTo(menuImageView.snp.height).dividedBy(5)
+        }
+
+        detailLabel.snp.updateConstraints {
+            $0.leading.height.trailing.equalTo(titleLabel)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+        }
+
+        priceLabel.snp.updateConstraints {
+            $0.leading.equalTo(titleLabel)
+            $0.height.equalTo(titleLabel)
+            $0.top.equalTo(detailLabel.snp.bottom).offset(4)
+            $0.width.equalTo(titleLabel).dividedBy(4)
+        }
+
+        discountedPriceLabel.snp.updateConstraints {
+            $0.leading.equalTo(priceLabel.snp.trailing).offset(10)
+            $0.height.equalTo(titleLabel)
+            $0.top.equalTo(detailLabel.snp.bottom).offset(4)
+            $0.width.equalTo(titleLabel).dividedBy(4)
+        }
+        
+        badgesStackView.snp.updateConstraints {
+            $0.height.leading.trailing.equalTo(titleLabel)
+            $0.top.equalTo(priceLabel.snp.bottom).offset(5)
+        }
+    }
 }
