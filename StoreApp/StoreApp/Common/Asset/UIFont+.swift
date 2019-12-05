451f9f37ca07210f0de4a9c4ec5bb169110dcf80
부엉이 <o.o.wl.soft@gmail.com>
2019-12-05 20:53:28 +0900

    feat: Semenic Color & Font 추가 #2

--- StoreApp/StoreApp/Common/Asset/UIFont+.swift
+++ StoreApp/StoreApp/Common/Asset/UIFont+.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2019 이동영. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    
+    static let titleFont = UIFont.boldSystemFont(ofSize: 18)
+    static let detailFont = UIFont.systemFont(ofSize: 15)
+    static let priceFont = UIFont.systemFont(ofSize: 14)
+    static let discountedPriceFont = UIFont.boldSystemFont(ofSize: 17)
+    static let badgeFont = UIFont.systemFont(ofSize: 12)
+}
