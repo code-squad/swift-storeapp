2bee8c6c0acf1e006c8c47e0ed527b0d85eecacb
부엉이 <o.o.wl.soft@gmail.com>
2019-12-06 13:32:52 +0900

    feat: Menu 모델 추가

--- StoreApp/StoreApp/Model/Menu.swift
+++ StoreApp/StoreApp/Model/Menu.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import Then
+
+// MARK: - Menu
+
+struct Menu: Decodable {
+    
+    // MARK: - Properties
+    
+    let imageURL: String
+    let deliveryType: [DeliveryType]
+    let title: String
+    let detail: String
+    let price: String?
+    let discountedPrice: String
+    let badge: [String]?
+
+    // MARK: - CodingKeys
+    
+    enum CodingKeys: String, CodingKey {
+        case imageURL
+        case deliveryType = "delivery_type"
+        case title
+        case detail = "description"
+        case price = "n_price"
+        case discountedPrice = "s_price"
+        case badge
+    }
+}
+
+// MARK: - Then
+
+extension Menu: Then {}
+
+// MARK: - DeliveryType
+
+enum DeliveryType: String, Decodable {
+    
+    case dawnShipping = "새벽배송"
+    case nationalShipping = "전국택배"
+}
+
