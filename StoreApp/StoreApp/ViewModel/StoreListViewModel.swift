1fcda8f9f44db74ce2592faac1cfbc5c61d4a4a1
부엉이 <o.o.wl.soft@gmail.com>
2019-12-07 00:33:56 +0900

    feat: Bindable 채택 및 구현

--- StoreApp/StoreApp/ViewModel/StoreListViewModel.swift
+++ StoreApp/StoreApp/ViewModel/StoreListViewModel.swift
@@ -7,6 +7,10 @@
 //
 
 import Foundation
+import UIKit
+
+typealias Store = (title: String, menus: [Menu])
+
 protocol StoreListViewBindable: AnyObject {
     
     var dataDidLoad: (() -> Void)? { get set }
@@ -19,3 +23,47 @@ protocol StoreListViewBindable: AnyObject {
     subscript(menu indexPath: IndexPath) -> Menu { get }
 }
 
+final class StoreListViewModel: StoreListViewBindable {
+    
+    // MARK: - Dependencies
+    
+    private let service: DataServable
+    
+    // MARK: - Properties
+    
+    private var stores: [Store] = []
+    
+    // MARK: - Status Closure
+    
+    var dataDidLoad: (() -> Void)?
+    var dataDidUpadated: (() -> Void)?
+    
+    // MARK: - Initializer
+    
+    init(service: DataServable) {
+        self.service = service
+    }
+    
+    // MARK: - Methods
+    
+    func numOfCategories() -> Int {
+        return stores.count
+    }
+    
+    func numOfMenusInCategory(_ category: Int) -> Int {
+        return stores[category].menus.count
+    }
+    
+    // MARK: - Subscripts
+    
+    subscript(category index: Int) -> String {
+        let store = stores[index]
+        return store.title
+    }
+    
+    subscript(menu indexPath: IndexPath) -> Menu {
+        let store = stores[indexPath.section]
+        return store.menus[indexPath.row]
+    }
+    
+}
