d664fa81532890b58bfbd8e82b55f2df4fd4eff7
부엉이 <o.o.wl.soft@gmail.com>
2019-12-06 22:53:07 +0900

    feat: Test용 StubService 추가

---
+++ StoreApp/StoreApp/Network/Testing/StubDataService.swift
@@ -0,0 +1,24 @@
+//
+//  StubService.swift
+//  StoreApp
+//
+//  Created by 이동영 on 2019/12/06.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+import Foundation
+
+struct StubDataService: DataServable {
+    
+    
+    private let sampleData = Dummy.storeData
+    
+    func fetchData<E: EndPoint, D: Decodable>(_ endPoint: E,
+                                              completion: @escaping (Result<D, Error>) -> Void)  {
+        do {
+            let decodable = try JSONDecoder().decode(D.self, from: sampleData)
+            completion(.success(decodable))
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+}
