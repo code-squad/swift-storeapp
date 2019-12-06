0f93b8195b1647937dc70a0e3c559b6c5b79428a
부엉이 <o.o.wl.soft@gmail.com>
2019-12-06 21:32:32 +0900

    feat: NSMutableAttrubutedString 를 핸들링하는 객체 추가

---
+++ StoreApp/StoreApp/Common/BeautyString/BeautyStringDSL.swift
@@ -0,0 +1,59 @@
+//
+//  BeautyStringDSL.swift
+//  StoreApp
+//
+//  Created by 이동영 on 2019/12/06.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+import Then
+import UIKit
+
+class BeautyStringDSL {
+    private var string: NSMutableAttributedString
+    private var range: NSRange
+    private var paragraphStyle: NSMutableParagraphStyle = .init()
+    
+    init(_ string : NSMutableAttributedString) {
+        self.string = string
+        self.range = NSRange(location: 0, length: self.string.length)
+    }
+    
+    @discardableResult
+    func font(_ font: UIFont) -> Self {
+        string.addAttributes([.font: font], range: range)
+        return self
+    }
+    
+    @discardableResult
+    func fgColor(_ color: UIColor) -> Self {
+        string.addAttributes([.foregroundColor: color], range: range)
+        return self
+    }
+    
+    @discardableResult
+    func bgColor(_ color: UIColor) -> Self {
+        string.addAttributes([.backgroundColor: color], range: range)
+        return self
+    }
+    
+    @discardableResult
+    func spacing(_ gap: Double) -> Self {
+        string.addAttributes([.kern: gap], range: range)
+        return self
+    }
+    
+    @discardableResult
+    func strikethrough(_ width: Double) -> Self {
+        string.addAttribute(.strikethroughStyle, value: width, range: range)
+        return self
+    }
+    
+    @discardableResult
+    func align(_ alignment: NSTextAlignment) -> Self {
+        paragraphStyle.alignment = alignment
+        string.addAttribute(.paragraphStyle, value: paragraphStyle, range: range)
+        return self
+    }
+}
+
+extension BeautyStringDSL: Then {}
