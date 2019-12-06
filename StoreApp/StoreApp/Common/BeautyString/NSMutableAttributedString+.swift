4552d0bfe08f8259174d179116851805fa5321fc
부엉이 <o.o.wl.soft@gmail.com>
2019-12-06 21:32:32 +0900

    feat: NSMutableAttrubutedString 를 핸들링하는 객체 추가

---
+++ StoreApp/StoreApp/Common/BeautyString/NSMutableAttributedString+.swift
@@ -0,0 +1,15 @@
+//
+//  NSMutableAttributedString+.swift
+//  StoreApp
+//
+//  Created by 이동영 on 2019/12/06.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import Foundation
+
+extension NSMutableAttributedString {
+    var beauty: BeautyStringDSL {
+        return BeautyStringDSL(self)
+    }
+}
