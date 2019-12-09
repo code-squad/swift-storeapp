4f6ac4559fdd2d1739bf56d1d0709882d7df7d1b
부엉이 <o.o.wl.soft@gmail.com>
2019-12-09 18:16:26 +0900

    feat: Bindable 프로토콜 분리 & 디렉터리 정리

---
+++ StoreApp/StoreApp/ViewModel/StoreListViewBindable.swift
@@ -0,0 +1,34 @@
+//
+//  StoreListViewBindable.swift
+//  StoreApp
+//
+//  Created by 이동영 on 2019/12/09.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - StoreListViewDataSource
+
+protocol StoreListViewDataSource : AnyObject {
+    
+    func numOfCategories() -> Int
+    func numOfMenusInCategory(_ category: Int) -> Int
+    
+    subscript(category index: Int) -> String { get }
+    subscript(menu indexPath: IndexPath) -> Menu { get }
+}
+
+// MARK: - StoreListViewInteractable
+
+protocol StoreListViewInteractable {
+    
+    var dataDidLoad: (() -> Void)? { get set }
+    var dataDidUpadated: (() -> Void)? { get set }
+}
+
+// MARK: - StoreListViewBindable
+
+protocol StoreListViewBindable: StoreListViewDataSource, StoreListViewInteractable {
+    
+}
