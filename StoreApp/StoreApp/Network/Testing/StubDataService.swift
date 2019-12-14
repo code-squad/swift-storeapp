25b59aa5e31d5fd63e6566ce3c78d0a196085632
부엉이 <o.o.wl.soft@gmail.com>
2019-12-15 02:09:13 +0900

    refactor: DispatchGroup으로 변경 / serial 하게 구현하여 순서보장하게 구현

--- StoreApp/StoreApp/Network/Testing/StubDataService.swift
+++ StoreApp/StoreApp/Network/Testing/StubDataService.swift
@@ -9,16 +9,60 @@ import Foundation
 
 struct StubDataService: DataServable {
     
-    func fetchData<D: Decodable>(_ endPoint: EndPoint,
-                                 completion: @escaping (Result<D, Error>) -> Void) {
+    let dispatchGroup = DispatchGroup()
+    let serialQueue = DispatchQueue(label: "store.fetch", qos: .default)
+    
+    func fetchData(completion: @escaping (Result<[Store], Error>) -> Void) {
+        var stores: [Store] = []
         
-        guard let data = endPoint.sampleData else { return }
+        fetchData(endPoint: DummyAPI.main) { result in
+            switch result {
+            case .success(let menus):
+                let store = Store(category: .main, menus: menus)
+                stores.append(store)
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
         
-        do {
-            let decodable = try JSONDecoder().decode(D.self, from: data)
-            completion(.success(decodable))
-        } catch let error {
-            completion(.failure(error))
+        fetchData(endPoint: DummyAPI.soup) { result in
+            switch result {
+            case .success(let menus):
+                let store = Store(category: .soup, menus: menus)
+                stores.append(store)
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        
+        fetchData(endPoint: DummyAPI.side) { result in
+            switch result {
+            case .success(let menus):
+                let store = Store(category: .side, menus: menus)
+                stores.append(store)
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        
+        dispatchGroup.notify(queue: .global()) {
+            completion(.success(stores))
         }
     }
+    
+    private func fetchData(endPoint: EndPoint,
+                           completion: @escaping (Result<[Menu], Error>) -> Void) {
+        
+        serialQueue.async(group: dispatchGroup) {
+            guard let data = endPoint.sampleData else { return }
+            
+            do {
+                let menus = try JSONDecoder().decode([Menu].self, from: data)
+                completion(.success(menus))
+            } catch let error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
 }
