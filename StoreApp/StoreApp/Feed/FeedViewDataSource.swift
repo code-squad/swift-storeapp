3106ea4ae169c58315d4a45007886fe20bb2f3a0
cmindy <cmindy.soft@gmail.com>
2019-12-16 02:33:46 +0900

    feat: json 디코딩을 통해 여러 섹션의 아이템을 보여주는 기능 추가

--- StoreApp/StoreApp/Feed/FeedViewDataSource.swift
+++ StoreApp/StoreApp/Feed/FeedViewDataSource.swift
@@ -9,45 +9,61 @@
 import UIKit
 
 class FeedViewDataSource: NSObject, UITableViewDataSource {
+
+    // MARK: - Section
     
+    struct FeedSection {
+        let category: StoreItemCategory
+        let storeItems: [StoreItem]
+    }
+
     // MARK: - Properties
     
-    var storeItems: [StoreItem] = [] {
-        didSet {
-            NotificationCenter.default.post(name: FeedEvent.itemDidUpdated.name, object: nil)
-        }
-    }
+    private var feedSections: [FeedSection] = []
     
     override init() {
         super.init()
         
-        decodeJSON()
+        decodeJSON(assets: StoreItemCategory.allCases)
     }
     
     // MARK: - UITableViewDataSource
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return feedSections.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return storeItems.count
+        return feedSections[section].storeItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedListCell.reuseID, for: indexPath) as? FeedListCell else {
             return .init()
         }
+        let storeItems = feedSections[indexPath.section].storeItems
         let storeItem = storeItems[indexPath.row]
         cell.configure(storeItem)
         return cell
     }
 }
 
+
+// MARK: - Methods
+
 extension FeedViewDataSource {
-    private func decodeJSON() {
+    private func decodeJSON(assets: [StoreItemCategory]) {
         let jsonDecoder = JSONDecoder()
-        guard let dataAsset = NSDataAsset(name: "main") else { return }
-        do {
-            storeItems = try jsonDecoder.decode([StoreItem].self, from: dataAsset.data)
-        } catch {
-            print(error.localizedDescription)
+        
+        assets.forEach { asset in
+            guard let dataAsset = NSDataAsset(name: asset.rawValue) else { return }
+            do {
+                let storeItems = try jsonDecoder.decode([StoreItem].self, from: dataAsset.data)
+                feedSections.append(FeedSection(category: asset, storeItems: storeItems))
+            } catch {
+                print(error.localizedDescription)
+            }
         }
+        NotificationCenter.default.post(name: FeedEvent.itemDidUpdated.name, object: nil)
     }
 }
