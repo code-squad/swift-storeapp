6b27237a15b43e57c958b28920c8a6c03a8cdaa4
cmindy <cmindy.soft@gmail.com>
2019-12-16 01:10:35 +0900

    ui: 디자인에 맞춰 Feed 뱃지의 속성 변경

--- StoreApp/StoreApp/Resources/Color/StoreColor.swift
+++ StoreApp/StoreApp/Resources/Color/StoreColor.swift
@@ -10,6 +10,6 @@ import UIKit
 
 enum StoreColor {
     static let teal = UIColor(red: 41/255, green: 182/255, blue: 174/255, alpha: 1.0)
-    static let purple = UIColor(red: 151/255, green: 91/255, blue: 177/255, alpha: 1.0)
+    static let purple = UIColor(red: 166/255, green: 113/255, blue: 188/255, alpha: 1.0)
     static let yellow = UIColor(red: 245/255, green: 168/255, blue: 9/255, alpha: 1.0)
 }
