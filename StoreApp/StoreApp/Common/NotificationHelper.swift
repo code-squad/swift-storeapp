17fd1da78d6fe4074d3e62e7efd1b800c14553e0
cmindy <cmindy.soft@gmail.com>
2019-12-16 00:47:11 +0900

    feat: 노티피케이션 네임 관리를 위한 프로토콜 추가

---
+++ StoreApp/StoreApp/Common/NotificationHelper.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationHelper.swift
+//  StoreApp
+//
+//  Created by CHOMINJI on 2019/12/16.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import Foundation
+
+protocol NotificationNameable {
+    var name: NSNotification.Name { get }
+}
