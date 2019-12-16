f8d415aee396f04c108deb9b8694ade3e22d89cf
cmindy <cmindy.soft@gmail.com>
2019-12-17 01:24:48 +0900

    fix: presenter를 외부에서 주입하도록 변경

--- StoreApp/StoreApp/AppDelegate.swift
+++ StoreApp/StoreApp/AppDelegate.swift
@@ -14,7 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        if let rootViewController = window?.rootViewController as? FeedViewController {
+            let feedViewPresenter = FeedViewPresenter(feedDataProvider: FeedDataProvider())
+            rootViewController.presenter = feedViewPresenter
+        }
         return true
     }
 
