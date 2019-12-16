953645899e3749dcb14fd48ca392a1766ba370a8
cmindy <cmindy.soft@gmail.com>
2019-12-16 21:47:17 +0900

    fix: FeedSection 파일 추가

---
+++ StoreApp/StoreApp/Model/Domain/FeedSection.swift
@@ -0,0 +1,14 @@
+//
+//  FeedSection.swift
+//  StoreApp
+//
+//  Created by CHOMINJI on 2019/12/16.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import Foundation
+
+struct FeedSection {
+    let category: StoreItemCategory
+    let storeItems: [StoreItem]
+}
